ada607fa2fa09cdf17233ae2d2a4b6717dbccca6
mourner <agafonkin@gmail.com>
2010-09-03 19:49:32 +0300

    namespace handling improvements, spec for noConflict

---
+++ spec/suites/envSpec.js
@@ -0,0 +1,15 @@
+describe('L#noConflict', function() {
+	it('should restore the previous L value and return Leaflet namespace', function(){
+		
+		expect(L.version).toBeDefined();
+		
+		var L2 = L.noConflict();
+		
+		expect(L).toEqual('test');
+		expect(L2.version).toBeDefined();
+		
+		this.after(function() {
+			window.L = L2;
+		});
+	});
+});
--- src/env.js
+++ src/env.js
@@ -1,11 +1,16 @@
 /*
- * Leaflet namespace config, allows using any global variable instead of L
+ * Leaflet namespace config, allows using any global variable instead of L & restoring the original value
  */
 
-var originalL = window.L, 
-	L = {};
-
-L.noConflict = function() {
-	window.L = originalL;
-	return this;
-};
+(function() {
+	var originalL = window.L;
+	
+	L = {
+		version: '0.0.1'
+	};
+	
+	L.noConflict = function() {
+		window.L = originalL;
+		return this;
+	};
+})();
