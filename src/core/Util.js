361d79c51f0875c1e7aa1b60e4e60e100d374241
mourner <agafonkin@gmail.com>
2010-09-15 17:04:02 +0300

    clean up formatting

--- src/core/Util.js
+++ src/core/Util.js
@@ -2,33 +2,33 @@
  * L.Util is a namespace for various utility functions.
  */
 
-L.Util = {};
-
-L.Util.extend = function(/*Object*/ dest) /*-> Object*/ {	// merge src properties into dest
-	var sources = Array.prototype.slice.call(arguments, 1);
-	for (var j = 0, len = sources.length, src; j < len; j++) {
-		src = sources[j] || {};
-		for (var i in src) {
-			if (src.hasOwnProperty(i)) {
-				dest[i] = src[i];
+L.Util = {
+	extend: function(/*Object*/ dest) /*-> Object*/ {	// merge src properties into dest
+		var sources = Array.prototype.slice.call(arguments, 1);
+		for (var j = 0, len = sources.length, src; j < len; j++) {
+			src = sources[j] || {};
+			for (var i in src) {
+				if (src.hasOwnProperty(i)) {
+					dest[i] = src[i];
+				}
 			}
 		}
-	}
-	return dest;
+		return dest;
+	},
+
+	bind: function(/*Function*/ fn, /*Object*/ obj) /*-> Object*/ {
+		return function() {
+			return fn.apply(obj, arguments);
+		};
+	},
+
+	stamp: (function() {
+		var lastId = 0, key = '_leaflet_id';
+		return function(/*Object*/ obj) {
+			obj[key] = obj[key] || ++lastId;
+			return obj[key];
+		};
+	})(),
+
+	falseFn: function() { return false; }	
 };
-
-L.Util.bind = function(/*Function*/ fn, /*Object*/ obj) /*-> Object*/ {
-	return function() {
-		return fn.apply(obj, arguments);
-	};
-};
-
-L.Util.stamp = (function() {
-	var lastId = 0, key = '_leaflet_id';
-	return function(/*Object*/ obj) {
-		obj[key] = obj[key] || ++lastId;
-		return obj[key];
-	};
-})();
-
-L.Util.falseFn = function() { return false; };
--- src/dom/DomEvent.js
+++ src/dom/DomEvent.js
@@ -3,65 +3,65 @@
  */
 
 L.DomEvent = {
-		/* inpired by John Resig, Dean Edwards and YUI addEvent implementations */
-		addListener: function(/*HTMLElement*/ obj, /*String*/ type, /*Function*/ fn, /*Object*/ context) {
-			var id = L.Util.stamp(fn);
-			function handler(e) {
-				return fn.call(context || obj, e || L.DomEvent._getEvent());
-			};
-			obj['_leaflet_' + type + id] = handler;
-			if ('addEventListener' in obj) {
-				if (type == 'mousewheel') {
-					obj.addEventListener('DOMMouseScroll', handler, false); 
-				}
-				obj.addEventListener(type, handler, false);
-			} else if ('attachEvent' in obj) {
-				obj.attachEvent("on" + type, handler);
+	/* inpired by John Resig, Dean Edwards and YUI addEvent implementations */
+	addListener: function(/*HTMLElement*/ obj, /*String*/ type, /*Function*/ fn, /*Object*/ context) {
+		var id = L.Util.stamp(fn);
+		function handler(e) {
+			return fn.call(context || obj, e || L.DomEvent._getEvent());
+		};
+		obj['_leaflet_' + type + id] = handler;
+		if ('addEventListener' in obj) {
+			if (type == 'mousewheel') {
+				obj.addEventListener('DOMMouseScroll', handler, false); 
 			}
-		},
-		
-		removeListener: function(/*HTMLElement*/ obj, /*String*/ type, /*Function*/ fn) {
-			var id = L.Util.stamp(fn),
-				key = '_leaflet_' + type + id;
-				handler = obj[key];
-			if ('removeEventListener' in obj) {
-				if (type == 'mousewheel') {
-					obj.removeEventListener('DOMMouseScroll', handler, false); 
-				}
-				obj.removeEventListener(type, handler, false);
-			} else if ('detachEvent' in obj) {
-				obj.detachEvent("on" + type, handler);
-				delete obj[key];
+			obj.addEventListener(type, handler, false);
+		} else if ('attachEvent' in obj) {
+			obj.attachEvent("on" + type, handler);
+		}
+	},
+	
+	removeListener: function(/*HTMLElement*/ obj, /*String*/ type, /*Function*/ fn) {
+		var id = L.Util.stamp(fn),
+			key = '_leaflet_' + type + id;
+			handler = obj[key];
+		if ('removeEventListener' in obj) {
+			if (type == 'mousewheel') {
+				obj.removeEventListener('DOMMouseScroll', handler, false); 
 			}
-		},
-		
-		_getEvent: function()/*->Event*/ {
-			var e = window.event;
-			if (!e) {
-				var caller = arguments.callee.caller;
-				while (caller) {
-					e = caller['arguments'][0];
-					if (e && Event == e.constructor) { break; }
-					caller = caller.caller;
-				}
-			}
-			return e;
-		},
-		
-		stopPropagation: function(/*Event*/ e) {
-			if (e.stopPropagation) {
-				e.stopPropagation();
-			} else {
-				e.cancelBubble = true;
-			}
-		},
-		
-		preventDefault: function(/*Event*/ e) {
-			if (e.preventDefault) {
-				e.preventDefault();
-			} else {
-				e.returnValue = false;
+			obj.removeEventListener(type, handler, false);
+		} else if ('detachEvent' in obj) {
+			obj.detachEvent("on" + type, handler);
+			delete obj[key];
+		}
+	},
+	
+	_getEvent: function()/*->Event*/ {
+		var e = window.event;
+		if (!e) {
+			var caller = arguments.callee.caller;
+			while (caller) {
+				e = caller['arguments'][0];
+				if (e && Event == e.constructor) { break; }
+				caller = caller.caller;
 			}
 		}
+		return e;
+	},
+	
+	stopPropagation: function(/*Event*/ e) {
+		if (e.stopPropagation) {
+			e.stopPropagation();
+		} else {
+			e.cancelBubble = true;
+		}
+	},
+	
+	preventDefault: function(/*Event*/ e) {
+		if (e.preventDefault) {
+			e.preventDefault();
+		} else {
+			e.returnValue = false;
+		}
+	}
 };
 
