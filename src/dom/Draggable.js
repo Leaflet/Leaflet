c407e5c771be066f5301f303c48c2585f08acf84
mourner <agafonkin@gmail.com>
2010-09-16 16:05:55 +0300

    added Draggable

---
+++ src/dom/Draggable.js
@@ -0,0 +1,91 @@
+L.Draggable = L.Class.extend({
+	includes: L.Mixin.Events,
+	
+	statics: {
+		START: L.Browser.mobileWebkit ? 'touchstart' : 'mousedown',
+		END: L.Browser.mobileWebkit ? 'touchend' : 'mouseup',
+		MOVE: L.Browser.mobileWebkit ? 'touchmove' : 'mousemove'
+	},
+	
+	initialize: function(element, dragStartTarget) {
+		this._element = element;
+		this._dragStartTarget = dragStartTarget || element;
+		
+		this.enable();
+	},
+	
+	enable: function() {
+		if (this._enabled) { return; }
+		L.DomEvent.addListener(this._dragStartTarget, L.Draggable.START, this._onDown, this);
+		this._enabled = true;
+	},
+	
+	disable: function() {
+		if (!this._enabled) { return; }
+		L.DomEvent.removeListener(this._dragStartTarget, L.Draggable.START, this._onDown);
+		this._enabled = false;
+	},
+	
+	_onDown: function(e) {
+		if (e.shiftKey || ((e.which != 1) && (e.button != 1) && !e.touches)) { return; }
+		
+		L.DomEvent.preventDefault(e);
+
+		if (e.touches) {
+			if (e.touches.length == 1) { e = e.touches[0]; }
+			else return;
+		}
+		
+		this._dragStartPos = L.DomUtil.getPosition(this._element);
+		this._startX = e.clientX;
+		this._startY = e.clientY;
+		
+		L.DomUtil.disableTextSelection();
+		
+		this._setMovingCursor();
+		
+		L.DomEvent.addListener(document, L.Draggable.MOVE, this._onMove, this);
+		L.DomEvent.addListener(document, L.Draggable.END, this._onUp, this);
+	},
+	
+	_onMove: function(e) {
+		L.DomEvent.preventDefault(e);
+
+		if (e.touches) {
+			if (e.touches.length == 1) { e = e.touches[0]; }
+			else return;
+		}
+		
+		var offset = new L.Point(e.clientX - this._startX, e.clientY - this._startY),
+			newPos = this._dragStartPos.add(offset);
+		
+		if (!this._notFirstMove) {
+			this.fire('dragstart');
+			this._notFirstMove = true;
+		}
+		
+		L.DomUtil.setPosition(this._element, newPos);
+		
+		this.fire('drag');
+	},
+	
+	_onUp: function(e) {
+		L.DomUtil.enableTextSelection();
+		
+		this._restoreCursor();
+		
+		L.DomEvent.removeListener(document, L.Draggable.MOVE, this._onMove);
+		L.DomEvent.removeListener(document, L.Draggable.END, this._onUp);
+		
+		this.fire('dragend');
+	},
+	
+	_setMovingCursor: function() {
+		this._bodyCursor = document.body.style.cursor;
+		document.body.style.cursor = 'move';
+	},
+	
+	_restoreCursor: function() {
+		document.body.style.cursor = this._bodyCursor;
+	}
+});
