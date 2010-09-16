5a6f41dd54b4e995f0031a7e557f8229f4296c8c
mourner <agafonkin@gmail.com>
2010-09-16 18:34:51 +0300

    Handler

--- src/handler/Handler.js
+++ src/handler/Handler.js
@@ -1,3 +1,7 @@
+/*
+ * L.Handler classes are used internally to inject interaction features to the Map class.
+ */
+
 L.Handler = L.Class.extend({
 	initialize: function(map, enabled) {
 		this._map = map;
