ff36820cf914355b7f3f8852f706e3d1133b2e1a
mourner <agafonkin@gmail.com>
2010-09-23 17:25:55 +0300

    fix namespace spec

--- spec/suites/LeafletSpec.js
+++ spec/suites/LeafletSpec.js
@@ -1,12 +1,12 @@
 describe('L#noConflict', function() {
 	it('should restore the previous L value and return Leaflet namespace', function(){
 		
-		expect(L.version).toBeDefined();
+		expect(L.VERSION).toBeDefined();
 		
 		var L2 = L.noConflict();
 		
 		expect(L).toEqual('test');
-		expect(L2.version).toBeDefined();
+		expect(L2.VERSION).toBeDefined();
 		
 		this.after(function() {
 			window.L = L2;
