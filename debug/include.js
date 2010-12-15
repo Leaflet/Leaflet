03e49eeaab8167fe14eef77029b3dcbba2a1235a
Mourner <agafonkin@gmail.com>
2010-12-15 17:14:59 +0200

    debug pages with common include

---
+++ debug/include.js
@@ -0,0 +1,45 @@
+(function() {
+	var scripts = [
+		'Leaflet.js',
+		
+		'core/Util.js', 
+		'core/Class.js',
+		'core/Events.js',
+		'core/Browser.js',
+		
+		'dom/DomEvent.js',
+		'dom/DomUtil.js',
+		'dom/Draggable.js',
+		
+		'dom/transition/Transition.js',
+		'dom/transition/Transition.Native.js',
+		'dom/transition/Transition.Timer.js',
+		
+		'geo/LatLng.js',
+		'geo/LatLngBounds.js',
+		'geo/Projection.js',
+		
+		'geometry/Point.js',
+		'geometry/Bounds.js',
+		'geometry/Transformation.js',
+		
+		'layer/TileLayer.js',
+		'layer/ImageOverlay.js',
+		'layer/Icon.js',
+		'layer/Marker.js',
+		
+		'handler/Handler.js',
+		'handler/MapDrag.js',
+		'handler/TouchZoom.js',
+		'handler/DoubleClickZoom.js',
+		'handler/ScrollWheelZoom.js',
+		
+		'map/Map.js',
+		'map/Map.Geolocation.js',
+		'map/Map.Animation.js'
+	];
+	
+	for (var i = 0; i < scripts.length; i++) {
+		document.writeln("<script type='text/javascript' src='../src/" + scripts[i] + "'></script>");
+	}
+})();
