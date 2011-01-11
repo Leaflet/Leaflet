663620efedd6b50166cafa92f6e0e88f1ecf7809
Mourner <agafonkin@gmail.com>
2011-01-11 17:51:02 +0200

    Marker#openPopup

--- src/layer/Marker.Popup.js
+++ src/layer/Marker.Popup.js
@@ -1,12 +1,15 @@
 
 L.Marker.include({
+	openPopup: function(content) {
+		this._map.openPopup(this._latlng, content, this.options.icon.popupAnchor);
+	},
+	
 	bindPopup: function(content) {
 		this._popupContent = content;
 		this.on('click', this._onMouseDown, this);
 	},
 	
 	_onMouseDown: function() {
-		this._map.closePopup();
-		this._map.openPopup(this._latlng, this._popupContent, this.options.icon.popupAnchor);
+		this.openPopup(this._popupContent);
 	}
 });
