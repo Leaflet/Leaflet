47ada8f7f25570cb195fe9f961e33de6fb305021
Mourner <agafonkin@gmail.com>
2011-04-13 14:51:31 +0300

    fix TileLayer error on mobile webkit

--- src/layer/TileLayer.js
+++ src/layer/TileLayer.js
@@ -143,7 +143,9 @@ L.TileLayer = L.Class.extend({
 				
 				// remove tile if it's out of bounds
 				if (x < bounds.min.x || x > bounds.max.x || y < bounds.min.y || y > bounds.max.y) {
-					this._container.removeChild(this._tiles[key]);
+					if (this._tiles[key].parentNode == this._container) {
+						this._container.removeChild(this._tiles[key]);
+					}
 					delete this._tiles[key];
 				}
 			}
@@ -183,7 +185,7 @@ L.TileLayer = L.Class.extend({
 
 		var layer = this._leaflet_layer;
 		
-		layer.fire('tileload', {tile: this});
+		layer.fire('tileload', {tile: this, url: this.src});
 		
 		layer._tilesToLoad--;
 		if (!layer._tilesToLoad) {
