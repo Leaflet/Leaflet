e243d84ec604e68107345e22190f33cc7569dfeb
Mourner <agafonkin@gmail.com>
2011-05-17 14:19:37 +0300

    WMS refactoring

--- src/layer/TileLayer.WMS.js
+++ src/layer/TileLayer.WMS.js
@@ -1,5 +1,7 @@
 L.TileLayer.WMS = L.TileLayer.extend({
 	defaultWmsParams: {
+		service: 'WMS',
+		request: 'GetMap',
 		version: '1.1.1',
 		layers: '',
 		styles: '',
@@ -7,59 +9,35 @@ L.TileLayer.WMS = L.TileLayer.extend({
 		transparent: false
 	},
 
-	initialize: function(url, options) {
+	initialize: function(/*String*/ url, /*Object*/ options) {
 		this._url = url;
-		L.Util.setOptions(this, options);
 		
-		var tileSize = this.options.tileSize;
-		
-		this.wmsParams = L.Util.extend({
-			service: 'WMS',
-			request: 'GetMap',
-			width: tileSize,
-			height: tileSize
-		}, this.defaultWmsParams);
+		this.wmsParams = L.Util.extend({}, this.defaultWmsParams);
+		this.wmsParams.width = this.wmsParams.height = this.options.tileSize;
 		
 		for (var i in options) {
-			if (this.defaultWmsParams.hasOwnProperty(i)) {
+			// all keys that are not TileLayer options go to WMS params
+			if (!this.options.hasOwnProperty(i)) {
 				this.wmsParams[i] = options[i];
 			}
 		}
-		
 		var projectionKey = (parseFloat(this.wmsParams.version) >= 1.3 ? 'crs' : 'srs');
 		this.wmsParams[projectionKey] = 'EPSG:3857';
+
+		L.Util.setOptions(this, options);
 	},
 	
-	getTileUrl: function(tilePoint, zoom) {
+	getTileUrl: function(/*Point*/ tilePoint, /*Number*/ zoom)/*-> String*/ {
 		var tileSize = this.options.tileSize,
 			nwPoint = tilePoint.multiplyBy(tileSize),
 			sePoint = nwPoint.add(new L.Point(tileSize, tileSize)),
-			nw = this._map.unproject(nwPoint, zoom, true),
-			se = this._map.unproject(sePoint, zoom, true),
-			nwMerc = this._latlngToSphericalMerc(nw),
-			seMerc = this._latlngToSphericalMerc(se),
-			bbox = [nwMerc.x, seMerc.y, seMerc.x, nwMerc.y].join(',');
+			nwMerc = this._map.unproject(nwPoint),
+			seMerc = this._map.unproject(sePoint),
+			r = 6378137,
+			nw = L.Projection.Mercator.project(nwMerc).multiplyBy(r),
+			se = L.Projection.Mercator.project(seMerc).multiplyBy(r),
+			bbox = [nw.x, se.y, se.x, nw.y].join(',');
 		
-		return this._url + this._getParamsStr() + "&bbox=" + bbox; 
-	},
-	
-	_latlngToSphericalMerc: function(latlng) {
-		var r = 6378137,
-			d = L.LatLng.DEG_TO_RAD,
-			x = r * latlng.lng * d,
-			sin = Math.sin(latlng.lat * d),
-			y = (r / 2) * Math.log((1 + sin)/(1 - sin));
-		
-		return new L.Point(x, y);
-	},
-	
-	_getParamsStr: function() {
-		var params = [];
-		for (var i in this.wmsParams) {
-			if (this.wmsParams.hasOwnProperty(i)) {
-				params.push(i + '=' + this.wmsParams[i]);
-			}
-		}
-		return '?' + params.join('&');
+		return this._url + L.Util.getParamString(this.wmsParams) + "&bbox=" + bbox;
 	}
 });
