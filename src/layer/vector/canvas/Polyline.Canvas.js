436915617a8ffa30b99c9c96b9e705dba119bba3
Mourner <agafonkin@gmail.com>
2011-06-21 14:37:34 +0300

    canvas polygon clicks through ray casting (yay!), refactoring

--- src/layer/vector/canvas/Polyline.Canvas.js
+++ src/layer/vector/canvas/Polyline.Canvas.js
@@ -8,22 +8,25 @@ L.Polyline.include(L.Path.SVG || !L.Path.CANVAS ? {} : {
 		}
 	},
 	
-	_onClick: function(e) {
-		var p1 = this._point, 
-			p2 = e.layerPoint;
-		
+	_containsPoint: function(p) {
 		var i, j, len, len2, dist, part;
-	
+		
 		for (i = 0, len = this._parts.length; i < len; i++) {
 			part = this._parts[i];
 			for (j = 0, len2 = part.length; j < len2 - 1; j++) {
-				dist = L.LineUtil.pointToSegmentDistance(e.layerPoint, part[j], part[j+1]);
+				dist = L.LineUtil.pointToSegmentDistance(p, part[j], part[j+1]);
 				
 				if (dist <= this.options.weight / 2) {
-					this.fire('click', e);
-					return;
+					return true;
 				}
 			}
+		}
+		return false;
+	},
+	
+	_onClick: function(e) {
+		if (this._containsPoint(e.layerPoint)) {
+			this.fire('click', e);
 		}
 	}
 });
