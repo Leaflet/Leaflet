a46c9040a73f27ea89e3c16cfe3f61ce977c54aa
Mourner <agafonkin@gmail.com>
2011-06-21 14:04:35 +0300

    canvas polyline click events

--- src/layer/vector/Polyline.Canvas.js
+++ src/layer/vector/Polyline.Canvas.js
@@ -1,6 +1,29 @@
 
 L.Polyline.include(L.Path.SVG || !L.Path.CANVAS ? {} : {
 	_initEvents: function() {
-		// TODO polyline events (through loop with pointToSegmentDistance)
+		if (this.options.clickable) {
+			// TODO hand cursor
+			// TODO mouseover, mouseout, dblclick 
+			this._map.on('click', this._onClick, this);
+		}
+	},
+	
+	_onClick: function(e) {
+		var p1 = this._point, 
+			p2 = e.layerPoint;
+		
+		var i, j, len, len2, dist, part;
+	
+		for (i = 0, len = this._parts.length; i < len; i++) {
+			part = this._parts[i];
+			for (j = 0, len2 = part.length; j < len2 - 1; j++) {
+				dist = L.LineUtil.pointToSegmentDistance(e.layerPoint, part[j], part[j+1]);
+				
+				if (dist <= this.options.weight / 2) {
+					this.fire('click', e);
+					return;
+				}
+			}
+		}
 	}
 });
