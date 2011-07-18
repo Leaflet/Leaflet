9bf87a81ca071a3e8e22efa4de76129989ca5ce0
Adam St. John <astjohn@gmail.com>
2011-07-18 15:12:38 -0400

    Add LatLng object to polyline on click.

--- src/layer/vector/drawing/Polyline.Drawing.js
+++ src/layer/vector/drawing/Polyline.Drawing.js
@@ -5,19 +5,33 @@
 L.Polyline = L.Polyline.extend({
   enableDrawing: function () {
     if (!this._drawingEnabled) {
-      //CM.Event.addListener(this.map, 'click', this._onDrawingClick, this);
+      this._map.on('click', this._onDrawingClick, this);
       this._drawingEnabled = true;
     }
   },
   disableDrawing: function () {
     if (this._drawingEnabled) {
-      //CM.Event.removeListener(this.map, 'click', this._onDrawingClick, this);
+      this._map.off('click', this._onDrawingClick, this);
       this._drawingEnabled = false;
     }
   },
   drawingEnabled: function () {
     return !!this._drawingEnabled;
   },
-
+  _onDrawingClick: function (e) {
+    this.addLatLng(e.latlng);
+//    if (this._editingEnabled) {
+//      this._markers.push(this._createMarker(a));
+//      if (b > 0) {
+//        if (this._markers[b - 1].middleRight) {
+//          this.map.removeOverlay(this._markers[b - 1].middleRight)
+//        }
+//        this._createMiddleMarker(this._markers[b - 1], this._markers[b]);
+//        if (CM.Polygon && (this instanceof CM.Polygon)) {
+//          this._createMiddleMarker(this._markers[b], this._markers[0])
+//        }
+//      }
+//    }
+  }
 });
 
