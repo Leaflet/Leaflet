744e180df0df04fbc40ac35658052663c8d33afd
Paul Spencer <paul@pagameba.com>
2011-12-01 10:46:35 -0500

    Revert "Merge branch 'drawing' of https://github.com/astjohn/Leaflet"
    This reverts commit 1cd88e6c79b787b1ce29a2df6a0ffc09b57410d3, reversing
    changes made to 92a41e21070a28037172effea85c40991e06ef1f.

--- debug/leaflet-include.js
+++ debug/leaflet-include.js
@@ -2,35 +2,35 @@
 	//TODO replace script list with the one from ../buid/deps.js
 	var scripts = [
 		'Leaflet.js',
-
-		'core/Util.js',
+		
+		'core/Util.js', 
 		'core/Class.js',
 		'core/Events.js',
 		'core/Browser.js',
-
+		
 		'geometry/Point.js',
 		'geometry/Bounds.js',
 		'geometry/Transformation.js',
 		'geometry/LineUtil.js',
 		'geometry/PolyUtil.js',
-
+		
 		'dom/DomEvent.js',
 		'dom/DomEvent.DoubleTap.js',
 		'dom/DomUtil.js',
 		'dom/Draggable.js',
-
+		
 		'dom/transition/Transition.js',
 		'dom/transition/Transition.Native.js',
 		'dom/transition/Transition.Timer.js',
-
+		
 		'geo/LatLng.js',
 		'geo/LatLngBounds.js',
-
+		
 		'geo/projection/Projection.js',
 		'geo/projection/Projection.SphericalMercator.js',
 		'geo/projection/Projection.LonLat.js',
 		'geo/projection/Projection.Mercator.js',
-
+		
 		'geo/crs/CRS.js',
 		'geo/crs/CRS.EPSG3857.js',
 		'geo/crs/CRS.EPSG4326.js',
@@ -45,17 +45,17 @@
 		
 		'layer/LayerGroup.js',
 		'layer/FeatureGroup.js',
-
+		
 		'layer/tile/TileLayer.js',
 		'layer/tile/TileLayer.WMS.js',
 		'layer/tile/TileLayer.Canvas.js',
 		'layer/ImageOverlay.js',
 		'layer/Popup.js',
-
+		
 		'layer/marker/Icon.js',
 		'layer/marker/Marker.js',
 		'layer/marker/Marker.Popup.js',
-
+		
 		'layer/vector/Path.js',
 		'layer/vector/Path.Popup.js',
 		'layer/vector/Path.SVG.js',
@@ -69,9 +69,9 @@
 		'layer/vector/Circle.js',
 		'layer/vector/canvas/Circle.Canvas.js',
 		'layer/vector/CircleMarker.js',
-
+		
 		'layer/GeoJSON.js',
-
+		
 		'handler/Handler.js',
 		'handler/MapDrag.js',
 		'handler/TouchZoom.js',
@@ -79,15 +79,13 @@
 		'handler/ScrollWheelZoom.js',
 		'handler/ShiftDragZoom.js',
 		'handler/MarkerDrag.js',
-		'handler/PolyDraw.js',
-		'handler/PolyEdit.js',
-
+		
 		'control/Control.js',
 		'control/Control.Zoom.js',
 		'control/Control.Attribution.js',
 		'control/Control.Layers.js',
 	];
-
+	
 	function getSrcUrl() {
 		var scripts = document.getElementsByTagName('script');
 		for (var i = 0; i < scripts.length; i++) {
@@ -100,7 +98,7 @@
 			}
 		}
 	}
-
+	
 	var path = getSrcUrl();
 	for (var i = 0; i < scripts.length; i++) {
 		document.writeln("<script type='text/javascript' src='" + path + "../src/" + scripts[i] + "'></script>");
@@ -113,9 +111,8 @@ function getRandomLatLng(map) {
 		northEast = bounds.getNorthEast(),
 		lngSpan = northEast.lng - southWest.lng,
 		latSpan = northEast.lat - southWest.lat;
-
+	
 	return new L.LatLng(
 			southWest.lat + latSpan * Math.random(),
 	        southWest.lng + lngSpan * Math.random());
 }
-
