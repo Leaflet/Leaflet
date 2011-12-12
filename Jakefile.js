f2a2868ed3aad55ff00570f51d8a86845f02347b
mourner <agafonkin@gmail.com>
2011-12-12 11:42:42 +0200

    add components configuration to jake build

--- Jakefile.js
+++ Jakefile.js
@@ -3,14 +3,30 @@ var fs = require('fs'),
 	jshint = require('jshint'),
 	deps = require('./build/deps.js').deps;
 
-function getFiles() {
+function getFiles(compsBase32) {
 	var memo = {},
-		i, srcs, j, len;
+		i, srcs, j, len,
+		comps;
+
+	if (compsBase32) {
+		comps = parseInt(compsBase32, 32).toString(2).split('');
+		console.log('Managing dependencies...')
+	}
+
+	var log = [];
 
 	for (i in deps) {
 		srcs = deps[i].src;
-		for (j = 0, len = srcs.length; j < len; j++) {
-			memo[srcs[j]] = true;
+
+		if (!comps || (parseInt(comps.pop(), 2) === 1)) {
+			if (comps) {
+				console.log('\t* ' + i);
+			}
+			for (j = 0, len = srcs.length; j < len; j++) {
+				memo[srcs[j]] = true;
+			}
+		} else if (comps) {
+			console.log('\t  ' + i);
 		}
 	}
 
@@ -24,7 +40,7 @@ function getFiles() {
 	return files;
 }
 
-function myUglify(code){
+function myUglify(code) {
 	var pro = uglify.uglify;
 
 	var ast = uglify.parser.parse(code);
@@ -33,7 +49,7 @@ function myUglify(code){
 	ast = pro.ast_squeeze_more(ast);
 
 	return pro.gen_code(ast);
-};
+}
 
 function combineFiles(files) {
 	var content = '';
@@ -47,26 +63,28 @@ var COPYRIGHT = "/*\n Copyright (c) 2010-2011, CloudMade, Vladimir Agafonkin\n" 
                 " Leaflet is a modern open source JavaScript library for interactive maps.\n" +
                 " http://leaflet.cloudmade.com\n*/\n";
 
-var SAVE_PATH = 'dist/leaflet.js';
+task('build', function (compsBase32, buildName) {
+	var name = buildName || 'custom',
+		savePath = 'dist/leaflet' + (compsBase32 ? '-' + name : '') + '.js';
 
-var files = getFiles();
+	var files = getFiles(compsBase32);
 
-task('build', function () {
 	console.log('Concatenating ' + files.length + ' files...');
 
 	var content = combineFiles(files);
-	
+
 	console.log('Uncompressed size: ' + content.length);
 	console.log('Compressing...');
 
 	var compressed = COPYRIGHT + myUglify(content);
-	fail("Helo world");
 
 	console.log('Compressed size: ' + compressed.length);
 
-	fs.writeFileSync(SAVE_PATH, compressed, 'utf8');
+	fs.writeFileSync(savePath, compressed, 'utf8');
 
-	console.log('Saved to ' + SAVE_PATH);
+	console.log('Saved to ' + savePath);
 });
 
 task('default', ['build']);
+
+// TODO task('lint')
