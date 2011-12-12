b3e5b426758c56d8f121cb3e6e37b02a6baca60c
mourner <agafonkin@gmail.com>
2011-12-12 22:10:29 +0200

    update build

--- dist/leaflet-src.js
+++ dist/leaflet-src.js
@@ -1,6 +1,6 @@
 /*
  Copyright (c) 2010-2011, CloudMade, Vladimir Agafonkin
- Leaflet is a modern open source JavaScript library for interactive maps.
+ Leaflet is a modern open-source JavaScript library for interactive maps.
  http://leaflet.cloudmade.com
 */
 
