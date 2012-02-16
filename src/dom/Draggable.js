bf558551ed06a16ca08d5a25ad18967ac1ade346
mourner <agafonkin@gmail.com>
2012-02-16 12:58:56 +0200

    Fix flickering cursor on marker dragging

--- src/dom/Draggable.js
+++ src/dom/Draggable.js
@@ -127,11 +127,12 @@ L.Draggable = L.Class.extend({
 
 	_setMovingCursor: function () {
 		this._bodyCursor = document.body.style.cursor;
-		document.body.style.cursor = 'move';
+		this._dragStartTarget.style.cursor = document.body.style.cursor = 'move';
 	},
 
 	_restoreCursor: function () {
 		document.body.style.cursor = this._bodyCursor;
+		this._dragStartTarget.style.cursor = '';
 	},
 
 	_simulateEvent: function (type, e) {
