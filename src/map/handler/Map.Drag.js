c2b0037f346c3811f740897c5ec8590b4883e0f9
Dino Ravnić <dino@giscloud.com>
2012-02-25 01:27:54 +0100

    - dragend should fire in _onDragEnd no matter if inertia is on or off - fixed an inertia edge case when _onDragEnd was called and _positions array was empty

--- src/map/handler/Map.Drag.js
+++ src/map/handler/Map.Drag.js
@@ -91,11 +91,8 @@ L.Map.Drag = L.Handler.extend({
 			options = map.options,
 			delay = +new Date() - this._lastTime;
 
-		if (!options.inertia || delay > options.inertiaThreshold) {
-			map
-				.fire('moveend')
-				.fire('dragend');
-
+		if (!options.inertia || delay > options.inertiaThreshold || this._positions[0] === undefined) {
+			map.fire('moveend')
 		} else {
 
 			var direction = this._lastPos.subtract(this._positions[0]),
@@ -125,6 +122,7 @@ L.Map.Drag = L.Handler.extend({
 				L.Util.requestAnimFrame(this._panInsideMaxBounds, map, true, map._container);
 			}
 		}
+		map.fire('dragend');
 	},
 
 	_panInsideMaxBounds: function () {
