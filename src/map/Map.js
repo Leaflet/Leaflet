59f6672e644b3d253e98a27929f40f8f9fc34fb8
mourner <agafonkin@gmail.com>
2012-03-01 14:10:27 +0200

    whitespace fix

--- src/map/Map.js
+++ src/map/Map.js
@@ -26,8 +26,8 @@ L.Map = L.Class.extend({
 
 		inertia: !L.Browser.android,
 		inertiaDeceleration: L.Browser.touch ? 3000 : 2000, // px/s^2
-		inertiaMaxSpeed:      L.Browser.touch ? 1500 : 1000, // px/s
-		inertiaThreshold:      L.Browser.touch ? 32   : 16, // ms
+		inertiaMaxSpeed:     L.Browser.touch ? 1500 : 1000, // px/s
+		inertiaThreshold:    L.Browser.touch ? 32   : 16, // ms
 
 		// controls
 		zoomControl: true,
