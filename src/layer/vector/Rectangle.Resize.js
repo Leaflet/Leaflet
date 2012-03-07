935223ae25a1da08fc4034d655c18be7f88b7415
Chris Brahms <brahms5@gmail.com>
2012-03-07 15:36:12 -0500

    Moving dragging and resize into separate files. Added some more logic towards the goal of having a resizable rectangle.

--- src/layer/vector/Rectangle.Resize.js
+++ src/layer/vector/Rectangle.Resize.js
@@ -1,113 +1,143 @@
-/* Added draggable, and resizable to Rectangle */
-L.Rectangle.Edit = L.Handler.extend({
-  statics: {
-    START: L.Browser.touch ? 'touchstart' : 'mousedown',
-    END: L.Browser.touch ? 'touchend' : 'mouseup',
-    MOVE: L.Browser.touch ? 'touchmove' : 'mousemove'
-  },
-  
-  options: {
-    
-  },
-  
-  initialize: function(rectangle) {
-    this._rectangle = rectangle;
-    L.Util.setOptions(this, options);
-  },
-  
-  addHooks: function() {
-    if (this._rectangle._map) {
-      if (!this._editLayer) {
-        this._initEditLayer();
-      }
-      this._rectangle._map.addLayer(this._editLayer);
-    }
-  },
-  
-  removeHooks: function() {
-    if (this._rectangle._map) {
-      this._rectangle._map.removeLayer(this._editLayer);
-      this._destroyEditLayer();
-    }
-  },
-  
-  _initEditLayer: function() {
-    this._editLayer = new L.LayerGroup();
-    
-    var latLngs = this._rectangle._boundsToLatLngs();
-    this._borderLine = new L.Polyline(latLngs);
-    this._borderLine._rectangleEdit = this;
-    
-    L.DomEvent.addListener(this._borderLine, L.Draggable.START, this._onBorderDragStart, this);
-    
-    this._editLayer.addLayer(this._borderLine);
-    
-    this._cornerMarkers = {
-      southWest: new L.Marker(latLngs[0], {draggable: true}),
-      northWest: new L.Marker(latLngs[1], {draggable: true}),
-      northEast: new L.Marker(latLngs[2], {draggable: true}),
-      southEast: new L.Marker(latLngs[3], {draggable: true})
-    };
-    for (i in this._cornerMarkers) {
-      var cornerMarker = this._cornerMarkers[i];
-      cornerMarker._editRectangle = this;
-      cornerMarker.on('dragstart', this._onCornerDragStart, this);
-      this._editLayer.addLayer(cornerMarker);
-    }
-    
-  },
-  
-  _destroyEditLayer: function() {
-    L.DomEvent.removeListener(this._borderLine, L.Draggable.START, this._onBorderDragStart);
-    this._editLayer.removeLayer(this._borderLine);
-    
-    for (i in this._cornerMarkers) {
-      var cornerMarker = this._cornerMarkers[i];
-      cornerMarker.off('dragstart', this._onCornerDragStart);
-      this._editLayer.removeLayer(cornerMarker);
-    }
-    
-    delete this._cornerMarkers;
-    delete this._borderLine;
-    delete this._editLayer;
-  },
-  
-  _onBorderDragStart: function(event) {
-    
-  },
-  
-  _onBorderDragMove: function(event) {
-    
-  },
-  
-  _onBorderDragEnd: function(event) {
-    
-  },
-  
-  _onCornerDragStart: function(event) {
-    
-  },
-  
-  _onCornerDragMove: function(event) {
-    
-  },
-  
-  _onCornerDragEnd: function(event) {
-    
-  },
-  
-  _onRectangleDragStart: function(event) {
-    
-  },
-  
-  _onRectangleDragMove: function(event) {
-    
-  }
-  
-  _onRectangleDragEnd: function(event) {
-    
-  },
-  
-  
-  
+/* Added  resizable to Rectangle */
+L.Rectangle.Resize = L.Handler.extend({
+
+	initialize : function(rectangle) {
+		this._rectangle = rectangle;
+	},
+
+	addHooks : function() {
+		this._initCorners();
+	},
+
+	removeHooks : function() {
+		this._destroyCorners();
+	},
+
+	_initCorners : function() {
+		if (!this._rectangle._map)
+			return;
+		if (!!this._cornersGroup)
+			return;
+		var latLngs = this._rectangle._latlngs;
+		this._cornersGroup = new L.LayerGroup();
+		this._cornerMarkers = {
+			southWest : new L.Marker(latLngs[0], {
+				draggable : true
+			}),
+			northWest : new L.Marker(latLngs[1], {
+				draggable : true
+			}),
+			northEast : new L.Marker(latLngs[2], {
+				draggable : true
+			}),
+			southEast : new L.Marker(latLngs[3], {
+				draggable : true
+			})
+		};
+		
+		for (var i in this._cornerMarkers) {
+			var cornerMarker = this._cornerMarkers[i];
+			cornerMarker._editRectangle = this;
+			cornerMarker.on('dragstart', this._onCornerDragStart, this);
+			this._cornersGroup.addLayer(cornerMarker);
+		}
+		this._rectangle._map.addLayer(this._cornersGroup);
+		
+	},
+
+	_destroyCornersGroup : function() {
+		for (i in this._cornerMarkers) {
+			var cornerMarker = this._cornerMarkers[i];
+			cornerMarker.off('dragstart', this._onCornerDragStart);
+			this._cornersGroup.removeLayer(cornerMarker);
+		}
+		this._rectangle._map.removeLayer(this._cornersGroup);
+
+		delete this._cornerMarkers;
+		delete this._cornersGroup;
+	},
+
+	_onCornerDragStart : function(event) {
+			var marker = event.target;
+			var latlng = marker.getLatLng();
+			var corners = this._cornerMarkers;
+			console.log("Got dragstart in rectangle.resize: LatLng: " + latlng);
+			
+			marker.on('drag', this._onCornerDragMove, this);
+			marker.on('dragend', this._onCornerDragEnd, this);
+	},
+
+	_onCornerDragMove : function(event) {
+
+		console.log('Got dragmove in rectangle.resize');
+		this._updateRectangle(event);
+	},
+	
+	_updateRectangle: function(event) {
+		var marker = event.target;
+		var newLatlng = marker.getLatLng();
+		var corners = this._cornerMarkers;
+		var latlngs = this._rectangle.getLatLngs();
+		
+		switch(marker){
+			case corners.southWest:
+				latlngs[0] = latlngs[4] = newLatlng;
+				this._rectangle.setLatLngs(latlngs);
+				console.log("southWest");
+				break;
+			case corners.southEast:
+				latlngs[3] = newLatlng;
+				console.log("southEast");
+				break;
+			case corners.northEast:
+				latlngs[2] = newLatlng;
+				console.log("northEast");
+				break;
+			case corners.northWest:
+				latlngs[1] = newLatlng;
+				console.log("northWest");
+				break;
+			default:
+				console.log("error, unknown corner");
+		};
+		this._rectangle.setLatLngs(latlngs);
+	},
+	
+	_onCornerDragEnd : function(event) {
+		var marker = event.target;
+		
+		console.log('Got dragend in rectangle.resize');
+		this._updateRectangle(event);
+		marker.off('drag', this._onCornerDragMove, this);
+		marker.off('dragend', this._onCornerDragEnd, this);
+	},
+
 });
+
+L.Rectangle.prototype._preResize = {
+		initialize:  L.Rectangle.prototype.initialize,
+		onAdd     :  L.Rectangle.prototype.onAdd,
+		onRemove  :  L.Rectangle.prototype.onRemove
+};
+
+L.Rectangle.prototype.initialize = function (latlngBounds, options){
+	L.Rectangle.prototype._preResize.initialize.call(this, latlngBounds, options);
+	
+	this.resizing = new L.Rectangle.Resize(this);
+};
+
+L.Rectangle.prototype.onAdd = function(map) {
+	L.Rectangle.prototype._preResize.onAdd.call(this, map);
+	if (this.options.resizable && !this.options.editable) {
+		this.resizing.addHooks();
+	}
+};
+
+L.Rectangle.prototype.onRemove = function(map) {
+	if(this.options.resizable && !this.options.editable) {
+		this.resizing.removeHooks();
+	}
+	L.Rectangle.prototype._preResize.onRemove.call(this, map);
+};
+
+
