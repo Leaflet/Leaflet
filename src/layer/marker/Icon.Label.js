99b75b4df97d61b3632ddadb9392013acadeb453
jacobtoye <jacob.toye@gmail.com>
2012-03-13 09:55:36 +1300

    Fixing Icon.Label to work with the new Icon class

--- src/layer/marker/Icon.Label.js
+++ src/layer/marker/Icon.Label.js
@@ -1,29 +1,25 @@
 L.Icon.Label = L.Icon.extend({
-	//user specified css class
-	iconLabelClass: '',
-	//label's text component, if this is null the element will not be created
-	labelText: null,
+	options: {
+		/*
+		labelAnchor: (Point) (top left position of the label within the wrapper, default is right)
+		wrapperAnchor: (Point) (position of icon and label relative to Lat/Lng)
+		iconAnchor: (Point) (top left position of icon within wrapper)
+		labelText: (String) (label's text component, if this is null the element will not be created)
+		*/
+		/* Icon options:
+		iconUrl: (String) (required)
+		iconSize: (Point) (can be set through CSS)
+		iconAnchor: (Point) (centered by default if size is specified, can be set in CSS with negative margins)
+		popupAnchor: (Point) (if not specified, popup opens in the anchor point)
+		shadowUrl: (Point) (no shadow by default)
+		shadowSize: (Point)
+		*/	
+		labelClassName: ''
+	},
 	
-	//This is now the top left position of the icon within the wrapper.
-	//If the label height is greater than the icon you will need to set the y value.
-	//y = (label height - icon height) / 2
-	iconAnchor: new L.Point(0, 0),
-	
-	//This is the top left position of the label within the wrapper. By default it will display at the right
-	//middle position of the default icon. x = width of icon + padding
-	//If the icon height is greater than the label height you will need to set the y value.
-	//y = (icon height - label height) / 2
-	labelAnchor: new L.Point(29, 8),
-	
-	//This is the position of the wrapper div. Use this to position icon + label relative to the Lat/Lng.
-	//By default the point of the default icon is anchor
-	wrapperAnchor: new L.Point(13, 41),
-
-	initialize: function (labelText, iconUrl) {
-		L.Icon.prototype.initialize.call(this, iconUrl);
-		if (labelText) {
-			this.labelText = labelText;
-		}
+	initialize: function (options) {
+		L.Util.setOptions(this, options);
+		L.Icon.prototype.initialize.call(this, this.options);
 	},
 
 	createIcon: function () {
@@ -34,41 +30,71 @@ L.Icon.Label = L.Icon.extend({
 		var shadow = L.Icon.prototype._createIcon.call(this, 'shadow');
 		//need to reposition the shadow
 		if (shadow) {
-			shadow.style.marginLeft = (-this.wrapperAnchor.x) + 'px';
-			shadow.style.marginTop = (-this.wrapperAnchor.y) + 'px';
+			shadow.style.marginLeft = (-this.options.wrapperAnchor.x) + 'px';
+			shadow.style.marginTop = (-this.options.wrapperAnchor.y) + 'px';
 		}
 		return shadow;
 	},
 
 	_createLabel: function (img) {
-		if (!this.labelText) {
+		if (!this.options.labelText) {
 			return img;
 		}
 
 		var wrapper = document.createElement('div'),
 			label = document.createElement('span');
 
-		label.className = 'leaflet-marker-iconlabel ' + this.iconLabelClass;
+		label.className = 'leaflet-marker-iconlabel ' + this.options.labelClassName;
 
-		label.innerHTML = this.labelText;
+		label.innerHTML = this.options.labelText;
 
 		//set up label's styles
-		label.style.marginLeft = this.labelAnchor.x + 'px';
-		label.style.marginTop = this.labelAnchor.y + 'px';
+		label.style.marginLeft = this.options.labelAnchor.x + 'px';
+		label.style.marginTop = this.options.labelAnchor.y + 'px';
 		
 		//set up wrapper anchor
-		wrapper.style.marginLeft = (-this.wrapperAnchor.x) + 'px';
-		wrapper.style.marginTop = (-this.wrapperAnchor.y) + 'px';
+		wrapper.style.marginLeft = (-this.options.wrapperAnchor.x) + 'px';
+		wrapper.style.marginTop = (-this.options.wrapperAnchor.y) + 'px';
 
 		wrapper.className = 'leaflet-marker-icon-wrapper';
 		
 		//reset icons margins (as super makes them -ve)
-		img.style.marginLeft = this.iconAnchor.x + 'px';
-		img.style.marginTop = this.iconAnchor.y + 'px';
+		img.style.marginLeft = this.options.iconAnchor.x + 'px';
+		img.style.marginTop = this.options.iconAnchor.y + 'px';
 		
 		wrapper.appendChild(img);
 		wrapper.appendChild(label);
 
 		return wrapper;
+	}
+});
+
+L.Icon.Label.Default = L.Icon.Label.extend({
+	options: {
+		//This is the top left position of the label within the wrapper. By default it will display at the right
+		//middle position of the default icon. x = width of icon + padding
+		//If the icon height is greater than the label height you will need to set the y value.
+		//y = (icon height - label height) / 2
+		labelAnchor: new L.Point(29, 8),
+		
+		//This is the position of the wrapper div. Use this to position icon + label relative to the Lat/Lng.
+		//By default the point of the default icon is anchor
+		wrapperAnchor: new L.Point(13, 41),
+		
+		//This is now the top left position of the icon within the wrapper.
+		//If the label height is greater than the icon you will need to set the y value.
+		//y = (label height - icon height) / 2
+		iconAnchor: new L.Point(0, 0),
+		
+		//label's text component, if this is null the element will not be created
+		labelText: null,
+		
+		/* From L.Icon.Default */
+		iconUrl: L.ROOT_URL + 'images/marker.png',
+		iconSize: new L.Point(25, 41),
+		popupAnchor: new L.Point(0, -33),
+
+		shadowUrl: L.ROOT_URL + 'images/marker-shadow.png',
+		shadowSize: new L.Point(41, 41)
 	}
 });
