8da6542931639ac876ffd76139704861cce204a9
cvisto <cvisto@yandex.ru>
2012-03-28 19:21:41 +0400

    changes in Polyline.closestLayerPoint: to know what segment of the path is the closest to passed point-parameter

--- src/layer/vector/Polyline.js
+++ src/layer/vector/Polyline.js
@@ -68,6 +68,8 @@ L.Polyline = L.Path.extend({
 				if (point._sqDist < minDistance) {
 					minDistance = point._sqDist;
 					minPoint = point;
+                    minPoint.startSegIndex = i-1;
+                    minPoint.endSegIndex = i;
 				}
 			}
 		}
