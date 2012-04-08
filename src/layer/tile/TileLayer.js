a8fa869a41cc286cd977fb490bb0d285f44993da
Mithgol the Webmaster <getgit@mithgol.ru>
2012-04-08 05:10:32 +0400

    includes fixes provided by Max Ogden

--- src/layer/tile/TileLayer.js
+++ src/layer/tile/TileLayer.js
@@ -29,14 +29,13 @@ L.TileLayer = L.Class.extend({
 		L.Util.setOptions(this, options);
 
 		// detecting retina displays, adjusting tileSize and zoom levels
-		if (this.options.detectRetina
-			&& window.devicePixelRatio > 1
-			&& options.maxZoom > 0
-		){
-			this.options.tileSize >>= 1;
-			options.zoomOffset++;
-			if (options.minZoom > 0) options.minZoom--;
-			options.maxZoom--;
+		if (this.options.detectRetina && window.devicePixelRatio > 1 && this.options.maxZoom > 0) {
+			this.options.tileSize = Math.floor(this.options.tileSize / 2);
+			this.options.zoomOffset++;
+			if (this.options.minZoom > 0) {
+			   this.options.minZoom--;
+			}
+			this.options.maxZoom--;
 		}
 
 		this._url = url;
