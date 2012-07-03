c3e58256d5af1b6ebf58ea03ace0c5aefddf17e8
Dylan Mikus <dbmikus@gmail.com>
2012-07-03 10:06:17 -0400

    Extracted Leaflet Icon styling into inner helper function to reduce code duplication.

--- src/layer/marker/Icon.js
+++ src/layer/marker/Icon.js
@@ -34,17 +34,14 @@ L.Icon = L.Class.extend({
 		return img;
 	},
 
+
 	_setIconStyles: function (img, name) {
 		var options = this.options,
 			anchor = options.iconAnchor,
 			popupAnchor = options.popupAnchor;
 
-		/* If size is defined, use defined value. Otherwise, set size equal to
-		 * the actual size of the imageUrl upon image load */
-		var size;
-		/* Size is defined */
-		if (options[name + 'Size']) {
-			size = options[name + 'Size'];
+		/* Automatically configures other values based on the size supplied */
+		var styleHelper = function (img, size) {
 			if (!anchor && size) {
 				anchor = new L.Point(Math.round(size.x / 2), size.y);
 			}
@@ -63,27 +60,21 @@ L.Icon = L.Class.extend({
 				img.style.marginLeft = (-anchor.x) + 'px';
 				img.style.marginTop	 = (-anchor.y) + 'px';
 			}
+		};
+
+		/* If size is defined, use defined value. Otherwise, set size equal to
+		 * the actual size of the imageUrl upon image load */
+		var size;
+		/* Size is defined */
+		if (options[name + 'Size']) {
+			size = options[name + 'Size'];
+			styleHelper(img, size);
 		}
 		/* Otherwise, size is not defined. Load in size from image */
 		else {
 			img.onload = function () {
 				size = new L.Point(this.width, this.height);
-				if (!anchor && size) {
-					anchor = new L.Point(Math.round(size.x / 2), size.y);
-				}
-				if (name === 'icon' && !popupAnchor && size) {
-					popupAnchor = new L.Point(0, -Math.round((size.y * 8) / 10));
-				}
-				if (name === 'shadow' && anchor && options.shadowOffset) {
-					anchor._add(options.shadowOffset);
-				}
-
-				this.className = 'leaflet-marker-' + name + ' ' + options.className + ' leaflet-zoom-animated';
-
-				if (anchor) {
-					this.style.marginLeft = (-anchor.x) + 'px';
-					this.style.marginTop  = (-anchor.y) + 'px';
-				}
+				styleHelper(this, size);
 			};
 		}
 	},
