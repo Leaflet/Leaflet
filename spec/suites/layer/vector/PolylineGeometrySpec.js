111dc1a8c3ced55dec17d5a8d928bc6380ab98bc
jacobtoye <jacob.toye@gmail.com>
2012-07-16 13:06:27 +1200

    Adding tests for polyline intersection

--- spec/suites/layer/vector/PolylineGeometrySpec.js
+++ spec/suites/layer/vector/PolylineGeometrySpec.js
@@ -32,4 +32,43 @@ describe('PolylineGeometry', function() {
 			expect(point.distance).toBeLessThan(point2.distance);
 		});
 	});
+
+	describe("Polyline Intersection", function () {
+		var latlngs = [
+				L.latLng(-37.7760, 175.2687),
+				L.latLng(-37.7664, 175.2786),
+				L.latLng(-37.7768, 175.2897),
+				L.latLng(-37.7736, 175.2797)
+			],
+			intersectlatLng1 = L.latLng(-37.7670, 175.2667),
+			intersectlatLng2 = L.latLng(-37.7690, 175.2978),
+			polyline = new L.Polyline([], {
+				'noClip': true
+			});
+		map.addLayer(polyline);
+		// Set up the map to work with above coords
+		map.setView(new L.LatLng(-37.7872, 175.2797), 16);
+
+		describe("#newLatLngIntersects", function () {
+			it("should not intersect when only one line segment", function () {
+				polyline.setLatLngs(latlngs.slice(0, 2));
+				expect(polyline.newLatLngIntersects(latlngs[2])).toBe(false);
+			});
+
+			it("should intersect first line segment", function () {
+				polyline.setLatLngs(latlngs.slice(0, 3));
+				expect(polyline.newLatLngIntersects(intersectlatLng1)).toBe(true);
+			});
+
+			it("should not intersect when add non-intersecting latlng", function () {
+				polyline.setLatLngs(latlngs.slice(0, 3));
+				expect(polyline.newLatLngIntersects(latlngs[3])).toBe(false);
+			});
+
+			it("should intersect second line segment", function () {
+				polyline.setLatLngs(latlngs.slice(0, 4));
+				expect(polyline.newLatLngIntersects(intersectlatLng2)).toBe(true);
+			});
+		});
+	});
 });
