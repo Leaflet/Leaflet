f3b3779cb8c051f105718fdf8522128a034fc3b2
Kenneth Gulbrandsøy <kenneth.gulbrandsoy@gmail.com>
2012-08-26 18:25:49 +0200

    Added support for different CRS for each L.TileLayer.WMS instance (code standard)

--- dist/leaflet-src.js
+++ dist/leaflet-src.js
@@ -2471,9 +2471,13 @@ L.TileLayer.WMS = L.TileLayer.extend({
 	},
 
 	onAdd: function (map) {
+		
 		var crs = (typeof this.options.crs === 'undefined' ? map.options.crs.code : this.options.crs);
+		
 		var projectionKey = parseFloat(this.wmsParams.version) >= 1.3 ? 'crs' : 'srs';
+		
 		delete this.wmsParams.crs;
+		
 		this.wmsParams[projectionKey] = crs.code;
 
 		L.TileLayer.prototype.onAdd.call(this, map);
