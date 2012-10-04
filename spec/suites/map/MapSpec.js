fbf91fef546125bd4950937fa04ad1bf0f5dc955
John Firebaugh <john.firebaugh@gmail.com>
2012-10-04 14:02:46 -0700

    Ensure that getBounds can be called during the first moveend event
    Fixes #1027

--- spec/suites/map/MapSpec.js
+++ spec/suites/map/MapSpec.js
@@ -1,1 +1,14 @@
-describe("Map", noSpecs);
+describe("Map", function () {
+	describe("#getBounds", function () {
+		it("is safe to call from within a moveend callback during initial load (#1027)", function () {
+			var c = document.createElement('div'),
+				map = L.map(c);
+
+			map.on("moveend", function () {
+			  map.getBounds();
+			});
+
+			map.setView([51.505, -0.09], 13);
+		});
+	});
+});
