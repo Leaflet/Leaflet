6047273801ca9f92b3f5f6ab07bfd09c151d458b
John Firebaugh <john.firebaugh@gmail.com>
2012-10-06 11:01:17 -0700

    Test the right class

--- spec/suites/geo/LatLngBoundsSpec.js
+++ spec/suites/geo/LatLngBoundsSpec.js
@@ -2,10 +2,10 @@ describe('LatLngBounds', function() {
 	var a, c;
 	
 	beforeEach(function() {
-		a = new L.Bounds(
-			new L.Point(14, 12),
-			new L.Point(30, 40));
-		c = new L.Bounds();
+		a = new L.LatLngBounds(
+			new L.LatLng(14, 12),
+			new L.LatLng(30, 40));
+		c = new L.LatLngBounds();
 	});
 	
 	describe('#isValid', function() {
