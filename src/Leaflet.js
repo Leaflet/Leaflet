bf920b47c9b8666285749cd2aa021dd4b3683151
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-10-10 09:41:45 -0400

    added loader

--- src/Leaflet.js
+++ src/Leaflet.js
@@ -15,3 +15,26 @@ if (typeof exports !== undefined + '') {
 }
 
 L.version = '0.4.4';
+function loadCSS(){
+var browser =(function(){
+//lovingly taken from the rhinosourus book of javascript which took it from jquery
+var match=  /(msie)[ \/]([\w.]+)/.exec(navigator.userAgent.toLowerCase())||[];
+return (match[1]==="msie"&& parseInt(match[2])<=8);
+}());
+
+
+     var head = document.getElementsByTagName("head")[0];
+     head.appendChild(makeLink("http://leaflet.cloudmade.com/dist/leaflet.css"));
+     if(browser.name==="msie"&&parseInt(browser.version)<=7){
+         head.appendChild(makeLink("http://leaflet.cloudmade.com/dist/leaflet.ie.css"));
+     }
+
+
+function makeLink(url){
+    var linkel = document.createElement("link");
+    linkel.rel="stylesheet";
+    linkel.href=url;
+    return linkel;
+}
+}
+loadCSS();
