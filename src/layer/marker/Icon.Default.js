073d568f48ed3525e4cc9e28ee0c811cc21d312d
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-10-12 07:31:28 -0400

    and then we set Icon to just reference the css image path instead of looking for it again

--- src/layer/marker/Icon.Default.js
+++ src/layer/marker/Icon.Default.js
@@ -26,18 +26,4 @@ L.Icon.Default = L.Icon.extend({
 	}
 });
 
-L.Icon.Default.imagePath = (function () {
-	var scripts = document.getElementsByTagName('script'),
-	    leafletRe = /\/?leaflet[\-\._]?([\w\-\._]*)\.js\??/;
-
-	var i, len, src, matches;
-
-	for (i = 0, len = scripts.length; i < len; i++) {
-		src = scripts[i].src;
-		matches = src.match(leafletRe);
-
-		if (matches) {
-			return src.split(leafletRe)[0] + '/images';
-		}
-	}
-}());
+L.Icon.Default.imagePath = L.CSS.imagePath;
