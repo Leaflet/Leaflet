5d6013e7ca21fe0e2fa48d079954f598bebf0c8a
Guillaume Dugas <dugas.guillaume@gmail.com>
2012-10-20 17:31:44 +0200

    Updating handler positionning

--- src/layer/vector/Circle.Drag.js
+++ src/layer/vector/Circle.Drag.js
@@ -3,59 +3,59 @@
  */
 
 L.Handler.CircleDrag = L.Handler.extend({
-	options: {
-		icon: new L.DivIcon({
-			iconSize: new L.Point(8, 8),
-			className: 'leaflet-div-icon leaflet-editing-icon'
-		})
-	},
-	
-	initialize: function (circle, options) {
-		this._circle = circle;
-		L.Util.setOptions(this, options);
-	},
+    options: {
+        icon: new L.DivIcon({
+            iconSize: new L.Point(8, 8),
+            className: 'leaflet-div-icon leaflet-editing-icon'
+        })
+    },
+    
+    initialize: function (circle, options) {
+        this._circle = circle;
+        L.Util.setOptions(this, options);
+    },
 
-	addHooks: function () {
-		var icon = this.options.icon;
-		if (this._circle._map) {
-			this._dragHandler = new L.Marker(this._circle.getLatLng(), {
-					icon: this.options.icon,
-					draggable: true
-				});
-			this._dragHandler
-				.on('dragstart', this._onDragStart, this)
-				.on('drag', this._onDrag, this)
-				.on('dragend', this._onDragEnd, this);
-			
-			this._markerGroup = new L.LayerGroup();
-			this._markerGroup.addLayer(this._dragHandler);
-			this._circle._map.addLayer(this._markerGroup);
-		}
-	},
-	
-	removeHooks: function () {
-		if (this._circle._map) {
-			this._circle._map.removeLayer(this._markerGroup);
-			delete this._markerGroup;
-		}
-	},
-	
-	_onDragStart: function (e) {
-		this._circle
-			.fire('movestart')
-			.fire('dragstart');
-	},
+    addHooks: function () {
+        var icon = this.options.icon;
+        if (this._circle._map) {
+            this._dragHandler = new L.Marker(this._circle.getLatLng(), {
+                    icon: this.options.icon,
+                    draggable: true
+                });
+            this._dragHandler
+                .on('dragstart', this._onDragStart, this)
+                .on('drag', this._onDrag, this)
+                .on('dragend', this._onDragEnd, this);
+            
+            this._markerGroup = new L.LayerGroup();
+            this._markerGroup.addLayer(this._dragHandler);
+            this._circle._map.addLayer(this._markerGroup);
+        }
+    },
+    
+    removeHooks: function () {
+        if (this._circle._map) {
+            this._circle._map.removeLayer(this._markerGroup);
+            delete this._markerGroup;
+        }
+    },
+    
+    _onDragStart: function (e) {
+        this._circle
+            .fire('movestart')
+            .fire('dragstart');
+    },
 
-	_onDrag: function (e) {
-		this._circle.setLatLng(e.target.getLatLng());
-		this._circle
-			.fire('move')
-			.fire('drag');
-	},
+    _onDrag: function (e) {
+        this._circle.setLatLng(e.target.getLatLng());
+        this._circle
+            .fire('move')
+            .fire('drag');
+    },
 
-	_onDragEnd: function () {
-		this._circle
-			.fire('moveend')
-			.fire('dragend');
-	}
+    _onDragEnd: function () {
+        this._circle
+            .fire('moveend')
+            .fire('dragend');
+    }
 });
--- src/layer/vector/Circle.Resize.js
+++ src/layer/vector/Circle.Resize.js
@@ -3,65 +3,95 @@
  */
 
 L.Handler.CircleResize = L.Handler.extend({
-	options: {
-		icon: new L.DivIcon({
-			iconSize: new L.Point(8, 8),
-			className: 'leaflet-div-icon leaflet-editing-icon'
-		})
-	},
-	
-	initialize: function (circle, options) {
-		this._circle = circle;
-		L.Util.setOptions(this, options);
-	},
+    options: {
+        icon: new L.DivIcon({
+            iconSize: new L.Point(8, 8),
+            className: 'leaflet-div-icon leaflet-editing-icon'
+        })
+    },
+    
+    initialize: function (circle, options) {
+        this._circle = circle;
+        L.Util.setOptions(this, options);
+    },
 
-	addHooks: function () {
-		var icon = this.options.icon;
-		if (this._circle._map) {
-			
-			// define handler (icon and position)
-			var bounds = this._circle.getBounds();
-			this._dragHandler = new L.Marker(bounds.getNorthEast(), {
-					icon: this.options.icon,
-					draggable: true
-				});
-			
-			// define handler events
-			this._dragHandler
-				.on('dragstart', this._onDragStart, this)
-				.on('drag', this._onDrag, this)
-				.on('dragend', this._onDragEnd, this);
-			
-			// display handler
-			this._markerGroup = new L.LayerGroup();
-			this._markerGroup.addLayer(this._dragHandler);
-			this._circle._map.addLayer(this._markerGroup);
-			
-			this._circle.on('drag', this._updateHandler, this);
-		}
-	},
-	
-	removeHooks: function () {
-		if (this._circle._map) {
-			this._circle.off('drag', this._updateHandler, this);
-			this._circle._map.removeLayer(this._markerGroup);
-			delete this._markerGroup;
-		}
-	},
-	
-	_updateHandler: function (e) {
-		var bounds = this._circle.getBounds();
-		this._dragHandler.setLatLng(bounds.getNorthEast());
-	},
-	
-	_onDragStart: function (e) { },
-
-	_onDrag: function (e) {
-		var circleCenter = this._circle.getLatLng(),
-			handlerPos = e.target.getLatLng();
-		this._circle.setRadius(circleCenter.distanceTo(handlerPos));
-		this._circle.fire('resize');
-	},
-	
-	_onDragEnd: function () { }
+    addHooks: function () {
+        if (this._circle._map) {
+            
+            // display handler
+            this._markerGroup = new L.LayerGroup();
+            this._circle._map.addLayer(this._markerGroup);
+            this._initHandler();
+        }
+    },
+    
+    removeHooks: function () {
+        if (this._circle._map) {
+            this._circle.off('drag', this._updateHandler, this);
+            this._circle._map.removeLayer(this._markerGroup);
+            delete this._markerGroup;
+            delete this._origins;
+        }
+    },
+    
+    _initHandler: function () {
+        // define handler (icon and position)
+        var icon = this.options.icon,
+            bounds = this._circle.getBounds(),
+            handlerll = new L.LatLng(this._circle.getLatLng().lat, bounds.getNorthEast().lng);
+            
+        this._dragHandler = new L.Marker(handlerll, {
+                icon: icon,
+                draggable: true
+            });
+        
+        // define handler events
+        this._dragHandler
+            .on('dragstart', this._onDragStart, this)
+            .on('drag', this._onDrag, this)
+            .on('dragend', this._onDragEnd, this);
+        
+        this._circle.on('drag', this._updateHandler, this);
+        this._markerGroup.addLayer(this._dragHandler);
+        this._setOrigins();
+    },
+    
+    _updateHandler: function (e) {
+        var originc = this._origins.circle,
+            originh = this._origins.handler,
+            newc = this._circle._map.project(this._circle.getLatLng()),
+            delta = {
+                x: newc.x - originc.x,
+                y: newc.y - originc.y
+            };
+        
+        var hpoint = new L.Point(originh.x + delta.x, originh.y + delta.y);
+        this._dragHandler.setLatLng(this._circle._map.unproject(hpoint));
+    },
+    
+    _onDragStart: function (e) {
+    },
+    
+    _onDrag: function (e) {
+        var circleCenter = this._circle.getLatLng(),
+            handlerPos = e.target.getLatLng();
+        this._circle.setRadius(circleCenter.distanceTo(handlerPos));
+        this._circle.fire('resize');
+    },
+    
+    _onDragEnd: function () {
+        this._setOrigins();
+    },
+    
+    _setOrigins: function () {
+        // memorize circle and handler origins
+        if (typeof(this._circle._map) !== 'undefined' && this._circle._map instanceof L.Map) {
+            var map = this._circle._map;
+            this._origins = {
+                circle: map.project(this._circle.getLatLng()),
+                handler: map.project(this._dragHandler.getLatLng())
+            };
+        }
+    }
+    
 });
