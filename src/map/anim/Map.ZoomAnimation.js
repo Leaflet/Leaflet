babbbe829ea22494598735a365b1812a113f58f1
danzel <danzel@localhost.geek.nz>
2012-11-30 11:27:12 +1300

    Re-order zoom end so we completely stop the zoom before getting people to redraw. Otherwise firefox will animate the canvas redrawing which is totally weird. Fixes #1187

--- src/map/anim/Map.ZoomAnimation.js
+++ src/map/anim/Map.ZoomAnimation.js
@@ -139,11 +139,11 @@ L.Map.include(!L.DomUtil.TRANSITION ? {} : {
 
 	_onZoomTransitionEnd: function () {
 		this._restoreTileFront();
-		L.Util.falseFn(this._tileBg.offsetWidth); // force reflow
-		this._resetView(this._animateToCenter, this._animateToZoom, true, true);
 
 		L.DomUtil.removeClass(this._mapPane, 'leaflet-zoom-anim');
+		L.Util.falseFn(this._tileBg.offsetWidth); // force reflow
 		this._animatingZoom = false;
+		this._resetView(this._animateToCenter, this._animateToZoom, true, true);
 
 		if (L.Draggable) {
 			L.Draggable._disabled = false;
