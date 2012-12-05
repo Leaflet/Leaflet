6f39fef9eba1ddf96434aa065ec8fed2ca91a08c
Bryan McBride <mcbride.bryan@gmail.com>
2012-12-05 17:15:23 -0500

    Workaround to fix #1200

--- src/layer/Popup.js
+++ src/layer/Popup.js
@@ -116,7 +116,6 @@ L.Popup = L.Class.extend({
 		if (this.options.closeButton) {
 			closeButton = this._closeButton =
 			        L.DomUtil.create('a', prefix + '-close-button', container);
-			closeButton.href = '#close';
 			closeButton.innerHTML = '&#215;';
 
 			L.DomEvent.on(closeButton, 'click', this._onCloseButtonClick, this);
