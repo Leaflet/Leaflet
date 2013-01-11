4ebfeb9d18346e1b2410cd3b136db72b3bde6e8f
Vladimir Agafonkin <agafonkin@gmail.com>
2013-01-11 18:50:21 +0200

    udpate copyright to be more precise

--- src/copyright.js
+++ src/copyright.js
@@ -1,4 +1,4 @@
 /*
  Leaflet, a JavaScript library for mobile-friendly interactive maps. http://leafletjs.com
- (c) 2010-2012, CloudMade, Vladimir Agafonkin
+ (c) 2010-2013, Vladimir Agafonkin, CloudMade
 */
