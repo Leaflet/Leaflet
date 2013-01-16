88f1e121eea6772955fdc01629ef35ba49cd48a6
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-01-16 18:20:20 -0500

    overlay events

--- src/control/Control.Layers.js
+++ src/control/Control.Layers.js
@@ -217,9 +217,12 @@ L.Control.Layers = L.Control.extend({
 				this._map.addLayer(obj.layer);
 				if (!obj.overlay) {
 					baseLayer = obj.layer;
+				} else {
+					this._map.fire('overlaylayeradd', {layer: obj});
 				}
 			} else if (!input.checked && this._map.hasLayer(obj.layer)) {
 				this._map.removeLayer(obj.layer);
+				this._map.fire('overlaylayerremoved', {layer: obj});
 			}
 		}
 
