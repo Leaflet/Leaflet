297d746a463dc7b42be08b5fcec4e5e14de0cb09
Vladimir Agafonkin <agafonkin@gmail.com>
2013-02-04 14:30:49 +0200

    make test task less verbose

--- spec/testacular.conf.js
+++ spec/testacular.conf.js
@@ -39,7 +39,7 @@ colors = true;
 
 // level of logging
 // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+logLevel = LOG_WARN;
 
 // enable / disable watching file and executing tests whenever any file changes
 autoWatch = false;
