ed8dc9f4cdb01ef06282475f7ed471d99cbcd4f1
Vladimir Agafonkin <agafonkin@gmail.com>
2013-02-04 18:31:14 +0200

    improve core specs #1347

--- spec/suites/core/EventsSpec.js
+++ spec/suites/core/EventsSpec.js
@@ -1,14 +1,14 @@
 describe('Events', function() {
 	var Klass;
-	
+
 	beforeEach(function() {
 		Klass = L.Class.extend({
 			includes: L.Mixin.Events
 		});
 	});
-	
+
 	describe('#fireEvent', function() {
-		
+
 		it('should fire all listeners added through #addEventListener', function() {
 			var obj = new Klass(),
 				spy = jasmine.createSpy(),
@@ -17,22 +17,22 @@ describe('Events', function() {
 				spy4 = jasmine.createSpy(),
 				spy5 = jasmine.createSpy();
 				spy6 = jasmine.createSpy();
-			
+
 			obj.addEventListener('test', spy);
 			obj.addEventListener('test', spy2);
 			obj.addEventListener('other', spy3);
 			obj.addEventListener({ test: spy4, other: spy5 });
 			obj.addEventListener({'test other': spy6 })
-			
+
 			expect(spy).not.toHaveBeenCalled();
 			expect(spy2).not.toHaveBeenCalled();
 			expect(spy3).not.toHaveBeenCalled();
 			expect(spy4).not.toHaveBeenCalled();
 			expect(spy5).not.toHaveBeenCalled();
 			expect(spy6).not.toHaveBeenCalled();
-			
+
 			obj.fireEvent('test');
-			
+
 			expect(spy).toHaveBeenCalled();
 			expect(spy2).toHaveBeenCalled();
 			expect(spy3).not.toHaveBeenCalled();
@@ -41,7 +41,6 @@ describe('Events', function() {
 			expect(spy6).toHaveBeenCalled();
 			expect(spy6.calls.length).toEqual(1);
 		});
-		
 
 		it('should provide event object to listeners and execute them in the right context', function() {
 			var obj = new Klass(),
@@ -49,46 +48,46 @@ describe('Events', function() {
 				obj3 = new Klass(),
 				obj4 = new Klass(),
 				foo = {};
-			
+
 			function listener1(e) {
 				expect(e.type).toEqual('test');
 				expect(e.target).toEqual(obj);
 				expect(this).toEqual(obj);
 				expect(e.baz).toEqual(1);
 			}
-			
+
 			function listener2(e) {
 				expect(e.type).toEqual('test');
 				expect(e.target).toEqual(obj2);
 				expect(this).toEqual(foo);
 				expect(e.baz).toEqual(2);
 			}
-			
+
 			function listener3(e) {
 				expect(e.type).toEqual('test');
 				expect(e.target).toEqual(obj3);
 				expect(this).toEqual(obj3);
 				expect(e.baz).toEqual(3);
 			}
-			
+
 			function listener4(e) {
 				expect(e.type).toEqual('test');
 				expect(e.target).toEqual(obj4);
 				expect(this).toEqual(foo);
 				expect(e.baz).toEqual(4);
 			}
-			
+
 			obj.addEventListener('test', listener1);
 			obj2.addEventListener('test', listener2, foo);
 			obj3.addEventListener({ test: listener3 });
 			obj4.addEventListener({ test: listener4 }, foo);
-			
+
 			obj.fireEvent('test', {baz: 1});
 			obj2.fireEvent('test', {baz: 2});
 			obj3.fireEvent('test', {baz: 3});
 			obj4.fireEvent('test', {baz: 4});
 		});
-		
+
 		it('should not call listeners removed through #removeEventListener', function() {
 			var obj = new Klass(),
 				spy = jasmine.createSpy(),
@@ -96,75 +95,112 @@ describe('Events', function() {
 				spy3 = jasmine.createSpy(),
 				spy4 = jasmine.createSpy(),
 				spy5 = jasmine.createSpy();
-			
+
 			obj.addEventListener('test', spy);
 			obj.removeEventListener('test', spy);
-			
+
 			obj.fireEvent('test');
-			
+
 			expect(spy).not.toHaveBeenCalled();
-			
+
 			obj.addEventListener('test2', spy2);
 			obj.addEventListener('test2', spy3);
 			obj.removeEventListener('test2');
-			
+
 			obj.fireEvent('test2');
-			
+
 			expect(spy2).not.toHaveBeenCalled();
 			expect(spy3).not.toHaveBeenCalled();
-			
+
 			obj.addEventListener('test3', spy4);
 			obj.addEventListener('test4', spy5);
 			obj.removeEventListener({
 				test3: spy4,
 				test4: spy5
 			});
-			
+
 			obj.fireEvent('test3');
 			obj.fireEvent('test4');
-			
+
 			expect(spy4).not.toHaveBeenCalled();
 			expect(spy5).not.toHaveBeenCalled();
 		});
+
+		// added due to context-sensitive removeListener optimization
+		it('should fire multiple listeners with the same context with id properly', function () {
+			var obj = new Klass(),
+				spy = jasmine.createSpy(),
+				spy2 = jasmine.createSpy(),
+				foo = {};
+
+			L.Util.stamp(foo);
+
+			obj.addEventListener('test', spy, foo);
+			obj.addEventListener('test', spy2, foo);
+
+			obj.fireEvent('test');
+
+			expect(spy).toHaveBeenCalled();
+			expect(spy2).toHaveBeenCalled();
+		});
+
+		it('should remove listeners with stamped contexts properly', function () {
+			var obj = new Klass(),
+				spy = jasmine.createSpy(),
+				spy2 = jasmine.createSpy(),
+				foo = {};
+
+			L.Util.stamp(foo);
+
+			obj.addEventListener('test', spy, foo);
+			obj.addEventListener('test', spy2, foo);
+
+			obj.removeEventListener('test', spy, foo);
+
+			obj.fireEvent('test');
+
+			expect(spy).not.toHaveBeenCalled();
+			expect(spy2).toHaveBeenCalled();
+		});
 	});
-	
+
 	describe('#on, #off & #fire', function() {
-		
+
 		it('should work like #addEventListener && #removeEventListener', function() {
 			var obj = new Klass(),
 				spy = jasmine.createSpy();
-			
+
 			obj.on('test', spy);
 			obj.fire('test');
-			
+
 			expect(spy).toHaveBeenCalled();
-			
+
 			obj.off('test', spy);
 			obj.fireEvent('test');
-			
+
 			expect(spy.callCount).toBeLessThan(2);
 		});
-		
+
 		it('should not override existing methods with the same name', function() {
 			var spy1 = jasmine.createSpy(),
 				spy2 = jasmine.createSpy(),
 				spy3 = jasmine.createSpy();
-			
+
 			var Klass2 = L.Class.extend({
 				includes: L.Mixin.Events,
 				on: spy1,
 				off: spy2,
 				fire: spy3
 			});
-			
+
 			var obj = new Klass2();
-			
+
 			obj.on();
 			expect(spy1).toHaveBeenCalled();
-			
+
 			obj.off();
 			expect(spy2).toHaveBeenCalled();
-			
+
 			obj.fire();
 			expect(spy3).toHaveBeenCalled();
 		});
--- spec/suites/core/UtilSpec.js
+++ spec/suites/core/UtilSpec.js
@@ -2,30 +2,30 @@ describe('Util', function() {
 
 	describe('#extend', function() {
 		var a;
-		
+
 		beforeEach(function() {
 			a = {
 				foo: 5,
 				bar: 'asd'
 			};
 		});
-		
+
 		it('should extend the first argument with the properties of the second', function() {
 			L.Util.extend(a, {
 				bar: 7,
 				baz: 3
 			});
-			
+
 			expect(a).toEqual({
 				foo: 5,
 				bar: 7,
 				baz: 3
 			});
 		});
-		
+
 		it('should work with more than 2 arguments', function() {
 			L.Util.extend(a, {bar: 7}, {baz: 3});
-			
+
 			expect(a).toEqual({
 				foo: 5,
 				bar: 7,
@@ -39,25 +39,51 @@ describe('Util', function() {
 			var fn = function() {
 				return this;
 			};
-			
+
 			var fn2 = L.Util.bind(fn, 5);
-			
+
 			expect(fn2()).toEqual(5);
 		});
+
+		it('should pass additional arguments to the bound function', function () {
+			var fn = jasmine.createSpy(),
+				foo = {},
+				a = {},
+				b = {};
+
+			var fn2 = L.Util.bind(fn, foo, a, b);
+
+			fn2();
+
+			expect(fn).toHaveBeenCalledWith(a, b);
+		});
 	});
-	
+
 	describe('#stamp', function() {
 		it('should set a unique id on the given object and return it', function() {
 			var a = {},
 				id = L.Util.stamp(a);
-			
+
 			expect(typeof id).toEqual('number');
 			expect(L.Util.stamp(a)).toEqual(id);
-			
+
 			var b = {},
 				id2 = L.Util.stamp(b);
-			
+
 			expect(id2).not.toEqual(id);
+		});
+	});
+
+	describe('#falseFn', function () {
+		it('should just return false', function () {
+			expect(L.Util.falseFn()).toBe(false);
+		});
+	});
+
+	describe('#formatNum', function () {
+		it('should format numbers with a given precision', function () {
+			expect(L.Util.formatNum(13.12325555, 3)).toEqual(13.123);
+			expect(L.Util.formatNum(13.12325555)).toEqual(13.12326);
 		});
 	});
 
@@ -71,34 +97,106 @@ describe('Util', function() {
 			};
 
 			expect(L.Util.getParamString(a.obj,a.url)).toEqual(a.result);
-			
-			var b = {	
+
+			var b = {
 				url: "http://example.com/get?justone=qs",
 				obj: {bar: 7, baz: 3},
 				result: "&bar=7&baz=3"
 			};
-			
+
 			expect(L.Util.getParamString(b.obj,b.url)).toEqual(b.result);
 
-			var c = {	
+			var c = {
 				url: undefined,
 				obj: {bar: 7, baz: 3},
 				result: "?bar=7&baz=3"
 			};
-			
+
 			expect(L.Util.getParamString(c.obj,c.url)).toEqual(c.result);
 		});
 	});
 
-	// TODO cancel/requestAnimFrame?
+	describe('#requestAnimFrame', function () {
+		it('should call a function on next frame, unless canceled', function () {
+			var spy = jasmine.createSpy(),
+				spy2 = jasmine.createSpy(),
+				called = false,
+				foo = {};
 
-	// TODO limitExecByInterval
+			runs(function () {
+				L.Util.requestAnimFrame(spy);
 
-	// TODO formatNum
+				L.Util.requestAnimFrame(function () {
+					called = true;
+					expect(this).toEqual(foo);
+					spy();
+				}, foo);
 
-	// TODO splitWords
+				L.Util.cancelAnimFrame(spy);
+			});
+
+			waitsFor(function () {
+				return called;
+			}, 'function should be called', 500);
+
+			runs(function () {
+				expect(spy).toHaveBeenCalled();
+				expect(spy2).not.toHaveBeenCalled();
+			});
+		});
+	});
+
+	describe('#limitExecByInterval', function() {
+		it('should limit execution to not more often than specified time interval', function () {
+			var spy = jasmine.createSpy(),
+				check = false;
+
+			var fn = L.Util.limitExecByInterval(spy, 20);
+
+			runs(function () {
+				fn();
+				fn();
+				fn();
+
+				expect(spy.calls.length).toEqual(1);
+
+				setTimeout(function () {
+					check = true;
+				}, 30);
+			});
+
+			waitsFor(function () {
+				return check;
+			});
+
+			runs(function () {
+				expect(spy.calls.length).toEqual(2);
+			});
+		});
+	});
+
+	describe('#splitWords', function () {
+		it('should split words into an array', function () {
+			expect(L.Util.splitWords('foo bar baz')).toEqual(['foo', 'bar', 'baz']);
+		})
+	})
 
 	// TODO setOptions
 
-	// TODO template
+	describe('#template', function () {
+		it('should evaluate templates with a given data object', function () {
+			var tpl = 'Hello {foo} and {bar}!';
+
+			var str = L.Util.template(tpl, {
+				foo: 'Vlad',
+				bar: 'Dave'
+			});
+
+			expect(str).toEqual('Hello Vlad and Dave!');
+
+			expect(function () {
+				L.Util.template(tpl, {foo: 'bar'});
+			}).toThrow();
+		});
+	});
 });
