3afee7eb4908971f3db9f7a3d9ff5416be8fb519
danzel <danzel@localhost.geek.nz>
2013-02-20 11:03:13 +1300

    Allow overwriting the given array, for usage by spliceLatLngs.

--- src/layer/vector/Polyline.js
+++ src/layer/vector/Polyline.js
@@ -47,7 +47,7 @@ L.Polyline = L.Path.extend({
 
 	spliceLatLngs: function () { // (Number index, Number howMany)
 		var removed = [].splice.apply(this._latlngs, arguments);
-		this._convertLatLngs(this._latlngs);
+		this._convertLatLngs(this._latlngs, true);
 		this.redraw();
 		return removed;
 	},
@@ -85,8 +85,8 @@ L.Polyline = L.Path.extend({
 		return bounds;
 	},
 
-	_convertLatLngs: function (latlngs) {
-		var i, len, target = [];
+	_convertLatLngs: function (latlngs, overwrite) {
+		var i, len, target = overwrite ? latlngs : [];
 
 		for (i = 0, len = latlngs.length; i < len; i++) {
 			if (L.Util.isArray(latlngs[i]) && typeof latlngs[i][0] !== 'number') {
