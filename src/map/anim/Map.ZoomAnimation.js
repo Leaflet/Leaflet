d856e8814e85e5c5e370faf8ccf3450e23c4f8a2
Vladimir Agafonkin <agafonkin@gmail.com>
2013-04-19 12:47:55 +0300

    don't animate if zoom difference is too large, close #1377
    also adds zoomAnimationThreshold map option (defaults to 4)

--- src/map/anim/Map.ZoomAnimation.js
+++ src/map/anim/Map.ZoomAnimation.js
@@ -3,7 +3,8 @@
  */
 
 L.Map.mergeOptions({
-	zoomAnimation: L.DomUtil.TRANSITION && !L.Browser.android23 && !L.Browser.mobileOpera
+	zoomAnimation: L.DomUtil.TRANSITION && !L.Browser.android23 && !L.Browser.mobileOpera,
+	zoomAnimationThreshold: 4
 });
 
 if (L.DomUtil.TRANSITION) {
@@ -27,6 +28,9 @@ L.Map.include(!L.DomUtil.TRANSITION ? {} : {
 	_animateZoomIfClose: function (center, zoom) {
 
 		if (this._animatingZoom) { return true; }
+
+		// don't animate if zoom difference is too large
+		if (Math.abs(zoom - this._zoom) > this.options.zoomAnimationThreshold) { return false; }
 
 		// offset is the pixel coords of the zoom origin relative to the current center
 		var scale = this.getZoomScale(zoom),
