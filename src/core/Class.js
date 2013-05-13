24fd67c0f6be3bc23936b452f29afd7316aeb333
Felixls <sergiols@fibertel.com.ar>
2013-05-13 16:44:23 -0300

    Update Class.js
    Compiler error:
    
    src/core/Class.js       line 64 col 14   Identifier '__super__' is not in camel case.
    
    // jshint ...  form doesn't work, it must be
    
    /*jshint ... */

--- src/core/Class.js
+++ src/core/Class.js
@@ -60,7 +60,7 @@ L.Class.extend = function (props) {
 	proto._initHooks = [];
 
 	var parent = this;
-	// jshint camelcase: false
+	/*jshint camelcase: false*/
 	NewClass.__super__ = parent.prototype;
 
 	// add method for calling all hooks
