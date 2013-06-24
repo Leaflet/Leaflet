e7d0fb1b377cee309adfbfcfc5008a1ab716645e
John Firebaugh <john.firebaugh@gmail.com>
2013-06-24 13:52:05 -0400

    Normalize all toGeoJSON output to Features

--- src/layer/GeoJSON.js
+++ src/layer/GeoJSON.js
@@ -33,7 +33,7 @@ L.GeoJSON = L.FeatureGroup.extend({
 		if (options.filter && !options.filter(geojson)) { return; }
 
 		var layer = L.GeoJSON.geometryToLayer(geojson, options.pointToLayer, options.coordsToLatLng);
-		layer.feature = geojson;
+		layer.feature = L.GeoJSON.asFeature(geojson);
 
 		layer.defaultOptions = layer.options;
 		this.resetStyle(layer);
@@ -161,7 +161,19 @@ L.extend(L.GeoJSON, {
 	},
 
 	getFeature: function (layer, newGeometry) {
-		return layer.feature ? L.extend({}, layer.feature, {geometry: newGeometry}) : newGeometry;
+		return layer.feature ? L.extend({}, layer.feature, {geometry: newGeometry}) : L.GeoJSON.asFeature(newGeometry);
+	},
+
+	asFeature: function (geoJSON) {
+		if (geoJSON.type === 'Feature') {
+			return geoJSON;
+		}
+
+		return {
+			type: 'Feature',
+			properties: {},
+			geometry: geoJSON
+		};
 	}
 });
 
@@ -212,7 +224,7 @@ L.Polygon.include({
 				var coords = [];
 
 				this.eachLayer(function (layer) {
-					coords.push(layer.toGeoJSON().coordinates);
+					coords.push(layer.toGeoJSON().geometry.coordinates);
 				});
 
 				return L.GeoJSON.getFeature(this, {
@@ -229,18 +241,18 @@ L.Polygon.include({
 
 L.LayerGroup.include({
 	toGeoJSON: function () {
-		var geoms = [];
+		var features = [];
 
 		this.eachLayer(function (layer) {
 			if (layer.toGeoJSON) {
-				geoms.push(layer.toGeoJSON());
+				features.push(L.GeoJSON.asFeature(layer.toGeoJSON()));
 			}
 		});
 
-		return L.GeoJSON.getFeature(this, {
-			type: 'GeometryCollection',
-			geometries: geoms
-		});
+		return {
+			type: 'FeatureCollection',
+			features: features
+		};
 	}
 });
 
