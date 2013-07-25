6db592b5da62e38ab5b2f12a9bc3315ce18166c0
Vladimir Agafonkin <agafonkin@gmail.com>
2013-07-25 22:43:11 +0300

    bump master version to 0.7-dev, close #1906

--- src/Leaflet.js
+++ src/Leaflet.js
@@ -2,7 +2,7 @@
 var oldL = window.L,
     L = {};
 
-L.version = '0.6.2';
+L.version = '0.7-dev';
 
 // define Leaflet for Node module pattern loaders, including Browserify
 if (typeof module === 'object' && typeof module.exports === 'object') {
