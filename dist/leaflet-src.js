985dd00c89a7748ae2e633f37289a3495da08b87
Vladimir Agafonkin <agafonkin@gmail.com>
2013-07-25 13:15:06 +0300

    update build to 0.6.4

--- dist/leaflet-src.js
+++ dist/leaflet-src.js
@@ -7,7 +7,7 @@
 var oldL = window.L,
     L = {};
 
-L.version = '0.6.3';
+L.version = '0.6.4';
 
 // define Leaflet for Node module pattern loaders, including Browserify
 if (typeof module === 'object' && typeof module.exports === 'object') {
@@ -1844,8 +1844,8 @@ L.Map = L.Class.extend({
 	},
 
 	getMinZoom: function () {
-		var z1 = this._layersMinZoom === undefined ? -Infinity : this._layersMinZoom,
-		    z2 = this._boundsMinZoom === undefined ? -Infinity : this._boundsMinZoom;
+		var z1 = this._layersMinZoom === undefined ? 0 : this._layersMinZoom,
+		    z2 = this._boundsMinZoom === undefined ? 0 : this._boundsMinZoom;
 		return this.options.minZoom === undefined ? Math.max(z1, z2) : this.options.minZoom;
 	},
 
@@ -7499,7 +7499,7 @@ L.Map.Keyboard = L.Handler.extend({
 		var body = document.body,
 		    docEl = document.documentElement,
 		    top = body.scrollTop || docEl.scrollTop,
-		    left = body.scrollTop || docEl.scrollLeft;
+		    left = body.scrollLeft || docEl.scrollLeft;
 
 		this._map._container.focus();
 
