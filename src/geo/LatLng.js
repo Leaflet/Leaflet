16f9c9a3024c95e78add1ad735adae604d707bbf
Seb Emonet <seb@cygnus.(none)>
2013-09-13 00:22:19 +0200

    Accept simple objects in L.latLngBounds(). Fixes #1915.

--- src/geo/LatLng.js
+++ src/geo/LatLng.js
@@ -75,13 +75,20 @@ L.latLng = function (a, b) { // (LatLng) or ([Number, Number]) or (Number, Numbe
 		return a;
 	}
 	if (L.Util.isArray(a)) {
-		return new L.LatLng(a[0], a[1]);
+		if (typeof a[0] === 'number' || typeof a[0] === 'string') {
+			return new L.LatLng(a[0], a[1]);
+		} else {
+			return null;
+		}
 	}
 	if (a === undefined || a === null) {
 		return a;
 	}
 	if (typeof a === 'object' && 'lat' in a) {
 		return new L.LatLng(a.lat, 'lng' in a ? a.lng : a.lon);
+	}
+	if (b === undefined) {
+		return null;
 	}
 	return new L.LatLng(a, b);
 };
