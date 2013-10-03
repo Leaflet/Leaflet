58cf71f3eb491e3ee88751830e2dd93598bb7154
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-10-03 13:08:19 -0400

    Spherical Radius is used in Spherical Mercator

--- src/geo/projection/Projection.SphericalMercator.js
+++ src/geo/projection/Projection.SphericalMercator.js
@@ -4,23 +4,23 @@
 
 L.Projection.SphericalMercator = {
 	MAX_LATITUDE: 85.0511287798,
-
+	R_MAJOR: 6378137,
 	project: function (latlng) { // (LatLng) -> Point
 		var d = L.LatLng.DEG_TO_RAD,
 		    max = this.MAX_LATITUDE,
 		    lat = Math.max(Math.min(max, latlng.lat), -max),
-		    x = latlng.lng * d,
+		    x = latlng.lng * d * this.R_MAJOR,
 		    y = lat * d;
 
 		y = Math.log(Math.tan((Math.PI / 4) + (y / 2)));
-
+		y *= this.R_MAJOR;
 		return new L.Point(x, y);
 	},
 
 	unproject: function (point) { // (Point, Boolean) -> LatLng
 		var d = L.LatLng.RAD_TO_DEG,
-		    lng = point.x * d,
-		    lat = (2 * Math.atan(Math.exp(point.y)) - (Math.PI / 2)) * d;
+		    lng = point.x * d / this.R_MAJOR,
+		    lat = (2 * Math.atan(Math.exp(point.y / this.R_MAJOR)) - (Math.PI / 2)) * d;
 
 		return new L.LatLng(lat, lng);
 	}
