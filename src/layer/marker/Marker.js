2629c181d0bd846ea51085048e19395946cf695f
Vladimir Agafonkin <agafonkin@gmail.com>
2013-10-11 18:44:10 +0300

    fix firefox console warnings when animating markers, close #2090

--- src/layer/marker/Marker.js
+++ src/layer/marker/Marker.js
@@ -207,7 +207,7 @@ L.Marker = L.Class.extend({
 	},
 
 	_animateZoom: function (opt) {
-		var pos = this._map._latLngToNewLayerPoint(this._latlng, opt.zoom, opt.center);
+		var pos = this._map._latLngToNewLayerPoint(this._latlng, opt.zoom, opt.center).round();
 
 		this._setPos(pos);
 	},
