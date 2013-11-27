410fe4db5c01c423f4994d55b3ed5c506b471714
Vladimir Agafonkin <agafonkin@gmail.com>
2013-11-28 00:59:39 +0200

    proper EPSG:4326 with 2x1 root, fixes #1207

--- src/geo/crs/CRS.EPSG4326.js
+++ src/geo/crs/CRS.EPSG4326.js
@@ -6,5 +6,7 @@ L.CRS.EPSG4326 = L.extend({}, L.CRS, {
 	code: 'EPSG:4326',
 
 	projection: L.Projection.LonLat,
-	transformation: new L.Transformation(1 / 360, 0.5, -1 / 360, 0.5)
+	transformation: new L.Transformation(1 / 180, 1, -1 / 180, 0.5),
+
+	wrapLng: true
 });
