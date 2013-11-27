66d8da12c18cd5c1f44c6789da1314d92e4d41f5
Per Liedman <per@liedman.net>
2013-11-27 09:05:08 +0100

    Added tests to verify size of EPSG3857 and 4326

--- spec/suites/geo/CRSSpec.js
+++ spec/suites/geo/CRSSpec.js
@@ -44,4 +44,37 @@ describe("CRS.EPSG3857", function () {
 			expect(crs.pointToLatLng(new L.Point(256, 0), 0)).nearLatLng(L.latLng(85.0511287798, 180));
 		});
 	});
+
+	describe("#getSize", function () {
+		it("gives correct size", function () {
+			var i,
+			    worldSize = 256,
+			    crsSize;
+			for (i = 0; i <= 22; i++) {
+				crsSize = crs.getSize(i);
+				expect(crsSize.x).equal(worldSize);
+				expect(crsSize.y).equal(worldSize);
+				worldSize *= 2;
+			}
+		});
+	});
+});
+
+describe("CRS.EPSG4326", function () {
+	var crs = L.CRS.EPSG4326;
+
+	describe("#getSize", function () {
+		it("gives correct size", function () {
+			var i,
+			    worldSize = 256,
+			    crsSize;
+			for (i = 0; i <= 22; i++) {
+				crsSize = crs.getSize(i);
+				expect(crsSize.x).equal(worldSize);
+				// Lat bounds are half as high (-90/+90 compared to -180/+180)
+				expect(crsSize.y).equal(worldSize / 2);
+				worldSize *= 2;
+			}
+		});
+	});
 });
