a00360bef69c562278a6823587ef138026e5e035
Vladimir Agafonkin <agafonkin@gmail.com>
2013-12-04 14:44:52 +0200

    clean up Control.Scale

--- src/control/Control.Scale.js
+++ src/control/Control.Scale.js
@@ -18,7 +18,7 @@ L.Control.Scale = L.Control.extend({
 		    container = L.DomUtil.create('div', className),
 		    options = this.options;
 
-		this._addScales(options, className, container);
+		this._addScales(options, className + '-line', container);
 
 		map.on(options.updateWhenIdle ? 'moveend' : 'move', this._update, this);
 		map.whenReady(this._update, this);
@@ -32,10 +32,10 @@ L.Control.Scale = L.Control.extend({
 
 	_addScales: function (options, className, container) {
 		if (options.metric) {
-			this._mScale = L.DomUtil.create('div', className + '-line', container);
+			this._mScale = L.DomUtil.create('div', className, container);
 		}
 		if (options.imperial) {
-			this._iScale = L.DomUtil.create('div', className + '-line', container);
+			this._iScale = L.DomUtil.create('div', className, container);
 		}
 	},
 
@@ -43,15 +43,11 @@ L.Control.Scale = L.Control.extend({
 		var bounds = this._map.getBounds(),
 		    centerLat = bounds.getCenter().lat,
 		    halfWorldMeters = 6378137 * Math.PI * Math.cos(centerLat * Math.PI / 180),
-		    dist = halfWorldMeters * (bounds.getNorthEast().lng - bounds.getSouthWest().lng) / 180,
+		    dist = halfWorldMeters * (bounds.getEast() - bounds.getWest()) / 180,
 
 		    size = this._map.getSize(),
 		    options = this.options,
-		    maxMeters = 0;
-
-		if (size.x > 0) {
-			maxMeters = dist * (options.maxWidth / size.x);
-		}
+		    maxMeters = size.x > 0 ? dist * (options.maxWidth / size.x) : 0;
 
 		this._updateScales(options, maxMeters);
 	},
@@ -60,48 +56,46 @@ L.Control.Scale = L.Control.extend({
 		if (options.metric && maxMeters) {
 			this._updateMetric(maxMeters);
 		}
-
 		if (options.imperial && maxMeters) {
 			this._updateImperial(maxMeters);
 		}
 	},
 
 	_updateMetric: function (maxMeters) {
-		var meters = this._getRoundNum(maxMeters);
+		var meters = this._getRoundNum(maxMeters),
+		    label = meters < 1000 ? meters + ' m' : (meters / 1000) + ' km';
 
-		this._mScale.style.width = this._getScaleWidth(meters / maxMeters) + 'px';
-		this._mScale.innerHTML = meters < 1000 ? meters + ' m' : (meters / 1000) + ' km';
+		this._updateScale(this._mScale, label, meters / maxMeters);
 	},
 
 	_updateImperial: function (maxMeters) {
 		var maxFeet = maxMeters * 3.2808399,
-		    scale = this._iScale,
 		    maxMiles, miles, feet;
 
 		if (maxFeet > 5280) {
 			maxMiles = maxFeet / 5280;
 			miles = this._getRoundNum(maxMiles);
-
-			scale.style.width = this._getScaleWidth(miles / maxMiles) + 'px';
-			scale.innerHTML = miles + ' mi';
+			this._updateScale(this._iScale, miles + ' mi', miles / maxMiles);
 
 		} else {
 			feet = this._getRoundNum(maxFeet);
-
-			scale.style.width = this._getScaleWidth(feet / maxFeet) + 'px';
-			scale.innerHTML = feet + ' ft';
+			this._updateScale(this._iScale, feet + ' ft', feet / maxFeet);
 		}
 	},
 
-	_getScaleWidth: function (ratio) {
-		return Math.round(this.options.maxWidth * ratio) - 10;
+	_updateScale: function (scale, text, ratio) {
+		scale.style.width = (Math.round(this.options.maxWidth * ratio) - 10) + 'px';
+		scale.innerHTML = text;
 	},
 
 	_getRoundNum: function (num) {
 		var pow10 = Math.pow(10, (Math.floor(num) + '').length - 1),
 		    d = num / pow10;
 
-		d = d >= 10 ? 10 : d >= 5 ? 5 : d >= 3 ? 3 : d >= 2 ? 2 : 1;
+		d = d >= 10 ? 10 :
+		    d >= 5 ? 5 :
+		    d >= 3 ? 3 :
+		    d >= 2 ? 2 : 1;
 
 		return pow10 * d;
 	}
