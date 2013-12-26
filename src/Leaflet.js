abdf0c2afacfe021277011f56f014d80fae375bc
Vladimir Agafonkin <agafonkin@gmail.com>
2013-12-26 18:45:29 +0200

    better compression for Leaflet.js

--- src/Leaflet.js
+++ src/Leaflet.js
@@ -3,6 +3,17 @@ var L = {
 	version: '0.8-dev'
 };
 
+function expose() {
+	var oldL = window.L;
+
+	L.noConflict = function () {
+		window.L = oldL;
+		return this;
+	};
+
+	window.L = L;
+}
+
 // define Leaflet for Node module pattern loaders, including Browserify
 if (typeof module === 'object' && typeof module.exports === 'object') {
 	module.exports = L;
@@ -13,12 +24,5 @@ if (typeof module === 'object' && typeof module.exports === 'object') {
 
 // define Leaflet as a global L variable, saving the original L to restore later if needed
 } else {
-	var oldL = window.L;
-
-	L.noConflict = function () {
-		window.L = oldL;
-		return this;
-	};
-
-	window.L = L;
+	expose();
 }
