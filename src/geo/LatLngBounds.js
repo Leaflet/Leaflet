ed44604411a074613d72771280e842c6b0908450
Antoine Pultier <antoine.pultier@sintef.no>
2014-01-15 11:37:58 +0100

    L.LatLngBounds.extend optimization

--- src/geo/LatLngBounds.js
+++ src/geo/LatLngBounds.js
@@ -15,30 +15,67 @@ L.LatLngBounds = function (southWest, northEast) { // (LatLng, LatLng) or (LatLn
 L.LatLngBounds.prototype = {
 	// extend the bounds to contain the given point or bounds
 	extend: function (obj) { // (LatLng) or (LatLngBounds)
-		if (!obj) { return this; }
-
-		var latLng = L.latLng(obj);
-		if (latLng !== null) {
-			obj = latLng;
-		} else {
-			obj = L.latLngBounds(obj);
-		}
+		var southWest = this._southWest,
+			northEast = this._northEast;
 
 		if (obj instanceof L.LatLng) {
-			if (!this._southWest && !this._northEast) {
+			if (!southWest && !northEast) {
 				this._southWest = new L.LatLng(obj.lat, obj.lng);
 				this._northEast = new L.LatLng(obj.lat, obj.lng);
 			} else {
-				this._southWest.lat = Math.min(obj.lat, this._southWest.lat);
-				this._southWest.lng = Math.min(obj.lng, this._southWest.lng);
+				if (obj.lat < southWest.lat) {
+					southWest.lat = obj.lat;
+				}
 
-				this._northEast.lat = Math.max(obj.lat, this._northEast.lat);
-				this._northEast.lng = Math.max(obj.lng, this._northEast.lng);
+				if (obj.lng < southWest.lng) {
+					southWest.lng = obj.lng;
+				}
+
+				if (obj.lat > northEast.lat) {
+					northEast.lat = obj.lat;
+				}
+
+				if (obj.lng > northEast.lng) {
+					northEast.lng = obj.lng;
+				}
 			}
+
 		} else if (obj instanceof L.LatLngBounds) {
-			this.extend(obj._southWest);
-			this.extend(obj._northEast);
+			var oSouthWest = obj._southWest,
+				oNorthEast = obj._northEast;
+
+			if (!southWest && !northEast) {
+				this._southWest = new L.LatLng(oSouthWest.lat, oSouthWest.lng);
+				this._northEast = new L.LatLng(oNorthEast.lat, oNorthEast.lng);
+			} else {
+				if (oSouthWest.lat < southWest.lat) {
+					southWest.lat = oSouthWest.lat;
+				}
+
+				if (oSouthWest.lng < southWest.lng) {
+					southWest.lng = oSouthWest.lng;
+				}
+
+				if (oNorthEast.lat > northEast.lat) {
+					northEast.lat = oNorthEast.lat;
+				}
+
+				if (oNorthEast.lng > northEast.lng) {
+					northEast.lng = oNorthEast.lng;
+				}
+			}
+		} else if (obj) {
+			var latLng = L.latLng(obj);
+			if (latLng !== null) {
+				this.extend(latLng);
+			} else {
+				var latLngBounds = L.latLngBounds(obj);
+				if (latLngBounds !== null) {
+					this.extend(latLngBounds);
+				}
+			}
 		}
+
 		return this;
 	},
 
