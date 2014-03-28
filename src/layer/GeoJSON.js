e7b016812b82c96eac0f01f38a041fc9b97b7f2a
Nathan Mascitelli <mascitelli.nathan@gmail.com>
2014-03-28 08:16:30 -0400

    Added return statement to setStyle in GeoJSON so the method returns this as documented.

--- src/layer/GeoJSON.js
+++ src/layer/GeoJSON.js
@@ -53,7 +53,7 @@ L.GeoJSON = L.FeatureGroup.extend({
 	},
 
 	setStyle: function (style) {
-		this.eachLayer(function (layer) {
+		return this.eachLayer(function (layer) {
 			this._setLayerStyle(layer, style);
 		}, this);
 	},
