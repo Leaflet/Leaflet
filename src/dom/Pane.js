9ee2e317980b9495452ddff915646ed32f434fef
Jake Wilson <jake.wilson@gunsightlabs.com>
2014-07-09 21:03:52 -0600

    Added method to get a pane's name. Added method to get a pane's renderer. If a renderer doesn't exist, it will automatically be created. This is used for vector layers.

--- src/dom/Pane.js
+++ src/dom/Pane.js
@@ -6,13 +6,36 @@ L.Pane = L.Class.extend({
 
 	initialize: function (map, name, container) {
 		this._name = name;
+		this._renderer = null;
 
+		// Define the class name for the container
 		var className = 'leaflet-pane' + (name ? ' leaflet-' + name.replace('Pane', '') + '-pane' : '');
+
+		// Create the container
 		this._container = L.DomUtil.create('div', className, container || map._mapPane.getContainer());
+
+		// Shortcut for accessing Pane's container's styles
+		this.style = this._container.style;
+	},
+
+	getName: function() {
+		return this._name;
 	},
 
 	getContainer: function () {
 		return this._container;
+	},
+
+	getRenderer: function (layer) {
+		var renderer = this._renderer;
+
+		// Create renderer if it doesn't exist
+		if ( !renderer ) {
+			renderer = this._renderer = (L.SVG && L.svg()) || (L.Canvas && L.canvas());
+			renderer.options.pane = this._name;
+		}
+
+		return renderer;
 	},
 
 	add: function (container) {
@@ -22,7 +45,6 @@ L.Pane = L.Class.extend({
 	remove: function (container) {
 		this._container.removeChild(container);
 	}
-
 
 });
 
