73147bc9e691f200d5728a39d93931192f168e62
Hans Kristian Flaatten <hans@starefossen.com>
2015-01-24 11:20:01 +0100

    Update year in copyright.js

--- src/copyright.js
+++ src/copyright.js
@@ -1,4 +1,4 @@
 /*
  Leaflet {VERSION}, a JS library for interactive maps. http://leafletjs.com
- (c) 2010-2014 Vladimir Agafonkin, (c) 2010-2011 CloudMade
+ (c) 2010-2015 Vladimir Agafonkin, (c) 2010-2011 CloudMade
 */
