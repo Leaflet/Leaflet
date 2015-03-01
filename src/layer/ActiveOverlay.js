2ca6d535eac977bca6330d86f847bc678a84fc30
Asko Kauppi <asko@bmdesign.fi>
2015-03-01 14:07:44 +0200

    cleaned up for push

--- src/layer/ActiveOverlay.js
+++ src/layer/ActiveOverlay.js
@@ -10,22 +10,22 @@
 
 L.ActiveOverlay = L.Layer.extend({
 
-  /* Design note: there does not seem to be a way to apply opacity to an 'svg' HTML tag,
-   *       so it's left out of options, compared to 'L.ImageOverlay'. To get the
-   *       same effect, one would set one group ('<g>') within the SVG and control
-   *       its opacity instead.
-   *
-   * Q: what does 'interactive' actually do for us? Maybe not needed?
-   */
+	/* Design note: there does not seem to be a way to apply opacity to an 'svg' HTML tag,
+	 *       so it's left out of options, compared to 'L.ImageOverlay'. To get the
+	 *       same effect, one would set one group ('<g>') within the SVG and control
+	 *       its opacity instead.
+	 *
+	 * Q: what does 'interactive' actually do for us? Maybe not needed?
+	 */
 	options: {
 		interactive: false
 	},
 
-  /* Design note: either the caller (as in here) or us could create the 'svgElem'.
-   *        This way is more in line with the 'L.ImageOverlay' parameter list (which has
-   *        the picture's URL as the first param). Also, this means we do not need to
-   *        expose the element as a property ('.svgElem') or return a value from 'initialize'.
-   */
+	/* Design note: either the caller (as in here) or us could create the 'svgElem'.
+	 *        This way is more in line with the 'L.ImageOverlay' parameter list (which has
+	 *        the picture's URL as the first param). Also, this means we do not need to
+	 *        expose the element as a property ('.svgElem') or return a value from 'initialize'.
+	 */
 	initialize: function (svgElem, bounds, options) { // (Svg element, LatLngBounds [, Object])
 
 		this._svgElem = svgElem;
@@ -35,7 +35,6 @@ L.ActiveOverlay = L.Layer.extend({
 		// Set by '._reset()':
 		//
 		// this._initialized: boolean
-		// this._svgWidth: number
 		// this._factor: number
 
 		L.setOptions(this, options);
@@ -82,23 +81,13 @@ L.ActiveOverlay = L.Layer.extend({
 		return this;
 	},
 
-  // Q: is this needed?
-  //
+	// Q: is this needed?
+	//
 	_initInteraction: function () {
 
 		if (this.options.interactive) {
 		  var el = this._svgElem;
 		  L.DomUtil.addClass(el, 'leaflet-interactive');
-
-      /*** REMOVE
-      // Note: Trying to disabled "dual panning" if the app makes 'svgElem' draggable (= can move above the map);
-      //      we don't want also the map to move around.
-      //
-      // If we have this on, croc can be dragged relative the map (though it seems jerky),
-      // but the "open" map areas won't pan, either (which is not what we want). AK150215
-      //
-      //L.DomEvent.disableClickPropagation(el);
-      ***/
 
 		  L.DomEvent.on(el, 'click dblclick mousedown mouseup mouseover mousemove mouseout contextmenu',
 				this._fireMouseEvent, this);
@@ -106,16 +95,16 @@ L.ActiveOverlay = L.Layer.extend({
 	},
 
 	_fireMouseEvent: function (e, type) {
-    // Note: This does not seem to matter, at all. (because 'this._map' is 'undefined')
-    //
+		// Note: This does not seem to matter, at all. (because 'this._map' is 'undefined')
+		//
 		if (this._map) {
 			this._map._fireMouseEvent(this, e, type, true);
 		}
 	},
 
-  // Q: should we have it here? (if it's about the attribution of the image,
-  //    then we don't).
-  //
+	// Q: should we have it here? (if it's about the attribution of the image,
+	//    then we don't).
+	//
 	getAttribution: function () {
 		return this.options.attribution;
 	},
@@ -147,36 +136,31 @@ L.ActiveOverlay = L.Layer.extend({
 		L.DomUtil.setTransform(this._svgElem, offset, e.scale);
 	},
 
-	/*
-	* Called at 'onAdd' and 'viewreset'
-	*/
 	_reset: function () {
 		var el = this._svgElem;
 
-    // Pixels from the top left of the map, if the map hasn't been panned
-    //
+		// Pixels from the top left of the map, if the map hasn't been panned
+		//
 		var bounds = L.bounds(
-      this._map.latLngToLayerPoint(this._bounds.getNorthWest()),
-      this._map.latLngToLayerPoint(this._bounds.getSouthEast()));
-      
-    var size = bounds.getSize();    // size in screen pixels
+			this._map.latLngToLayerPoint(this._bounds.getNorthWest()),
+			this._map.latLngToLayerPoint(this._bounds.getSouthEast()));
 
-    //console.log( "LLBounds: "+ this._bounds.getNorthWest() +" "+ this._bounds.getSouthEast() );
-    console.log( "Bounds: "+ bounds.min +" "+ bounds.max );
-    console.log( "Size: "+ size );
-    
+		var size = bounds.getSize();    // size in screen pixels
+
 		if (!this._initial) {
 			this._initial = true;
-			
+
 			// The actual dimensions (in meters) get anchored in the SVG 'viewBox' properties
 			// (and don't change by zooming or panning). These are the SVG coordinate dimensions
 			// of the bounding box.
 			//
+			// Q: Would it be more akin to Leaflet design to cache the initial 'size.x' as 'this._svgWidth'?
+			//   We now read it back from the viewBox property. Less code or less members? Both work.
+			//
 			el.setAttribute('viewBox', [0, 0, size.x, size.y].join(' '));
-			this._svgWidth = size.x; 
 			this._factor = 1.0;
 		} else {
-		  this._factor = size.x / this._svgWidth;     // pixel's size in meters (note: syntax highlighting problem with SEE')
+			this._factor = size.x / el.getAttribute('viewBox').split(' ')[2];
 		}
 
 		L.DomUtil.setPosition(el, bounds.min);
@@ -190,24 +174,20 @@ L.ActiveOverlay = L.Layer.extend({
 	*
 	* Note: For a normal image, this would mean conversion from/to pixel coordinates
 	*      (which feature is not in the 'ImageOverlay' API but could be).
-	*
-	* Q: We're currently using the normal 'Point' object for the SVG coordinates, but 
-	*    these are conceptually different than the pixel coordinates. Is this okay, or
-	*    should a special 'SvgPoint' be made to be more specific?
 	*/
-	latLngToSvgPoint: function(latLng) {  // (LatLng) -> SvgPoint
-    var offset = L.DomUtil.getPosition(this._svgElem);    // offset in screen pixels, without panning
+	latLngToSvgPoint: function(latLng) {  // (LatLng) -> Point
+		var offset = L.DomUtil.getPosition(this._svgElem);    // offset in screen pixels, without panning
 
-    var p = this._map.latLngToLayerPoint(latLng)._subtract(offset)._divideBy(this._factor);
+		var p = this._map.latLngToLayerPoint(latLng)._subtract(offset)._divideBy(this._factor);
 		return p;
 	},
 
-  // Note: This has not been tested!!
-  //
-	svgPointToLatLng: function(p) {   // (SvgPoint) -> LatLng
-    var offset = L.DomUtil.getPosition(this._svgElem);
+	// Note: This has not been tested!!
+	//
+	svgPointToLatLng: function(p) {   // (Point) -> LatLng
+		var offset = L.DomUtil.getPosition(this._svgElem);
 
-		var latlng = this._map.layerPointToLatLng( p.multiplyBy(this._factor)._add(offset) );
+		var latlng = this._map.layerPointToLatLng(p.multiplyBy(this._factor)._add(offset));
 		return latlng;
 	}
 });
