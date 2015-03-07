196040b7950a5502bbc480f3dd1fa1ca0e96a956
Yohan Boniface <yb@enix.org>
2015-03-07 12:45:41 +0100

    Attempt to fix random PhantomJS DISCONNECTED errors on Travis

--- spec/karma.conf.js
+++ spec/karma.conf.js
@@ -63,6 +63,11 @@ module.exports = function (config) {
 		// If browser does not capture in given timeout [ms], kill it
 		captureTimeout: 5000,
 
+		// Workaround for PhantomJS random DISCONNECTED error
+		browserDisconnectTimeout: 10000, // default 2000
+		browserDisconnectTolerance: 1, // default 0
+		browserNoActivityTimeout: 60000, //default 10000
+
 		// Continuous Integration mode
 		// if true, it capture browsers, run tests and exit
 		singleRun: true
