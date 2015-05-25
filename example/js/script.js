86058563ac904e5ea8184e92de090860e1562d02
Alexey Seliverstov <alexeyseliverstov.dev@gmail.com>
2015-05-25 17:24:27 +0100

    roads are now different colors

--- example/js/script.js
+++ example/js/script.js
@@ -14,28 +14,32 @@ command += "PREFIX geof: <http:\/\/www.opengis.net\/def\/geosparql\/function\/>"
 command += "PREFIX spatial: <http:\/\/geovocab.org\/spatial#>";
 command += "PREFIX strdf: <http:\/\/strdf.di.uoa.gr\/ontology#>";
 command += " ";
-command += "SELECT DISTINCT ?class ?className ?line ?nameValue ?db ?vocab ?field";
+command += "SELECT DISTINCT ?line ?roadReference ?highwayType ";
 command += "WHERE {";
 command += "  ?db void:sparqlEndpoint ?endpoint .";
 command += "  ?db local:vocabularyURL ?vocab .";
 command += "  ?db local:hasClass ?class .";
 command += "  ?class local:className ?className.";
-command += "  ?class local:hasField ?field.";
-command += "  ?field local:fieldName ?fieldName.";
-command += "  ?class local:hasField ?nameField .";
-command += "  ?nameField local:fieldName ?nameFieldName.  ";
+command += "  ?class local:hasField ?highwayField .";
+command += "  ?highwayField local:fieldName ?highwayFieldName.";
+command += "  ";
+command += "  ?class local:hasField ?refField.";
+command += "  ?refField local:fieldName ?refFieldName";
+
 command += "  FILTER REGEX(STR(?db), \"porto\")";
-command += "  FILTER REGEX(STR(?className), \"planet_osm_roads$\")";
-command += "  FILTER REGEX(STR(?field), \"planet_osm_roads_name$\")";
-command += "  FILTER REGEX(STR(?nameField), \"planet_osm_roads_name$\")";
+command += "  FILTER REGEX(STR(?className), \"planet_osm_line$\")";
+command += "  FILTER REGEX(STR(?highwayFieldName), \"planet_osm_line_highway$\")";
+command += "  FILTER REGEX(STR(?refFieldName), \"planet_osm_line_ref$\")";
 command += "  {";
 command += "    SERVICE SILENT ?endpoint {";
 command += "        ?b a ?className.";
 command += "        ?b geo:asWKT ?line.";
-command += "        ?b ?nameFieldName ?nameValue";
+command += "        ?b ?highwayFieldName ?highwayType.";
+command += "        ?b ?refFieldName ?roadReference";
 command += "    }";
 command += "  }";
 command += "}";
+
 
 $.ajax({
     type: 'POST',
@@ -50,17 +54,20 @@ function mapPoints(data){
   console.log(data.results);
   for (i=0; i<data.results.bindings.length;i++){
     var point = data.results.bindings[i].point.value;
-
+    var roadType = data.results.bindings[i].type.value;
     var lat = point.split("(")[1].split(")")[0].split(" ")[0];
     var lon = point.split("(")[1].split(")")[0].split(" ")[1];
     console.log("lat: "+lat+" long: "+lon);
-    feat = {'type': 'Feature', "properties": {"linetype": "map", "name": name, "popupContent": "hi"}, 'geometry':{'type':'Point', 'coordinates':[lat, lon]}, onEachFeature: onEachFeature
+    feat = {'type': 'Feature', "properties": {"linetype": "road-"+roadType, "name": name, "popupContent": name}, 'geometry':{'type':'Point', 'coordinates':[lat, lon]}, onEachFeature: onEachFeature
 };
     if (i<100)
       L.geoJson(feat, {
         style: function(feature) {
         switch (feature.properties.linetype) {
-            case 'map': return {color: "#ff0000"};
+            case 'road-motorway_link': return {color: "#ff0000"};
+            case 'road-primary': return {color: "#ff0000"};
+            case 'road-secondary': return {color: "#ff0000"};
+            case 'road-tertiary': return {color: "#ff0000"};
             case 'car':   return {color: "#0000ff"};
             case 'bus':   return {color: "#0000ff"};
         }
@@ -73,7 +80,9 @@ function mapPoints(data){
 function mapLines(data){
   for (i=0; i<data.results.bindings.length;i++){
     var rawCoordsString = data.results.bindings[i].line.value;
-    var name = data.results.bindings[i].nameValue.value;
+    console.log(data.results.bindings[i]);
+    var name = data.results.bindings[i].roadReference.value;
+    var roadType = data.results.bindings[i].highwayType.value;
     var coordsArray = rawCoordsString.split("(")[1].split(")")[0].split(",");
     var newCoordsArray = [];
     for (j=0; j<coordsArray.length;j++){
@@ -89,12 +98,16 @@ function mapLines(data){
     feat = {
       'type': 'Feature', 
       "properties": 
-      {"linetype": "map", "name": name, "popupContent": name}, 
+      {"linetype": "road-"+roadType, "name": name, "popupContent": name}, 
       'geometry':{'type':'LineString', 'coordinates':newCoordsArray}, 
       "id":i+1};
       L.geoJson(feat, {
         style: function(feature) {
           switch (feature.properties.linetype) {
+              case 'road-motorway_link': return {color: "#ff0000"};
+              case 'road-primary': return {color: "#ff0000"};
+              case 'road-secondary': return {color: "#ff0000"};
+              case 'road-tertiary': return {color: "#ff0000"};
               case 'map': return {color: "#010011"};
               case 'car':   return {color: "#e65d1f"};
               case 'bus':   return {color: "#509ddd"};
