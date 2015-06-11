c3288ab7650c932c6089bba9ab68fe07ca9675bf
Iván Sánchez Ortega <ivan@mazemap.no>
2015-06-11 11:35:09 +0200

    Synthetic doubletap events respect stopPropagation, fixes #3532

--- src/dom/DomEvent.DoubleTap.js
+++ src/dom/DomEvent.DoubleTap.js
@@ -33,7 +33,7 @@ L.extend(L.DomEvent, {
 		}
 
 		function onTouchEnd() {
-			if (doubleTap) {
+			if (doubleTap && !touch.cancelBubble) {
 				if (L.Browser.pointer) {
 					// work around .type being readonly with MSPointer* events
 					var newTouch = {},
