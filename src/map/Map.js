Leaflet/Leaflet

commit 9b7f24f5f6efb94be2a96d7d7f4c604783135d10

More natural event bubbling

src/map/Map.js
@@ -601,6 +601,7 @@ L.Map = L.Evented.extend({
 		if (!L.DomEvent) { return; }
 
 		this._targets = {};
+		this._targets[L.stamp(this._container)] = this;
 
 		var onOff = remove ? 'off' : 'on';
 
@@ -623,25 +624,27 @@ L.Map = L.Evented.extend({
 		this._container.scrollLeft = 0;
 	},
 
-	_findEventTarget: function (src) {
+	_findEventTargets: function (src, bubble) {
+		var targets = [], target;
 		while (src) {
-			var target = this._targets[L.stamp(src)];
+			target = this._targets[L.stamp(src)];
 			if (target) {
-				return target;
+				targets.push(target);
+				if (!bubble) { break; }
 			}
 			if (src === this._container) {
 				break;
 			}
 			src = src.parentNode;
 		}
-		return null;
+		return targets;
 	},
 
 	_handleDOMEvent: function (e) {
 		if (!this._loaded || L.DomEvent._skipped(e)) { return; }
 
-		// find the layer the event is propagating from
-		var target = this._findEventTarget(e.target || e.srcElement),
+		// find the layer the event is propagating from and its parents
+		var targets = this._findEventTargets(e.target || e.srcElement, !(e.type === 'mouseover' || e.type === 'mouseout')),
 			type = e.type === 'keypress' && e.keyCode === 13 ? 'click' : e.type;
 
 		if (e.type === 'click') {
@@ -652,7 +655,7 @@ L.Map = L.Evented.extend({
 		}
 
 		// special case for map mouseover/mouseout events so that they're actually mouseenter/mouseleave
-		if (!target && (type === 'mouseover' || type === 'mouseout') &&
+		if (!targets.length && (type === 'mouseover' || type === 'mouseout') &&
 		                !L.DomEvent._checkMouse(this._container, e)) { return; }
 
 		if (type === 'mousedown') {
@@ -660,30 +663,37 @@ L.Map = L.Evented.extend({
 			L.DomUtil.preventOutline(e.target || e.srcElement);
 		}
 
-		this._fireDOMEvent(target || this, e, type);
+		if (!targets.length) {
+			targets = [this];
+		}
+		this._fireDOMEvent(targets, e, type);
 	},
 
-	_fireDOMEvent: function (target, e, type) {
-		if (!target.listens(type, true)) { return; }
+	_fireDOMEvent: function (elements, e, type) {
 
 		if (type === 'contextmenu') {
 			L.DomEvent.preventDefault(e);
 		}
+
+		var target = elements[0];
 
 		// prevents firing click after you just dragged an object
 		if (e.type === 'click' && !e._simulated && this._draggableMoved(target)) { return; }
 
 		var data = {
 				originalEvent: e
-			},
-			propagate = (type !== 'mouseover' && type !== 'mouseout');
+			};
 		if (e.type !== 'keypress') {
 			data.containerPoint = target instanceof L.Marker ?
 					this.latLngToContainerPoint(target.getLatLng()) : this.mouseEventToContainerPoint(e);
 			data.layerPoint = this.containerPointToLayerPoint(data.containerPoint);
 			data.latlng = this.layerPointToLatLng(data.layerPoint);
 		}
-		target.fire(type, data, propagate);
+		for (var i = 0; i < elements.length; i++) {
+			if (data.originalEvent._stopped) { break; }
+			if (!elements[i].listens(type, true)) { continue; }
+			elements[i].fire(type, data);
+		}
 	},
 
 	_draggableMoved: function (obj) {

src/core/Events.js
@@ -211,7 +211,6 @@ L.Evented = L.Class.extend({
 
 	_propagateEvent: function (e) {
 		for (var id in this._eventParents) {
-			if (e._stopped) { break; }
 			this._eventParents[id].fire(e.type, L.extend({layer: e.target}, e), true);
 		}
 	}

src/dom/DomEvent.js
@@ -130,9 +130,10 @@ L.DomEvent = {
 
 		if (e.stopPropagation) {
 			e.stopPropagation();
+		} else if (e.originalEvent) {  // In case of Leaflet event.
+			e.originalEvent._stopped = true;
 		} else {
 			e.cancelBubble = true;
-			e._stopped = true;  // In case of Leaflet event.
 		}
 		L.DomEvent._skipped(e);
 

src/layer/Layer.js
@@ -67,9 +67,6 @@ L.Layer = L.Evented.extend({
 L.Map.include({
 	addLayer: function (layer) {
 		var id = L.stamp(layer);
-
-		layer.addEventParent(this);
-
 		if (this._layers[id]) { return layer; }
 		this._layers[id] = layer;
 
@@ -86,8 +83,6 @@ L.Map.include({
 
 	removeLayer: function (layer) {
 		var id = L.stamp(layer);
-
-		layer.removeEventParent(this);
 
 		if (!this._layers[id]) { return this; }
 

commit fd1b94550c64806b868c439d8b1a0d87f70d6413

Fix popup broken since 9b7f24f5f6efb94be2a96d7d7f4c604783135d10
Popup where binded by the LayerGroup, which was not finring the click
event, because not propagated from the layers

src/map/Map.js
@@ -695,7 +695,7 @@ L.Map = L.Evented.extend({
 
 		for (var i = 0; i < targets.length; i++) {
 			if (targets[i].listens(type, true)) {
-				targets[i].fire(type, data);
+				targets[i].fire(type, data, true);
 				if (data.originalEvent._stopped) { return; }
 			}
 		}