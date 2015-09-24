bcde047f0991b922ea845d3786d9a887f0b02f7e
Coen Mulders <cmulders@users.noreply.github.com>
2015-09-25 00:19:32 +0200

    Added zoom for the custom scale in L.CRS.Simple
    Fixes #3867

--- src/geo/crs/CRS.Simple.js
+++ src/geo/crs/CRS.Simple.js
@@ -10,6 +10,10 @@ L.CRS.Simple = L.extend({}, L.CRS, {
 		return Math.pow(2, zoom);
 	},
 
+	zoom: function (scale) {
+		return Math.log(scale) / Math.LN2;
+	},
+
 	distance: function (latlng1, latlng2) {
 		var dx = latlng2.lng - latlng1.lng,
 		    dy = latlng2.lat - latlng1.lat;
