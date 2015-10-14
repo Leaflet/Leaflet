857848fbeeb0d23b5cccef6e38f3781505bbf811
Vladimir Agafonkin <agafonkin@gmail.com>
2015-10-14 18:29:48 +0300

    come back to the old publishing approach with improvements

--- build/publish.sh
+++ build/publish.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+npm update
+
 VERSION=$(node --eval "console.log(require('./package.json').version);")
 
-# # create build, zip, bower and component artifacts
+npm test || exit 1
+
+git checkout -b build
+
 npm run build
+git add dist/leaflet-src.js dist/leaflet.js dist/leaflet-src.map -f
 
-cd dist && zip -x .DS_Store -r leaflet.zip . && cd ..
-
-# cp build/*.json ./
+copyfiles -u 1 build/*.json ./
 tin -v $VERSION
+git add component.json bower.json -f
+
+git commit -m "v$VERSION"
+
+git tag v$VERSION -f
+git push --tags -f
+
+npm publish --tag beta
+
+git checkout master
+git branch -D build
