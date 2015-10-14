213c602f4e0bd288c76c87036a0ed9e1c61ca7c4
Vladimir Agafonkin <agafonkin@gmail.com>
2015-10-14 17:56:50 +0300

    new Travis configuration with automatic releases

--- build/publish.sh
+++ build/publish.sh
@@ -1,31 +1,11 @@
 #!/bin/bash
 
-#make sure deps are up to date
-rm -r node_modules
-npm install
+VERSION=$(node --eval "console.log(require('./package.json').version);")
 
-# get current version
- VERSION=$(node --eval "console.log(require('./package.json').version);")
-# Build
-git checkout -b build
-npm test || exit 1
-npm run prepublish
-git add dist/leaflet-src.js dist/leaflet.js -f
+# # create build, zip, bower and component artifacts
+npm run build
 
-# create the bower and component files
-copyfiles -u 1 build/*.json ./
+cd dist && zip -x .DS_Store -r leaflet.zip . && cd ..
+
+# cp build/*.json ./
 tin -v $VERSION
-git add component.json bower.json -f
-
-git commit -m "build $VERSION"
-
-# Tag and push
-echo git tag $VERSION
-# git push --tags git@github.com:leaflet/leaflet.git $VERSION
-
-# # # Publish JS modules
-# npm publish
-
-# # # Cleanup
-# git checkout master
-# git branch -D build
