26993c091eeb04996110ba8d25ba2fd510310eea
Yuri Egorov <ysegorov@gmail.com>
2016-02-26 11:44:03 +0300

    pass options object to Marker constructor to have proper pane if specified

--- src/layer/GeoJSON.js
+++ src/layer/GeoJSON.js
@@ -89,12 +89,12 @@ L.extend(L.GeoJSON, {
 		switch (geometry.type) {
 		case 'Point':
 			latlng = coordsToLatLng(coords);
-			return pointToLayer ? pointToLayer(geojson, latlng) : new L.Marker(latlng);
+			return pointToLayer ? pointToLayer(geojson, latlng) : new L.Marker(latlng, options);
 
 		case 'MultiPoint':
 			for (i = 0, len = coords.length; i < len; i++) {
 				latlng = coordsToLatLng(coords[i]);
-				layers.push(pointToLayer ? pointToLayer(geojson, latlng) : new L.Marker(latlng));
+				layers.push(pointToLayer ? pointToLayer(geojson, latlng) : new L.Marker(latlng, options));
 			}
 			return new L.FeatureGroup(layers);
 
