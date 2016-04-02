9cd28a09a057b4b50a0599ea3dd7ef93de376f06
Per Liedman <per@liedman.net>
2016-04-02 22:01:11 +0200

    Fix SVG dragging in IE and Edge.
    Use correspondingUseElement as target for SVG.
    
    Close #4359.

--- src/dom/Draggable.js
+++ src/dom/Draggable.js
@@ -118,6 +118,11 @@ L.Draggable = L.Evented.extend({
 			L.DomUtil.addClass(document.body, 'leaflet-dragging');
 
 			this._lastTarget = e.target || e.srcElement;
+			// IE and Edge do not give the <use> element, so fetch it
+			// if necessary
+			if ((window.SVGElementInstance) && (this._lastTarget instanceof SVGElementInstance)) {
+				this._lastTarget = this._lastTarget.correspondingUseElement;
+			}
 			L.DomUtil.addClass(this._lastTarget, 'leaflet-drag-target');
 		}
 
