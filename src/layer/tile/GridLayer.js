e075d4bc5c146380a9fe77d2edde9b2d07d4a123
Jay Varner <jayvarner@gmail.com>
2016-04-19 16:42:13 -0400

    Fixed bad spacing.

--- src/layer/tile/GridLayer.js
+++ src/layer/tile/GridLayer.js
@@ -116,13 +116,8 @@ L.GridLayer = L.Layer.extend({
 		// `Map pane` where the grid layer will be added.
 		pane: 'tilePane',
 
-<<<<<<< b9ee756d17bb9ddbf11ddb68a32dc971995ae0a3
 		// @option className: String = ''
-		// A custom class name to assign to the tile layer. Empty by default.
-=======
-         // @option className: String = ''
-		 // A custom class name to assign to the tile layer. Empty by default.
->>>>>>> Added className option to the options object on GridLayer.
+        // A custom class name to assign to the tile layer. Empty by default.
 		className: ''
 	},
 
