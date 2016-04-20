2b52369fac8e2007cce37e43ea189032f46cd4b6
Nathan Cahill <nathan@nathancahill.com>
2016-04-20 12:57:06 -0600

    Add CRS.distance docstring (#4466)
    * add CRS.distance docstring
    
    * add CRS.distance return values

--- src/geo/crs/CRS.Earth.js
+++ src/geo/crs/CRS.Earth.js
@@ -4,7 +4,8 @@
  *
  * Serves as the base for CRS that are global such that they cover the earth.
  * Can only be used as the base for other CRS and cannot be used directly,
- * since it does not have a `code`, `projection` or `transformation`.
+ * since it does not have a `code`, `projection` or `transformation`. `distance()` returns
+ * meters.
  */
 
 L.CRS.Earth = L.extend({}, L.CRS, {
