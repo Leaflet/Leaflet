5e127089df65a88c0a21f71d6757a364346dc96d
Iván Sánchez Ortega <ivan@mazemap.no>
2016-05-27 10:27:13 +0200

    CSS-based paths for L.Icon.Default. #4604, #3696, #4579

--- src/layer/marker/Marker.js
+++ src/layer/marker/Marker.js
@@ -17,7 +17,7 @@ L.Marker = L.Layer.extend({
 	// @aka Marker options
 	options: {
 		// @option icon: Icon = *
-		// Icon class to use for rendering the marker. See [Icon documentation](#L.Icon) for details on how to customize the marker icon. Set to new `L.Icon.Default()` by default.
+		// Icon class to use for rendering the marker. See [Icon documentation](#L.Icon) for details on how to customize the marker icon. If not specified, a new `L.Icon.Default` is used.
 		icon: new L.Icon.Default(),
 
 		// Option inherited from "Interactive layer" abstract class
