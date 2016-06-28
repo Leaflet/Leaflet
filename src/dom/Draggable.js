e7e63d2640d9190125b3b2495a8432429279f66e
Fabien NICOLLET <fnicollet@gmail.com>
2016-06-28 14:22:07 +0200

    Fix draggable markers rotation (#13)
    * Account for rotation in Draggable
    
    * Fix rotation on draggable markers
    
    Related to https://github.com/hyperknot/Leaflet/issues/12
    
    * DRY shadowAnchor property
    
    * Call updateMapBearing only if rotation enabled

--- src/dom/Draggable.js
+++ src/dom/Draggable.js
@@ -68,6 +68,10 @@ L.Draggable = L.Evented.extend({
 		this._moved = false;
 	},
 
+	updateMapBearing: function(mapBearing) {
+		this._mapBearing = mapBearing;
+	},
+
 	_onDown: function (e) {
 		// Ignore simulated events, since we handle both touch and
 		// mouse explicitly; otherwise we risk getting duplicates of
@@ -120,7 +124,12 @@ L.Draggable = L.Evented.extend({
 
 		var first = (e.touches && e.touches.length === 1 ? e.touches[0] : e),
 		    newPoint = new L.Point(first.clientX, first.clientY),
-		    offset = newPoint.subtract(this._startPoint);
+		    offset = newPoint.subtract(this._startPoint),
+		    bearing = this._mapBearing || 0;
+
+		if (bearing) {
+			offset = offset.rotate(-bearing);
+		}
 
 		if (!offset.x && !offset.y) { return; }
 		if (Math.abs(offset.x) + Math.abs(offset.y) < this.options.clickTolerance) { return; }
--- src/layer/marker/Marker.Drag.js
+++ src/layer/marker/Marker.Drag.js
@@ -64,6 +64,9 @@ L.Handler.MarkerDrag = L.Handler.extend({
 		    .closePopup()
 		    .fire('movestart')
 		    .fire('dragstart');
+		if (this._marker._map._rotate){
+			this._draggable.updateMapBearing(this._marker._map._bearing);	
+		}
 	},
 
 	_onDrag: function (e) {
@@ -72,9 +75,19 @@ L.Handler.MarkerDrag = L.Handler.extend({
 		    iconPos = L.DomUtil.getPosition(marker._icon),
 		    latlng = marker._map.layerPointToLatLng(iconPos);
 
+		if (marker._map._rotate) {
+			var iconAnchor = marker.options.icon.options.iconAnchor;
+			L.DomUtil.setPosition(marker._icon, iconPos, -marker._map._bearing, iconPos.add(iconAnchor));
+		}
+
 		// update shadow position
 		if (shadow) {
-			L.DomUtil.setPosition(shadow, iconPos);
+			if (marker._map._rotate) {
+				var shadowAnchor = marker.options.icon.options.shadowAnchor ? iconPos.add(marker.options.icon.options.shadowAnchor) : iconPos.add(iconAnchor);
+				L.DomUtil.setPosition(shadow, iconPos, -marker._map._bearing || 0, shadowAnchor);
+			} else {
+				L.DomUtil.setPosition(shadow, iconPos);
+			}
 		}
 
 		marker._latlng = latlng;
