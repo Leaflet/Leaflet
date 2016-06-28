c7637139e4caaa353863e4d546bbf6364981401e
Fabien NICOLLET <fnicollet@gmail.com>
2016-06-28 23:44:23 +0200

    Simplify CompassBearing
    Probing wasn't necessary, checking DeviceOrientationEvent for support is enough to determine if the browser is capable. Also now compatible with the classic hook logic, so you can call enable/disable on the hook
    ```javascript
    map.compassBearing.disable();
    ```
    Tested with the Chrome dev tools

--- src/map/handler/Map.CompassBearing.js
+++ src/map/handler/Map.CompassBearing.js
@@ -5,62 +5,30 @@
 L.Map.CompassBearing = L.Handler.extend({
 
 	initialize: function(map) {
-		this._capable = false;
-		if (!map._rotate){
-			return;
+		if (!window.DeviceOrientationEvent) { 
+			this._capable = false;
+			return; 
 		}
+		this._capable = true;
 		this._map = map;
-
-		if (!window.DeviceOrientationEvent) { return; }
-
-		this._probing = true;
+		
 		this._throttled = L.Util.throttle(this._onDeviceOrientation, 100, this);
-
-		L.DomEvent.on(window, 'deviceorientation', this._probe, this);
-		window.setTimeout(this._cancelProbe.bind(this), 1000);
 	},
 
 	addHooks: function () {
-		if (this._capable) {
-// 			alert('Tracking compass');
+		if (this._capable && this._map._rotate) {
 			L.DomEvent.on(window, 'deviceorientation', this._throttled, this);
-		} else if (!this._probing) {
-// 			alert('Not tracking compass');
-			this._enabled = false;
-		} else {
-// 			console.log('Waiting for feature probing');
 		}
 	},
 
 	removeHooks: function () {
-		if (this._capable) {
+		if (this._capable && this._map._rotate) {
 			L.DomEvent.off(window, 'deviceorientation', this._throttled, this);
 		}
 	},
 
-	_onDeviceOrientation: function(ev) {
-		if (ev.absolute && this._enabled) {
-			this._map.setBearing(ev.alpha);
-		}
-	},
-
-	_probe: function(ev) {
-// 		console.log('Probed deviceOrientation', ev);
-		L.DomEvent.off(window, 'deviceorientation', this._probe, this);
-		this._capable = true;
-		if (this._enabled) {
-			this.addHooks();
-		}
-	},
-
-	_cancelProbe: function(ev) {
-		L.DomEvent.off(window, 'deviceorientation', this._probe, this);
-
-// 		if (!this._capable) {
-// 			alert('No device orientation');
-// 		}
-
-		this._probing = false;
+	_onDeviceOrientation: function(event) {
+		this._map.setBearing(event.alpha);
 	}
 });
 
