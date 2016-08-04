e3b49137c595209a5967ce0a8264a13ce9a617ef
Yohan Boniface <yb@enix.org>
2016-08-04 10:44:52 +0200

    Do not call zoomIn/zoomOut on zoomControl click if already at zoom limit
    fix #4790

--- src/control/Control.Zoom.js
+++ src/control/Control.Zoom.js
@@ -62,13 +62,13 @@ L.Control.Zoom = L.Control.extend({
 	},
 
 	_zoomIn: function (e) {
-		if (!this._disabled) {
+		if (!this._disabled && this._map._zoom < this._map.getMaxZoom()) {
 			this._map.zoomIn(this._map.options.zoomDelta * (e.shiftKey ? 3 : 1));
 		}
 	},
 
 	_zoomOut: function (e) {
-		if (!this._disabled) {
+		if (!this._disabled && this._map._zoom > this._map.getMinZoom()) {
 			this._map.zoomOut(this._map.options.zoomDelta * (e.shiftKey ? 3 : 1));
 		}
 	},
