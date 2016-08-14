bb19024de94bce9b27247cc920e1d0fea3be651a
Steve Kashishian <steve@snkashis.com>
2016-08-14 10:42:11 -0500

    Throw error on call to L.Polygon getCenter before map add
    References #4740

--- src/layer/vector/Polygon.js
+++ src/layer/vector/Polygon.js
@@ -55,6 +55,10 @@ L.Polygon = L.Polyline.extend({
 	},
 
 	getCenter: function () {
+		if (!this._map) {
+			throw new Error('Must add layer to map before using getCenter()');
+		}
+
 		var i, j, p1, p2, f, area, x, y, center,
 		    points = this._rings[0],
 		    len = points.length;
