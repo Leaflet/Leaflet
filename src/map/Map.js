73a40ddff0f7ba80b0d9b975dd3ea41022b0576e
Andris Nolendorfs <andris.nolendorfs@gmail.com>
2016-09-15 01:21:39 +0300

    Small documentation fixes

--- src/map/Map.js
+++ src/map/Map.js
@@ -852,14 +852,14 @@ L.Map = L.Evented.extend({
 		this._pixelOrigin = this._getNewPixelOrigin(center);
 
 		// @event zoom: Event
-		// Fired repeteadly during any change in zoom level, including zoom
+		// Fired repeatedly during any change in zoom level, including zoom
 		// and fly animations.
 		if (zoomChanged || (data && data.pinch)) {	// Always fire 'zoom' if pinching because #3530
 			this.fire('zoom', data);
 		}
 
 		// @event move: Event
-		// Fired repeteadly during any movement of the map, including pan and
+		// Fired repeatedly during any movement of the map, including pan and
 		// fly animations.
 		return this.fire('move', data);
 	},
