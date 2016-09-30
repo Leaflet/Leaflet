6b0808d6f062c7d58e522d89e824ec20ed67ffec
Babajide Fowotade <jide.b.tade@gmail.com>
2016-09-30 08:28:42 +0100

    Don't load class leaflet-tile-loaded in case of error (#4980)

--- src/layer/tile/GridLayer.js
+++ src/layer/tile/GridLayer.js
@@ -811,14 +811,16 @@ L.GridLayer = L.Layer.extend({
 			this._pruneTiles();
 		}
 
-		L.DomUtil.addClass(tile.el, 'leaflet-tile-loaded');
+		if (!err) {
+			L.DomUtil.addClass(tile.el, 'leaflet-tile-loaded');
 
-		// @event tileload: TileEvent
-		// Fired when a tile loads.
-		this.fire('tileload', {
-			tile: tile.el,
-			coords: coords
-		});
+			// @event tileload: TileEvent
+			// Fired when a tile loads.
+			this.fire('tileload', {
+				tile: tile.el,
+				coords: coords
+			});
+		}
 
 		if (this._noTilesToLoad()) {
 			this._loading = false;
