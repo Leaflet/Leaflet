2ff436fb2b3f14250c9d020d0d95feac1b1b5278
Vladimir Agafonkin <agafonkin@gmail.com>
2016-10-04 19:10:41 +0300

    WIP rollup: export more stuff

--- src/geo/projection/Projection.js
+++ src/geo/projection/Projection.js
@@ -19,4 +19,4 @@
 
 export {LonLat} from './Projection.LonLat';
 export {Mercator} from './Projection.Mercator';
-export {SphericalMercator} from './Projection.Mercator';
+export {SphericalMercator} from './Projection.SphericalMercator';
