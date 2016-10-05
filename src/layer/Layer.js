249ce0498206c96c09f3e31812662b005ad2c1b5
Vladimir Agafonkin <agafonkin@gmail.com>
2016-10-05 13:23:58 +0300

    rollup: export Layer

--- src/layer/Layer.js
+++ src/layer/Layer.js
@@ -1,4 +1,8 @@
 
+import {Evented} from '../core/Events';
+import {Map} from '../map/Map';
+import {stamp, isArray} from '../core/Util';
+
 /*
  * @class Layer
  * @inherits Evented
@@ -24,7 +28,7 @@
  */
 
 
-L.Layer = L.Evented.extend({
+export var Layer = Evented.extend({
 
 	// Classes extending `L.Layer` will inherit the following options:
 	options: {
@@ -71,12 +75,12 @@ L.Layer = L.Evented.extend({
 	},
 
 	addInteractiveTarget: function (targetEl) {
-		this._map._targets[L.stamp(targetEl)] = this;
+		this._map._targets[stamp(targetEl)] = this;
 		return this;
 	},
 
 	removeInteractiveTarget: function (targetEl) {
-		delete this._map._targets[L.stamp(targetEl)];
+		delete this._map._targets[stamp(targetEl)];
 		return this;
 	},
 
@@ -147,11 +151,11 @@ L.Layer = L.Evented.extend({
  *
  * @section Methods for Layers and Controls
  */
-L.Map.include({
+Map.include({
 	// @method addLayer(layer: Layer): this
 	// Adds the given layer to the map
 	addLayer: function (layer) {
-		var id = L.stamp(layer);
+		var id = stamp(layer);
 		if (this._layers[id]) { return this; }
 		this._layers[id] = layer;
 
@@ -169,7 +173,7 @@ L.Map.include({
 	// @method removeLayer(layer: Layer): this
 	// Removes the given layer from the map.
 	removeLayer: function (layer) {
-		var id = L.stamp(layer);
+		var id = stamp(layer);
 
 		if (!this._layers[id]) { return this; }
 
@@ -196,7 +200,7 @@ L.Map.include({
 	// @method hasLayer(layer: Layer): Boolean
 	// Returns `true` if the given layer is currently added to the map
 	hasLayer: function (layer) {
-		return !!layer && (L.stamp(layer) in this._layers);
+		return !!layer && (stamp(layer) in this._layers);
 	},
 
 	/* @method eachLayer(fn: Function, context?: Object): this
@@ -215,7 +219,7 @@ L.Map.include({
 	},
 
 	_addLayers: function (layers) {
-		layers = layers ? (L.Util.isArray(layers) ? layers : [layers]) : [];
+		layers = layers ? (isArray(layers) ? layers : [layers]) : [];
 
 		for (var i = 0, len = layers.length; i < len; i++) {
 			this.addLayer(layers[i]);
@@ -224,13 +228,13 @@ L.Map.include({
 
 	_addZoomLimit: function (layer) {
 		if (isNaN(layer.options.maxZoom) || !isNaN(layer.options.minZoom)) {
-			this._zoomBoundLayers[L.stamp(layer)] = layer;
+			this._zoomBoundLayers[stamp(layer)] = layer;
 			this._updateZoomLevels();
 		}
 	},
 
 	_removeZoomLimit: function (layer) {
-		var id = L.stamp(layer);
+		var id = stamp(layer);
 
 		if (this._zoomBoundLayers[id]) {
 			delete this._zoomBoundLayers[id];
