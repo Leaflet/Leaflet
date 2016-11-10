73182ada6ebb94c70b87e7ce6dbcfd535769921c
Declan Sander <dms497@cornell.edu>
2016-11-09 22:09:53 -0500

    added opacity to CircleMarker

--- spec/suites/layer/vector/CircleMarkerSpec.js
+++ spec/suites/layer/vector/CircleMarkerSpec.js
@@ -8,7 +8,7 @@
 		describe("when a CircleMarker is added to the map ", function () {
 			describe("with a radius set as an option", function () {
 				it("takes that radius", function () {
-					var marker = L.circleMarker([0, 0], {radius: 20}).addTo(map);
+					var marker = L.circleMarker([0, 0], {radius: 20, opacity: 1}).addTo(map);
 
 					expect(marker._radius).to.be(20);
 				});
@@ -16,7 +16,7 @@
 
 			describe("and radius is set before adding it", function () {
 				it("takes that radius", function () {
-					var marker = L.circleMarker([0, 0], {radius: 20});
+					var marker = L.circleMarker([0, 0], {radius: 20, opacity: 1});
 					marker.setRadius(15);
 					marker.addTo(map);
 					expect(marker._radius).to.be(15);
@@ -25,7 +25,7 @@
 
 			describe("and radius is set after adding it", function () {
 				it("takes that radius", function () {
-					var marker = L.circleMarker([0, 0], {radius: 20});
+					var marker = L.circleMarker([0, 0], {radius: 20, opacity: 1});
 					marker.addTo(map);
 					marker.setRadius(15);
 					expect(marker._radius).to.be(15);
@@ -34,18 +34,72 @@
 
 			describe("and setStyle is used to change the radius after adding", function () {
 				it("takes the given radius", function () {
-					var marker = L.circleMarker([0, 0], {radius: 20});
+					var marker = L.circleMarker([0, 0], {radius: 20, opacity: 1});
 					marker.addTo(map);
 					marker.setStyle({radius: 15});
 					expect(marker._radius).to.be(15);
 				});
 			});
+
 			describe("and setStyle is used to change the radius before adding", function () {
 				it("takes the given radius", function () {
-					var marker = L.circleMarker([0, 0], {radius: 20});
+					var marker = L.circleMarker([0, 0], {radius: 20, opacity: 1});
 					marker.setStyle({radius: 15});
 					marker.addTo(map);
 					expect(marker._radius).to.be(15);
+				});
+			});
+		});
+	});
+
+	describe("#_opacity", function () {
+		var map;
+		beforeEach(function () {
+			map = L.map(document.createElement('div'));
+			map.setView([0, 0], 1);
+		});
+		describe("when a circleMarker is added to the map", function () {
+			describe("with a opacity set as an option", function () {
+				it("takes that opacity", function () {
+					var marker = L.circleMarker([0, 0], {radius: 20, opacity: 0.5}).addTo(map);
+
+					expect(marker._opacity).to.be(0.5);
+				});
+			});
+
+			describe("and opacity is set before adding it", function () {
+				it("takes that opacity", function () {
+					var marker = L.circleMarker([0, 0], {radius: 20, opacity: 0.5});
+					marker.setOpacity(0.7);
+					marker.addTo(map);
+					expect(marker._opacity).to.be(0.7);
+				});
+			});
+
+			describe("and opacity is set after adding it", function () {
+				it("takes that opacity", function () {
+					var marker = L.circleMarker([0, 0], {radius: 20, opacity: 0.5});
+					marker.addTo(map);
+					marker.setOpacity(0.7);
+					expect(marker._opacity).to.be(0.7);
+				});
+			});
+
+			describe("and setStyle is used to change the radius after adding", function () {
+				it("takes the given opacity", function () {
+					var marker = L.circleMarker([0, 0], {radius: 20, opacity: 0.5});
+					marker.addTo(map);
+					marker.setStyle({opacity: 0.7});
+					expect(marker._opacity).to.be(0.7);
+				});
+			});
+
+			describe("and setStyle is used to change the radius before adding", function () {
+				it("takes the given opacity", function () {
+					var marker = L.circleMarker([0, 0], {radius: 20, opacity: 0.5});
+					marker.setStyle({opacity: 0.7});
+					marker.addTo(map);
+					expect(marker._opacity).to.be(0.7);
 				});
 			});
 		});
