db7d6afec1032f948ad47d6b97ceb58c41ab8cb0
Damian Moore <damian@epixstudios.co.uk>
2017-05-07 22:25:34 +0100

    Using proper minus character in the zoom control which means CSS workaround isn't needed

--- src/control/Control.Zoom.js
+++ src/control/Control.Zoom.js
@@ -26,9 +26,9 @@ export var Zoom = Control.extend({
 		// The title set on the 'zoom in' button.
 		zoomInTitle: 'Zoom in',
 
-		// @option zoomOutText: String = '-'
+		// @option zoomOutText: String = '−'
 		// The text set on the 'zoom out' button.
-		zoomOutText: '-',
+		zoomOutText: '−',
 
 		// @option zoomOutTitle: String = 'Zoom out'
 		// The title set on the 'zoom out' button.
