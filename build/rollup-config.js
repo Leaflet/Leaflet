276d6df153766f5498a08c74e03e6881952f16cf
Per Liedman <per@liedman.net>
2017-05-10 10:04:18 +0200

    Update copyright banner to the fact that it's in fact 2017 now (#5503)

--- build/rollup-config.js
+++ build/rollup-config.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'release') {
 
 const banner = `/*
  * Leaflet ` + version + `, a JS library for interactive maps. http://leafletjs.com
- * (c) 2010-2016 Vladimir Agafonkin, (c) 2010-2011 CloudMade
+ * (c) 2010-2017 Vladimir Agafonkin, (c) 2010-2011 CloudMade
  */`;
 
 export default {
