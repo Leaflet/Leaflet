a52efb40360a1a8f9d9e263661a6a992d57fdef2
ghybs <ghybs@users.noreply.github.com>
2017-05-24 08:19:08 +0400

    Docs(Marker): modify icon option description
    To explain that a _common_ / generic instance of `L.Icon.Default` is used, instead of leaving room to think that a _new instance_ of `L.Icon.Default` is created for every Marker without specified `icon` option.
    
    See https://github.com/Leaflet/Leaflet.markercluster/issues/786#issuecomment-302893446

--- src/layer/marker/Marker.js
+++ src/layer/marker/Marker.js
@@ -24,7 +24,9 @@ export var Marker = Layer.extend({
 	// @aka Marker options
 	options: {
 		// @option icon: Icon = *
-		// Icon class to use for rendering the marker. See [Icon documentation](#L.Icon) for details on how to customize the marker icon. If not specified, a new `L.Icon.Default` is used.
+		// Icon class to use for rendering the marker.
+		// See [Icon documentation](#L.Icon) for details on how to customize the marker icon.
+		// If not specified, a common instance of `L.Icon.Default` is used.
 		icon: new IconDefault(),
 
 		// Option inherited from "Interactive layer" abstract class
