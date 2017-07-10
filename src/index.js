ca458b01eb3e2f3711715dd6935b165bb3179091
ghybs <ghybs1@gmail.com>
2017-07-10 16:31:40 +0400

    Style(ESindex): add whitespaces
    for lint to be happy!

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@
 // do not understand JSON.
 // We could use a similar workaround as d3, which builds a dedicated module out of package.json using json2module
 // https://www.npmjs.com/package/json2module
-//import {version} from '../package.json';
-//export {version};
+// import {version} from '../package.json';
+// export {version};
 
 // control
 export * from './control/index';
@@ -31,15 +31,15 @@ export * from './map/index';
 
 // Module loaders do not need no conflict mode.
 // This is managed by the application developer when importing the module, if necessary.
-/*var oldL = window.L;
+/* var oldL = window.L;
 export function noConflict() {
 	window.L = oldL;
 	return this;
-}*/
+} */
 
 // Ideally no longer needed.
 // The application developer directly uses the imported classes and namespaces.
 // Nevertheless there is an issue for legacy plugins which expect L to be globally available.
 // It may be possible to provide a very simple patch to be imported when necessary.
 // Always export us to window global (see #2364)
-//window.L = exports;
+// window.L = exports;
