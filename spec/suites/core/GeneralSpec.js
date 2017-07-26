0a6c500e58597f89e358b417ddbb9f7889a3fc1c
Per Liedman <per@liedman.net>
2017-07-26 11:51:35 +0200

    Add test for extending L namespace

--- spec/suites/core/GeneralSpec.js
+++ spec/suites/core/GeneralSpec.js
@@ -11,7 +11,9 @@ describe('General', function () {
 
 	describe('namespace extension', function () {
 		L.Util.foo = 'bar';
+		L.Foo = 'Bar';
 
 		expect(L.Util.foo).to.eql('bar');
+		expect(L.Foo).to.eql('Bar');
 	});
 });
