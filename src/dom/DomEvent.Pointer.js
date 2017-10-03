11cbfa809576c3c6e7f3bcb8631e6cf6fc06c685
daverayment <dave.rayment@gmail.com>
2017-10-03 15:43:12 +0100

    Update DomPointer.js to revert typo (#5817)
    Restore IE-specific pointerType check which was accidentally removed previously. Fixes #5798.

--- src/dom/DomEvent.Pointer.js
+++ src/dom/DomEvent.Pointer.js
@@ -55,7 +55,7 @@ export function removePointerListener(obj, type, id) {
 
 function _addPointerStart(obj, handler, id) {
 	var onDown = Util.bind(function (e) {
-		if (e.pointerType !== 'mouse' && e.pointerType !== e.MSPOINTER_TYPE_MOUSE && e.pointerType !== e.MSPOINTER_TYPE_MOUSE) {
+		if (e.pointerType !== 'mouse' && e.MSPOINTER_TYPE_MOUSE && e.pointerType !== e.MSPOINTER_TYPE_MOUSE) {
 			// In IE11, some touch events needs to fire for form controls, or
 			// the controls will stop working. We keep a whitelist of tag names that
 			// need these events. For other target tags, we prevent default on the event.
