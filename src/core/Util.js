db75417f44719c14eeb0bb33902a2d91884a279d
GoToLoop <GoToLoop@users.noreply.github.com>
2018-02-10 16:24:43 -0200

    Make Util.isArray accept arraylike objects
    It'd be very nice if typed arrays and other arraylike objects would be valid to pass to Leaflet's methods too.
    My propose is: `return obj && obj.length >= 0 && typeof obj === 'object';`.
    But any other conditional approach would be great too. What matters is typed arrays to be valid arguments.

--- src/core/Util.js
+++ src/core/Util.js
@@ -178,8 +178,9 @@ export function template(str, data) {
 
 // @function isArray(obj): Boolean
 // Compatibility polyfill for [Array.isArray](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray)
-export var isArray = Array.isArray || function (obj) {
-	return (Object.prototype.toString.call(obj) === '[object Array]');
+export function isArray(obj) {
+	return obj && obj.length >= 0 && typeof obj === 'object';
+	//return (Object.prototype.toString.call(obj) === '[object Array]');
 };
 
 // @function indexOf(array: Array, el: Object): Number
