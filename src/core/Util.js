5b3dae8761a7d65fc11fe4a1fca945beb56bec96
GoToLoop <GoToLoop@users.noreply.github.com>
2018-02-10 19:50:47 -0200

    typeof obj==='object'&&obj!==null&&obj.length>=0
    return typeof obj === 'object' && obj !== null && obj.length >= 0;

--- src/core/Util.js
+++ src/core/Util.js
@@ -179,7 +179,7 @@ export function template(str, data) {
 // @function isArray(obj): Boolean
 // Compatibility polyfill for [Array.isArray](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray)
 export function isArray(obj) {
-	return obj !== undefined && obj !== null && obj.length >= 0 && typeof obj === 'object';
+	return typeof obj === 'object' && obj !== null && obj.length >= 0;
 	// return (Object.prototype.toString.call(obj) === '[object Array]');
 }
 
