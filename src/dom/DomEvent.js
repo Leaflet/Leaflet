dbafb0f6c1615be6fdbebaf80585c049b86fa5ac
ghybs <ghybs1@gmail.com>
2018-02-14 16:13:15 +0400

    Docs(getMousePosition): explicitly exclude border

--- src/dom/DomEvent.js
+++ src/dom/DomEvent.js
@@ -214,7 +214,7 @@ export function stop(e) {
 
 // @function getMousePosition(ev: DOMEvent, container?: HTMLElement): Point
 // Gets normalized mouse position from a DOM event relative to the
-// `container` or to the whole page if not specified.
+// `container` (border excluded) or to the whole page if not specified.
 export function getMousePosition(e, container) {
 	if (!container) {
 		return new Point(e.clientX, e.clientY);
