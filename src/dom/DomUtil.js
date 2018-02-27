f8bee80e68408883cfc253a249ce2c52e3fce5ac
ghybs <ghybs1@gmail.com>
2018-02-27 17:29:49 +0400

    Feat(DomUtil): add getScale function
    so that it can be used by DomEvent.getMousePosition and Draggable.

--- src/dom/DomUtil.js
+++ src/dom/DomUtil.js
@@ -328,3 +328,17 @@ export function getSizedParentNode(element) {
 	} while ((!element.offsetWidth || !element.offsetHeight) && element !== document.body);
 	return element;
 }
+
+// @function getScale(el: HTMLElement): Object
+// Computes the CSS scale currently applied on the element.
+// Returns an object with `x` and `y` members as horizontal and vertical scales respectively,
+// and `boundingClientRect` as the result of [`getBoundingClientRect()`](https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect).
+export function getScale(element) {
+	var rect = element.getBoundingClientRect(); // Read-only in old browsers.
+
+	return {
+		x: rect.width / element.offsetWidth || 1,
+		y: rect.height / element.offsetHeight || 1,
+		boundingClientRect: rect
+	};
+}
