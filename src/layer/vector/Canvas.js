bdc22033f25146d98247cb4ae59bf9c4e352beff
McBen <viertelvor12@gmx.net>
2018-06-22 10:06:32 +0200

    Canvas: allow reset of dashArray and allow array-type as option

--- src/layer/vector/Canvas.js
+++ src/layer/vector/Canvas.js
@@ -187,7 +187,7 @@ export var Canvas = Renderer.extend({
 	},
 
 	_updateDashArray: function (layer) {
-		if (layer.options.dashArray) {
+		if (typeof layer.options.dashArray === 'string') {
 			var parts = layer.options.dashArray.split(','),
 			    dashArray = [],
 			    i;
@@ -195,6 +195,8 @@ export var Canvas = Renderer.extend({
 				dashArray.push(Number(parts[i]));
 			}
 			layer.options._dashArray = dashArray;
+		} else {
+			layer.options._dashArray = layer.options.dashArray;
 		}
 	},
 
