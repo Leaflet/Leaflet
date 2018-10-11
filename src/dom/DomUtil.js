479295e7b2f0bf3244c2335386efce0c1f1a1aaa
Schleuse <r.schleuse@gmail.com>
2018-10-11 21:16:06 +0200

    Fix #6335 by specifically checking for correspondingElement

--- src/dom/DomUtil.js
+++ src/dom/DomUtil.js
@@ -149,6 +149,10 @@ export function setClass(el, name) {
 // @function getClass(el: HTMLElement): String
 // Returns the element's class.
 export function getClass(el) {
+	// Check if the element is an SVGElementInstance and use the correspondingElement instead
+	if (el.correspondingElement) {
+		el = el.correspondingElement;
+	}
 	return el.className.baseVal === undefined ? el.className : el.className.baseVal;
 }
 
