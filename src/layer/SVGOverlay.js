61dbdcfbf2767c809022c5a0b22f5ac9e181b2a3
Andrea Faggin <andrea.faggin@develondigital.com>
2019-02-18 10:28:59 +0100

    Changed parameter name, added comment

--- src/layer/SVGOverlay.js
+++ src/layer/SVGOverlay.js
@@ -22,54 +22,25 @@ import * as Util from '../core/Util';
 
 export var SVGOverlay = ImageOverlay.extend({
 	_initImage: function () {
-		var isElement = this._url.tagName === 'svg';
-		var el = this._image = isElement ? this._url : _getSVGElement(this._url);
-
 		var el = this._image = this._url;
-
-
 
 		DomUtil.addClass(el, 'leaflet-image-layer');
 		if (this._zoomAnimated) { DomUtil.addClass(el, 'leaflet-zoom-animated'); }
 
 		el.onselectstart = Util.falseFn;
 		el.onmousemove = Util.falseFn;
-	},
+	}
 
 	// @method getElement(): SVGElement
 	// Returns the instance of [`SVGElement`](https://developer.mozilla.org/docs/Web/API/SVGElement)
 	// used by this overlay.
-
-
-	// _getSVGElement(url): string
-	// Gets the svg image from url with ['XMLHTTPRequest'](https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest)
-
-
-	_getSVGElement(url) {
-		var request = new XMLHttpRequest();
-		request.addEventListener('load', this._parseSVGElement);
-		request.addEventListener('error', function(error) {
-			console.error('SVGOverlay loading file error ', error)
-		});
-
-		request.open('GET', url);
-
-
-	},
-
-	// _parseSVGElement(content): SVGElement
-	// Parses a given string to an SVGElement with ['DOMParser'](https://developer.mozilla.org/en-US/docs/Web/API/DOMParser) API
-
-	_parseSVGElement(content) {
-
-	}
 });
 
 
 // @factory L.svgOverlay(svg: String|SVGElement, bounds: LatLngBounds, options?: SVGOverlay options)
-// Instantiates an image overlay object given the URL of the SVG image (or an SVG root tag) and the
-// geographical bounds it is tied to.
+// Instantiates an image overlay object given an SVG element and the geographical bounds it is tied to.
+// A viewBox attribute is required on the SVG element to zoom in and out properly.
 
-export function svgOverlay(image, bounds, options) {
-	return new SVGOverlay(image, bounds, options);
+export function svgOverlay(el, bounds, options) {
+	return new SVGOverlay(el, bounds, options);
 }
