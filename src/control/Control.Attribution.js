ec209b5b42b96bc92c0f573580e860472e6ef13c
Will Selway <wills@bookmygarage.com>
2019-04-08 15:34:19 +0100

    Changed attribution control default link to HTTPS

--- src/control/Control.Attribution.js
+++ src/control/Control.Attribution.js
@@ -21,7 +21,7 @@ export var Attribution = Control.extend({
 
 		// @option prefix: String = 'Leaflet'
 		// The HTML text shown before the attributions. Pass `false` to disable.
-		prefix: '<a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a>'
+		prefix: '<a href="https://leafletjs.com" title="A JS library for interactive maps">Leaflet</a>'
 	},
 
 	initialize: function (options) {
