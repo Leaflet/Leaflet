83c76f3d28d3b8be5efcbba6fadbcdc59580114c
BenTalagan <ben_talagan@github.nospam.com>
2019-09-06 12:27:40 +0200

    Fix race condition in Marker._setPos when _icon is not present

--- src/layer/marker/Marker.js
+++ src/layer/marker/Marker.js
@@ -287,7 +287,10 @@ export var Marker = Layer.extend({
 	},
 
 	_setPos: function (pos) {
-		DomUtil.setPosition(this._icon, pos);
+
+		if (this._icon) {
+			DomUtil.setPosition(this._icon, pos);
+		}
 
 		if (this._shadow) {
 			DomUtil.setPosition(this._shadow, pos);
