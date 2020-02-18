f662d44af5170bb5ba4ffb8a9b644c9569bcdd66
johnd0e <johnd0e@mail.ua>
2020-02-18 16:03:04 +0200

    Fix exception when calling LayerGroup/hasLayer() with wrong layerId (#6998)
    Now `false` returned, as per docs

--- src/layer/LayerGroup.js
+++ src/layer/LayerGroup.js
@@ -73,7 +73,9 @@ export var LayerGroup = Layer.extend({
 	// @method hasLayer(id: Number): Boolean
 	// Returns `true` if the given internal ID is currently added to the group.
 	hasLayer: function (layer) {
-		return !!layer && (layer in this._layers || this.getLayerId(layer) in this._layers);
+		if (!layer) { return false; }
+		var layerId = typeof layer === 'number' ? layer : this.getLayerId(layer);
+		return layerId in this._layers;
 	},
 
 	// @method clearLayers(): this
