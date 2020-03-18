Leaflet/Leaflet

commit 19d2bd027ec1616f05759224f3c53081b6fad6af

Be more picky about pointer events (blind attempt to fix #5180) (#5185)

src/dom/DomEvent.Pointer.js
@@ -50,7 +50,7 @@ L.extend(L.DomEvent, {
 
 	_addPointerStart: function (obj, handler, id) {
 		var onDown = L.bind(function (e) {
-			if (e.pointerType !== 'mouse' && e.pointerType !== e.MSPOINTER_TYPE_MOUSE) {
+			if (e.pointerType !== 'mouse' && e.MSPOINTER_TYPE_MOUSE && e.pointerType !== e.MSPOINTER_TYPE_MOUSE) {
 				// In IE11, some touch events needs to fire for form controls, or
 				// the controls will stop working. We keep a whitelist of tag names that
 				// need these events. For other target tags, we prevent default on the event.

src/dom/DomEvent.DoubleTap.js
@@ -17,6 +17,9 @@ L.extend(L.DomEvent, {
 			var count;
 
 			if (L.Browser.pointer) {
+				if (!L.Browser.edge && e.pointerType === 'mouse') {
+					return;
+				}
 				count = L.DomEvent._pointersCount;
 			} else {
 				count = e.touches.length;

commit 9b1c8d22f704af65c17a95e4c330c926a1cfdb3d

Fix bug introduced in 19d2bd027ec1616f05759224f3c53081b6fad6af (brackets missing)
The condition was never met: `e.pointerType !== 'mouse' && e.MSPOINTER_TYPE_MOUSE`

src/dom/DomEvent.Pointer.js
@@ -55,15 +55,14 @@ export function removePointerListener(obj, type, id) {
 
 function _addPointerStart(obj, handler, id) {
 	var onDown = Util.bind(function (e) {
-		if (e.pointerType !== 'mouse' && e.MSPOINTER_TYPE_MOUSE && e.pointerType !== e.MSPOINTER_TYPE_MOUSE) {
+		if (e.pointerType !== (e.MSPOINTER_TYPE_MOUSE || 'mouse')) {
 			// In IE11, some touch events needs to fire for form controls, or
 			// the controls will stop working. We keep a whitelist of tag names that
 			// need these events. For other target tags, we prevent default on the event.
-			if (TAG_WHITE_LIST.indexOf(e.target.tagName) < 0) {
-				DomEvent.preventDefault(e);
-			} else {
+			if (Browser.ie && TAG_WHITE_LIST.indexOf(e.target.tagName)) {
 				return;
 			}
+			DomEvent.preventDefault(e);
 		}
 
 		_handlePointer(e, handler);
@@ -113,7 +112,9 @@ function _handlePointer(e, handler) {
 function _addPointerMove(obj, handler, id) {
 	var onMove = function (e) {
 		// don't fire touch moves when mouse isn't down
-		if ((e.pointerType === e.MSPOINTER_TYPE_MOUSE || e.pointerType === 'mouse') && e.buttons === 0) { return; }
+		if ((e.pointerType === (e.MSPOINTER_TYPE_MOUSE || 'mouse')) && e.buttons === 0) {
+			return;
+		}
 
 		_handlePointer(e, handler);
 	};