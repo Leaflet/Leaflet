Leaflet/Leaflet

commit 19d2bd027ec1616f05759224f3c53081b6fad6af

Be more picky about pointer events (blind attempt to fix #5180) (#5185)

src/dom/DomEvent.Pointer.js
@@ -50,7 +50,7 @@ L.extend(L.DomEvent, {
 
 	_addPointerStart: function (obj, handler, id) {
 		var onDown = L.bind(function (e) {
-			if (e.pointerType !== 'mouse' && e.pointerType !== e.MSPOINTER_TYPE_MOUSE) {
+			if (e.pointerType !== 'mouse' && e.MSPOINTER_TYPE_MOUSE && e.pointerType !== e.MSPOINTER_TYPE_MOUSE) {
 				// In IE11, some touch events needs to fire for form controls, or
 				// the controls will stop working. We keep a whitelist of tag names that
 				// need these events. For other target tags, we prevent default on the event.

src/dom/DomEvent.DoubleTap.js
@@ -17,6 +17,9 @@ L.extend(L.DomEvent, {
 			var count;
 
 			if (L.Browser.pointer) {
+				if (!L.Browser.edge && e.pointerType === 'mouse') {
+					return;
+				}
 				count = L.DomEvent._pointersCount;
 			} else {
 				count = e.touches.length;

commit 94d4ae25e728e4242d1cdca72200f684eb578d7b

fixup! Fix bug introduced in 19d2bd027ec1616f05759224f3c53081b6fad6af (brackets missing)
Co-Authored-By: Vladimir Agafonkin <agafonkin@gmail.com>

src/dom/DomEvent.Pointer.js
@@ -59,7 +59,7 @@ function _addPointerStart(obj, handler, id) {
 			// In IE11, some touch events needs to fire for form controls, or
 			// the controls will stop working. We keep a whitelist of tag names that
 			// need these events. For other target tags, we prevent default on the event.
-			if (Browser.ie && TAG_WHITE_LIST.indexOf(e.target.tagName)) {
+			if (Browser.ie && TAG_WHITE_LIST.indexOf(e.target.tagName) >= 0) {
 				return;
 			}
 			DomEvent.preventDefault(e);
@@ -132,4 +132,3 @@ function _addPointerEnd(obj, handler, id) {
 	obj.addEventListener(POINTER_UP, onUp, false);
 	obj.addEventListener(POINTER_CANCEL, onUp, false);
 }
-