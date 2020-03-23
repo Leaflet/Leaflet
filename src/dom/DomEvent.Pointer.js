c2b4d7e1aa78bc98af12c8493349969dc4dccd8f
johnd0e <johnd0e@mail.ua>
2020-03-23 12:00:54 +0200

    Fix invalid condition in _addPointerStart (#7033)
    * Fix bug introduced in 19d2bd027ec1616f05759224f3c53081b6fad6af (brackets missing)
    
    The condition was never met: `e.pointerType !== 'mouse' && e.MSPOINTER_TYPE_MOUSE`
    
    * fixup! Fix bug introduced in 19d2bd027ec1616f05759224f3c53081b6fad6af (brackets missing)
    
    Co-Authored-By: Vladimir Agafonkin <agafonkin@gmail.com>
    
    Co-authored-by: Vladimir Agafonkin <agafonkin@gmail.com>

--- src/dom/DomEvent.Pointer.js
+++ src/dom/DomEvent.Pointer.js
@@ -52,15 +52,14 @@ export function removePointerListener(obj, type, id) {
 
 function _addPointerStart(obj, handler, id) {
 	var onDown = Util.bind(function (e) {
-		if (e.pointerType !== 'mouse' && e.MSPOINTER_TYPE_MOUSE && e.pointerType !== e.MSPOINTER_TYPE_MOUSE) {
+		if (e.pointerType !== (e.MSPOINTER_TYPE_MOUSE || 'mouse')) {
 			// In IE11, some touch events needs to fire for form controls, or
 			// the controls will stop working. We keep a whitelist of tag names that
 			// need these events. For other target tags, we prevent default on the event.
-			if (TAG_WHITE_LIST.indexOf(e.target.tagName) < 0) {
-				DomEvent.preventDefault(e);
-			} else {
+			if (Browser.ie && TAG_WHITE_LIST.indexOf(e.target.tagName) >= 0) {
 				return;
 			}
+			DomEvent.preventDefault(e);
 		}
 
 		_handlePointer(e, handler);
@@ -108,7 +107,9 @@ function _handlePointer(e, handler) {
 function _addPointerMove(obj, handler, id) {
 	var onMove = function (e) {
 		// don't fire touch moves when mouse isn't down
-		if ((e.pointerType === e.MSPOINTER_TYPE_MOUSE || e.pointerType === 'mouse') && e.buttons === 0) { return; }
+		if ((e.pointerType === (e.MSPOINTER_TYPE_MOUSE || 'mouse')) && e.buttons === 0) {
+			return;
+		}
 
 		_handlePointer(e, handler);
 	};
@@ -126,4 +127,3 @@ function _addPointerEnd(obj, handler, id) {
 	obj.addEventListener(POINTER_UP, onUp, false);
 	obj.addEventListener(POINTER_CANCEL, onUp, false);
 }
-
