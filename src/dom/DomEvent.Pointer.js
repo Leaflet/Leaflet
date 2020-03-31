f0c6028927dba686f4a56107278ba763c42c606d
johndoe <johnd0e@mail.ua>
2020-03-31 15:47:53 +0300

    DomEvent.Pointer: process touch events only

--- src/dom/DomEvent.Pointer.js
+++ src/dom/DomEvent.Pointer.js
@@ -51,15 +51,15 @@ export function removePointerListener(obj, type, id) {
 
 function _addPointerStart(obj, handler, id) {
 	var onDown = function (e) {
-		if (e.pointerType !== (e.MSPOINTER_TYPE_MOUSE || 'mouse')) {
-			// In IE11, some touch events needs to fire for form controls, or
-			// the controls will stop working. We keep a whitelist of tag names that
-			// need these events. For other target tags, we prevent default on the event.
-			if (Browser.ie && TAG_WHITE_LIST.indexOf(e.target.tagName) >= 0) {
-				return;
-			}
-			DomEvent.preventDefault(e);
+		if (e.pointerType === (e.MSPOINTER_TYPE_MOUSE || 'mouse')) { return; }
+
+		// In IE11, some touch events needs to fire for form controls, or
+		// the controls will stop working. We keep a whitelist of tag names that
+		// need these events. For other target tags, we prevent default on the event.
+		if (Browser.ie && TAG_WHITE_LIST.indexOf(e.target.tagName) >= 0) {
+			return;
 		}
+		DomEvent.preventDefault(e);
 
 		_handlePointer(e, handler);
 	};
@@ -105,10 +105,7 @@ function _handlePointer(e, handler) {
 
 function _addPointerMove(obj, handler, id) {
 	var onMove = function (e) {
-		// don't fire touch moves when mouse isn't down
-		if ((e.pointerType === (e.MSPOINTER_TYPE_MOUSE || 'mouse')) && e.buttons === 0) {
-			return;
-		}
+		if (e.pointerType === (e.MSPOINTER_TYPE_MOUSE || 'mouse')) { return; }
 
 		_handlePointer(e, handler);
 	};
@@ -119,6 +116,8 @@ function _addPointerMove(obj, handler, id) {
 
 function _addPointerEnd(obj, handler, id) {
 	var onUp = function (e) {
+		if (e.pointerType === (e.MSPOINTER_TYPE_MOUSE || 'mouse')) { return; }
+
 		_handlePointer(e, handler);
 	};
 
