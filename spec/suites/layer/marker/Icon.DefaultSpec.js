3715c51e25f006541693300d3064f5a95c69bd7a
johndoe <johnd0e@mail.ua>
2020-04-14 18:42:40 +0300

    Icon.DefaultSpec.js: new test: detect icon images path
    Use beforeEach/afterEach
    Use map.remove() before detouching map container

--- spec/suites/layer/marker/Icon.DefaultSpec.js
+++ spec/suites/layer/marker/Icon.DefaultSpec.js
@@ -1,33 +1,44 @@
 describe("Icon.Default", function () {
 
-	it("icon measures 25x41px", function () {
-		var div = document.createElement('div');
+	var map, div;
+
+	beforeEach(function () {
+		div = document.createElement('div');
 		div.style.height = '100px';
 		document.body.appendChild(div);
+		map = L.map(div).setView([0, 0], 0);
+	});
 
-		var map = L.map(div).setView([0, 0], 0);
+	afterEach(function () {
+		map.remove();
+		document.body.removeChild(div);
+	});
 
+	it("detect icon images path", function () {
+		var origPath = L.Icon.Default.imagePath; // set in after.js
+		expect(origPath).to.be.ok();
+		delete L.Icon.Default.imagePath;
+		var marker = new L.Marker([0, 0]);
+
+		expect(L.Icon.Default.imagePath).to.not.be.ok();
+		marker.addTo(map);
+		expect(L.Icon.Default.imagePath).to.equal(document.location.origin + origPath);
+	});
+
+	it("icon measures 25x41px", function () {
 		var marker = new L.Marker([0, 0]).addTo(map);
 
 		var img = map.getPane('markerPane').querySelector('img');
 		expect(img.clientHeight).to.be(41);
 		expect(img.clientWidth).to.be(25);
-		document.body.removeChild(div);
 	});
 
 	it("shadow measures 41x41px", function () {
-		var div = document.createElement('div');
-		div.style.height = '100px';
-		document.body.appendChild(div);
-
-		var map = L.map(div).setView([0, 0], 0);
-
 		var marker = new L.Marker([0, 0]).addTo(map);
 
 		var img = map.getPane('shadowPane').querySelector('img');
 		expect(img.clientHeight).to.be(41);
 		expect(img.clientWidth).to.be(41);
-		document.body.removeChild(div);
 	});
 
 });
