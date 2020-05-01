50c7e7408f55835cd9ef10e7b0a21925038ad3e2
johndoe <johnd0e@mail.ua>
2020-05-01 18:10:43 +0300

    karma.conf.js: change reporter to default ('progress')
    Because 'dots' is confusing when mixed up with different warnings.
    
    But for CI script 'dots' fits better due to travis-ci log qualities.

--- spec/karma.conf.js
+++ spec/karma.conf.js
@@ -69,7 +69,7 @@ module.exports = function (config) {
 
 		// test results reporter to use
 		// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-		reporters: ['dots'],
+		// reporters: ['dots'],
 
 		// web server port
 		port: 9876,
