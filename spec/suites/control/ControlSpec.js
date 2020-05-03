7e1f1b5ecce4ce7277bfdf219fe0ee05554b508e
johndoe <johnd0e@mail.ua>
2020-05-03 12:55:43 +0300

    Test to ensure that Control click and scroll are not propagated to map

--- spec/suites/control/ControlSpec.js
+++ spec/suites/control/ControlSpec.js
@@ -1,26 +1,32 @@
 describe("Control", function () {
-	var map;
-
-	beforeEach(function () {
-		map = L.map(document.createElement('div'));
-	});
-
 	function onAdd() {
 		return L.DomUtil.create('div', 'leaflet-test-control');
 	}
 
+	var map,
+	    container,
+	    control;
+
+	beforeEach(function () {
+		container = document.createElement('div');
+		document.body.appendChild(container);
+		map = L.map(container).setView([0, 0], 1);
+		control = new L.Control();
+		control.onAdd = onAdd;
+		control.addTo(map);
+	});
+
+	afterEach(function () {
+		map.remove();
+		document.body.removeChild(container);
+	});
+
 	describe("#addTo", function () {
 		it("adds the container to the map", function () {
-			var control = new L.Control();
-			control.onAdd = onAdd;
-			control.addTo(map);
 			expect(map.getContainer().querySelector('.leaflet-test-control')).to.equal(control.getContainer());
 		});
 
 		it("removes the control from any existing map", function () {
-			var control = new L.Control();
-			control.onAdd = onAdd;
-			control.addTo(map);
 			control.addTo(map);
 			expect(map.getContainer().querySelectorAll('.leaflet-test-control').length).to.equal(1);
 			expect(map.getContainer().querySelector('.leaflet-test-control')).to.equal(control.getContainer());
@@ -29,23 +35,43 @@ describe("Control", function () {
 
 	describe("#remove", function () {
 		it("removes the container from the map", function () {
-			var control = new L.Control();
-			control.onAdd = onAdd;
-			control.addTo(map).remove();
+			control.remove();
 			expect(map.getContainer().querySelector('.leaflet-test-control')).to.equal(null);
 		});
 
 		it("calls onRemove if defined", function () {
-			var control = new L.Control();
-			control.onAdd = onAdd;
 			control.onRemove = sinon.spy();
-			control.addTo(map).remove();
+			control.remove();
 			expect(control.onRemove.called).to.be(true);
 		});
 
 		it("is a no-op if the control has not been added", function () {
 			var control = new L.Control();
 			expect(control.remove()).to.equal(control);
+		});
+	});
+
+	describe("DOM events bubbling:", function () {
+		it("click is not propagated to map", function () {
+			var spy = sinon.spy();
+			map.on('click', spy); // disableClickPropagation
+			L.DomEvent.on(container, 'mousedown touchstart dblclick', spy);
+			happen.click(control._container);
+			happen.dblclick(control._container);
+			happen.mousedown(control._container);
+			happen.mouseup(control._container);
+			happen.touchstart(control._container, {touches:[]});
+			expect(spy.called).to.not.be.ok();
+		});
+
+		it("scroll is not propagated to map", function () {
+			var control = new L.Control();
+			control.onAdd = onAdd;
+			control.addTo(map);
+			var spy = sinon.spy();
+			L.DomEvent.on(container, 'wheel', spy); // disableScrollPropagation
+			happen.once(control._container, {type: 'wheel', detail: -100});
+			expect(spy.called).to.not.be.ok();
 		});
 	});
 });
