5f660fb8cf646d0a564ef313296061fe2cbaa1d2
bozdoz <howaboutben@gmail.com>
2020-11-03 10:27:42 -0400

    revert whitespace change

--- spec/before.js
+++ spec/before.js
@@ -1,5 +1,5 @@
 // Trick Leaflet into believing we have a touchscreen (for desktop)
-if (window.TouchEvent) { window.ontouchstart = function () {}; }
+if (window.TouchEvent) { window.ontouchstart = function(){} };
 
 // allow tests for `detectRetina`
 window.devicePixelRatio = 2;
