d060ef2be58548a2b56bd8865d1ab678b6a70046
stell <info@prime-real.de>
2020-12-23 23:19:15 +0100

    Update PosAnimation.js
    Small changes

--- src/dom/PosAnimation.js
+++ src/dom/PosAnimation.js
@@ -13,18 +13,17 @@ import * as DomUtil from '../dom/DomUtil';
  * ```js
  * var myPositionMarker = L.marker([48.864716, 2.294694]).addTo(map);
  * 
- * myPositionMarker.on("click", function(){
- * 	var pos = map.latLngToLayerPoint(myPositionMarker.getLatLng())
+ * myPositionMarker.on("click", function() {
+ * 	var pos = map.latLngToLayerPoint(myPositionMarker.getLatLng());
  * 	pos.y -= 25;
  * 	var fx = new L.PosAnimation();
  * 
- * 	fx.once('end',function(){
+ * 	fx.once('end',function() {
  * 		pos.y += 25;
  * 		fx.run(myPositionMarker._icon, pos, 0.8);
  * 	});
  * 
  * 	fx.run(myPositionMarker._icon, pos, 0.3);
- * 
  * });
  * 
  * ```
