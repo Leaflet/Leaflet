b261c2306947e9f305a589f390d74dd934bd71e5
Dorian Benedetti <dorian.benedetti@etu.u-bordeaux.fr>
2021-01-29 09:05:44 +0100

    add getStyle, create, remove, empty, toFront toBack test

--- spec/suites/dom/DomUtilSpec.js
+++ spec/suites/dom/DomUtilSpec.js
@@ -56,4 +56,97 @@ describe('DomUtil', function () {
 	// describe('#setPosition', noSpecs);
 
 	// describe('#getStyle', noSpecs);
+	describe("#getStyle", function () {
+		it('should return corresponding style if defined into HTML element', function () {
+			expect(L.DomUtil.getStyle(el, "position")).to.eql("absolute");
+		});
+		it("should return empty string is style isn't defined into HTML element", function () {
+			var e = document.createElement('div');
+			expect(L.DomUtil.getStyle(e, "position")).to.be('');
+		});
+		it("should return undefined is style don't exist into HTML element or default css", function () {
+			expect(L.DomUtil.getStyle(el, "random_name_for_style")).to.be(undefined);
+		});
+	});
+
+	describe("#create", function () {
+		it("should create an HTML element div without any class name", function () {
+			var e = L.DomUtil.create("div");
+			expect(e.className).to.eql('');
+			expect(e.tagName).to.eql("DIV");
+		});
+		it("should create an HTML element div with specified class name", function () {
+			var e = L.DomUtil.create("div", "test");
+			expect(e.className).to.eql('test');
+			expect(e.tagName).to.eql("DIV");
+		});
+		it("should create an HTML element <p> with a div for parent", function () {
+			var parent = L.DomUtil.create("div");
+			var child = L.DomUtil.create("p", "test", parent);
+			expect(child.parentNode === parent).to.be(true);
+		});
+	});
+
+	describe("#remove", function () {
+		it("should remove specified element", function () {
+			var e = L.DomUtil.create("div", "test", el);
+			L.DomUtil.remove(e);
+			expect(el.contains(e)).to.be(false);
+		});
+		it("should do nothing if the element hasn't parent (in other way, it doesn't exist)", function () {
+			var e = L.DomUtil.create("div", "test");
+			L.DomUtil.remove(e);
+			expect(document.body.contains(e)).to.be(false);
+		});
+	});
+
+	describe("#empty", function () {
+		it("should remove all children of element", function () {
+			L.DomUtil.create("div", "test", el);
+			L.DomUtil.create("div", "test1", el);
+			L.DomUtil.create("div", "test2", el);
+			L.DomUtil.empty(el);
+			expect(el.childNodes.length).to.be(0);
+		});
+		it("should do nothing if element doesn't have children", function () {
+			expect(el.childNodes.length).to.be(0);
+			L.DomUtil.empty(el);
+			expect(el.childNodes.length).to.be(0);
+		});
+	});
+
+	describe("#toFront", function () {
+		it("should place the first child of element to the last position", function () {
+			var e1 = L.DomUtil.create("div", "test", el);
+			L.DomUtil.create("div", "test1", el);
+			L.DomUtil.create("div", "test2", el);
+			expect(el.lastChild).to.not.eql(e1);
+			L.DomUtil.toFront(e1);
+			expect(el.lastChild).to.eql(e1);
+		});
+		it("shouldn't move an element if he's already in the last position", function () {
+			L.DomUtil.create("div", "test", el);
+			L.DomUtil.create("div", "test1", el);
+			var e1 = L.DomUtil.create("div", "test2", el);
+			expect(el.lastChild).to.eql(e1);
+			L.DomUtil.toFront(e1);
+			expect(el.lastChild).to.eql(e1);
+		});
+		it("shouldn't crash if element doesn't have a parent", function () {
+			var e = L.DomUtil.create("div");
+			L.DomUtil.toFront(e);
+		});
+	});
+
+	// describe("#toback", function () {
+	// 	it("should place the lastChild of an HTML element to the last position", function () {
+	// 		L.DomUtil.create("div", "test", el);
+	// 		L.DomUtil.create("div", "test1", el);
+	// 		var e1 = L.DomUtil.create("div", "test2", el);
+	// 		expect(el.firstChild).to.not.eql(e1);
+	// 		L.DomUtil.toBack(el);
+	// 		expect(el.firstChild).to.eql(e1);
+	// 	});
+
+	// });
 });
