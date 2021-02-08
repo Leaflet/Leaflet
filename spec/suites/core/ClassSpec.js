ce638c72cbd3849976f20829bcf8ae17bae86e0d
falke-design <design.falke@gmail.com>
2021-02-08 20:31:43 +0100

    Break the reference to the options of the Class prototype

--- spec/suites/core/ClassSpec.js
+++ spec/suites/core/ClassSpec.js
@@ -117,6 +117,15 @@ describe("Class", function () {
 			expect(K2.prototype.options.foo).to.eql('bar');
 		});
 
+		it("prevents change of prototype options", function () {
+			var K1 = L.Class.extend({options: {}});
+			var rect = L.rectangle([[0, 0], [0, 0]]);
+			rect.klass = new K1(rect);
+			rect.klass.options.foo = 'bar';
+			expect(K1.prototype.options.foo).to.not.eql('bar');
+			expect(rect.klass.options.foo).to.eql('bar');
+		});
+
 		it("adds constructor hooks correctly", function () {
 			var spy1 = sinon.spy();
 
