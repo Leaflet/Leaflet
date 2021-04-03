7edcd194e277ea37a0981c9adc16d5a11327a213
lewis.george.norris@gmail.com <lewis.george.norris@gmail.com>
2021-04-03 17:42:00 +0100

    Added tests for getStyle, create & remote, and empty elements dom utils.

--- spec/suites/dom/DomUtilSpec.js
+++ spec/suites/dom/DomUtilSpec.js
@@ -52,7 +52,48 @@ describe('DomUtil', function () {
 			expect(el.className).to.eql('foo barz');
 		});
 	});
+  describe('#getStyle', function () {
+		it('gets the value for a certain style attribute on an element,', function () {
+			el.id = 'testId';
+			el.style.color = 'black';
+			expect(L.DomUtil.getStyle(el, 'color')).to.eql('black');
+			el.style.color = 'green';
+			expect(L.DomUtil.getStyle(el, 'color')).to.eql('green');
+		});
+	});
 
+	describe('#create, #remove, #empty', function () {
+		it('create a HTML element, sets a class, appends it to container element.', function () {
+			el.id = 'testId';
+			expect(el.children.length).to.equal(0);
+			L.DomUtil.create('h1', 'title', el);
+			expect(el.children.length).to.equal(1);
+		});
+		it('removes element from parent', function () {
+			el.id = 'testId';
+			expect(el.children.length).to.equal(0);
+			L.DomUtil.create('h1', 'title', el);
+			expect(el.children.length).to.equal(1);
+			L.DomUtil.remove(L.DomUtil.get(el.children[0]));
+			expect(el.children.length).to.equal(0);
+		});
+		it('removes all of the elements children elements', function () {
+			el.id = 'testId';
+			expect(el.children.length).to.equal(0);
+
+			[0, 1, 2, 3].forEach(function (num) {
+				L.DomUtil.create('div', 'childContainer' + num, el);
+			});
+			expect(el.children.length).to.equal(4);
+
+			L.DomUtil.remove(L.DomUtil.get(el.children[0]));
+			expect(el.children.length).to.equal(3);
+
+			L.DomUtil.empty(el);
+			expect(el.children.length).to.equal(0);
+		});
+	});
+  
 	// describe('#setPosition', noSpecs);
 
 	// describe('#getStyle', noSpecs);
