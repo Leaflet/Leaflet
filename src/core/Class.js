3e8de0feab9a382fd673370160f63b136672de2e
Sebastian Krüger <2pi_r2@gmx.de>
2021-09-01 10:54:47 +0200

    ✨feat (es6-class): rewrite base class

--- src/core/Class.js
+++ src/core/Class.js
@@ -1,126 +1,149 @@
-import * as Util from './Util';
+const initHooks = new WeakMap()
+const options = new WeakMap()
 
-// @class Class
-// @aka L.Class
-
-// @section
-// @uninheritable
-
-// Thanks to John Resig and Dean Edwards for inspiration!
-
-export function Class() {}
-
-Class.extend = function (props) {
-
-	// @function extend(props: Object): Function
-	// [Extends the current class](#class-inheritance) given the properties to be included.
-	// Returns a Javascript function that is a class constructor (to be called with `new`).
-	var NewClass = function () {
-
-		// call the constructor
+export class Class {
+	constructor() {
+		// Call custom constructor. Doesnt propergate through parents.
 		if (this.initialize) {
-			this.initialize.apply(this, arguments);
+			this.initialize(...arguments);
+		}
+		// Call regesterted init hooks
+		this.callInitHooks()
+	}
+
+	// Helper to get prototype of current instance for loookups in Map
+	get _getProto() {
+		return this.prototype || this.__proto__
+	}
+
+	// getter for initHooks
+	// BRAKING-CHANGE: Has no setter.
+	get _initHooks() {
+		return [...(this._getProto._initHooks || []), ...(initHooks.has(this._getProto) ? initHooks.get(this._getProto) : [])]
+	}
+
+	// New getter for default options of class
+	get _defaultOptions() {
+		return {
+			...(this._getProto._defaultOptions) || {},
+			...(options.has(this._getProto) ? options.get(this._getProto) : {})
+		}
+	}
+
+	// options of instance
+	get options() {
+		if (!this._options) { this._options = this._defaultOptions }
+
+		return this._options
+	}
+
+	set options(value) {
+		this._options = value
+	}
+
+	static mergeOptions(option) {
+		if (!options.has(this.prototype)) options.set(this.prototype, {})
+
+		options.set(this.prototype, { ...options.get(this.prototype), ...option })
+		return this;
+	}
+
+	static addInitHook(hook, ...args) {
+		let fn = hook
+		if (typeof hook === 'string') {
+			fn = function () {
+				this[hook].apply(this, args);
+			}
 		}
 
-		// call all constructor hooks
-		this.callInitHooks();
-	};
+		if (typeof fn !== 'function') return
 
-	var parentProto = NewClass.__super__ = this.prototype;
+		if (!initHooks.has(this.prototype)) initHooks.set(this.prototype, [])
 
-	var proto = Util.create(parentProto);
-	proto.constructor = NewClass;
-
-	NewClass.prototype = proto;
-
-	// inherit parent's statics
-	for (var i in this) {
-		if (Object.prototype.hasOwnProperty.call(this, i) && i !== 'prototype' && i !== '__super__') {
-			NewClass[i] = this[i];
-		}
+		initHooks.get(this.prototype).push(fn)
+		return this;
 	}
 
-	// mix static properties into the class
-	if (props.statics) {
-		Util.extend(NewClass, props.statics);
-		delete props.statics;
+	static include(props) {
+		applyMixin(this, props)
+		return this;
 	}
 
-	// mix includes into the prototype
-	if (props.includes) {
-		checkDeprecatedMixinEvents(props.includes);
-		Util.extend.apply(null, [proto].concat(props.includes));
-		delete props.includes;
+	static extend(props) {
+		return extendClass(this, props)
 	}
 
-	// merge options
-	if (proto.options) {
-		props.options = Util.extend(Util.create(proto.options), props.options);
-	}
-
-	// mix given properties into the prototype
-	Util.extend(proto, props);
-
-	proto._initHooks = [];
-
-	// add method for calling all hooks
-	proto.callInitHooks = function () {
-
+	callInitHooks() {
 		if (this._initHooksCalled) { return; }
-
-		if (parentProto.callInitHooks) {
-			parentProto.callInitHooks.call(this);
-		}
-
 		this._initHooksCalled = true;
 
-		for (var i = 0, len = proto._initHooks.length; i < len; i++) {
-			proto._initHooks[i].call(this);
-		}
-	};
-
-	return NewClass;
-};
-
-
-// @function include(properties: Object): this
-// [Includes a mixin](#class-includes) into the current class.
-Class.include = function (props) {
-	Util.extend(this.prototype, props);
-	return this;
-};
-
-// @function mergeOptions(options: Object): this
-// [Merges `options`](#class-options) into the defaults of the class.
-Class.mergeOptions = function (options) {
-	Util.extend(this.prototype.options, options);
-	return this;
-};
-
-// @function addInitHook(fn: Function): this
-// Adds a [constructor hook](#class-constructor-hooks) to the class.
-Class.addInitHook = function (fn) { // (Function) || (String, args...)
-	var args = Array.prototype.slice.call(arguments, 1);
-
-	var init = typeof fn === 'function' ? fn : function () {
-		this[fn].apply(this, args);
-	};
-
-	this.prototype._initHooks = this.prototype._initHooks || [];
-	this.prototype._initHooks.push(init);
-	return this;
-};
-
-function checkDeprecatedMixinEvents(includes) {
-	if (typeof L === 'undefined' || !L || !L.Mixin) { return; }
-
-	includes = Util.isArray(includes) ? includes : [includes];
-
-	for (var i = 0; i < includes.length; i++) {
-		if (includes[i] === L.Mixin.Events) {
-			console.warn('Deprecated include of L.Mixin.Events: ' +
-				'this property will be removed in future releases, ' +
-				'please inherit from L.Evented instead.', new Error().stack);
+		for (var i = 0; i < this._initHooks.length; i++) {
+			this._initHooks[i].call(this);
 		}
 	}
+}
+
+function applyMixin(Class, props) {
+	// var members = [];
+
+	for (const prop in props) {
+		if (Object.hasOwnProperty.call(props, prop)) {
+			const member = props[prop];
+
+			// if (typeof member === 'function') {
+				Class.prototype[prop] = member;
+			// } else {
+			// 	members.push(prop);
+			// }
+		}
+	}
+
+	// WIP better members...
+	// if (members.length === 0) return
+
+	// // DONT USE ARROW FUNCTION HERE THIS CONTEXT IS IMPORTANT!
+	// Class.prototype._setupMembers = function () {
+	// 	this._getProto._setupMembers && this._getProto._setupMembers()
+	// 	members.forEach((prop) => {
+	// 		console.log(this, prop, props[prop])
+	// 		this[prop] = props[prop];
+	// 	});
+	// }
+}
+
+function extendClass(Class, props) {
+	class NewClass extends Class { }
+
+	if ('statics' in props) {
+
+		for (const stat in props.statics) {
+			if (Object.hasOwnProperty.call(props.statics, stat)) {
+				NewClass[stat] = props.statics[stat]
+			}
+		}
+
+		delete props.statics
+	}
+
+	if ('options' in props) {
+		NewClass.mergeOptions(props.options)
+		delete props.options
+	}
+
+	if ('includes' in props) {
+		if (!Array.isArray(props.includes)) {
+			props.includes = [props.includes]
+		}
+
+		for (let i = 0; i < props.includes.length; i++) {
+			const mixin = props.includes[i];
+
+			NewClass.include(mixin)
+		}
+		delete props.includes
+	}
+
+	applyMixin(NewClass, props);
+
+	return NewClass
 }
