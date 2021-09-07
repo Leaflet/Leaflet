4ce6a8c50d213230296426ec6fb443d6c003c4af
Sebastian Krüger <2pi_r2@gmx.de>
2021-09-07 19:23:32 +0200

    cleanup copy script

--- build/copy.js
+++
@@ -1,6 +0,0 @@
-const { writeFileSync, readFileSync, unlinkSync } = require('fs')
-
-writeFileSync('./dist/leaflet-src.esm.js', readFileSync('./dist/temp/leaflet-modern.esm.js', 'utf-8'))
-writeFileSync('./dist/leaflet-src.esm.js.map', readFileSync('./dist/temp/leaflet-modern.esm.js.map', 'utf-8'))
-unlinkSync('./dist/temp/leaflet-modern.esm.js')
-unlinkSync('./dist/temp/leaflet-modern.esm.js.map')
