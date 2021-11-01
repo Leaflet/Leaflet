725e7939303f03eeb373e0f3b9ab55082f221178
falke-design <design.falke@gmail.com>
2021-11-01 10:45:00 +0100

    Change interactive CSS class on Tooltip from leaflet-clickable to leaflet-interactive

--- spec/suites/layer/TooltipSpec.js
+++ spec/suites/layer/TooltipSpec.js
@@ -68,6 +68,18 @@ describe('Tooltip', function () {
 		expect(spy.calledOnce).to.be(true);
 	});
 
+	it("has class leaflet-interactive", function () {
+		var layer = new L.Marker(center).addTo(map);
+		layer.bindTooltip('Tooltip', {permanent: true, interactive: true});
+		expect(L.DomUtil.hasClass(layer._tooltip._container,'leaflet-interactive')).to.be(true);
+	});
+
+	it("has not class leaflet-interactive", function () {
+		var layer = new L.Marker(center).addTo(map);
+		layer.bindTooltip('Tooltip', {permanent: true});
+		expect(L.DomUtil.hasClass(layer._tooltip._container,'leaflet-interactive')).to.be(false);
+	});
+
 	it("can be forced on left direction", function () {
 		var layer = new L.Marker(center).addTo(map);
 		var spy = sinon.spy();
