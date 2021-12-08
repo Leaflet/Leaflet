9d9bce6b700af68a93d061653c184e8f1dd9e10e
falke-design <design.falke@gmail.com>
2021-12-08 20:21:17 +0100

    Cleanup CanvasSpec

--- spec/suites/layer/vector/CanvasSpec.js
+++ spec/suites/layer/vector/CanvasSpec.js
@@ -1,29 +1,20 @@
 describe('Canvas', function () {
-	document.body.appendChild(document.createElement('div'));
-	var c, map, latLngs;
+	var container, map, latLngs;
 
 	function p2ll(x, y) {
 		return map.layerPointToLatLng([x, y]);
 	}
 
 	beforeEach(function () {
-		c = document.createElement('div');
-		c.style.width = '400px';
-		c.style.height = '400px';
-		c.style.position = 'absolute';
-		c.style.top = '0';
-		c.style.left = '0';
-		document.body.appendChild(c);
-		map = new L.Map(c, {preferCanvas: true, zoomControl: false});
+		var obj = createMapContainer({preferCanvas: true, zoomControl: false});
+		container = obj.container;
+		map = obj.map;
 		map.setView([0, 0], 6);
 		latLngs = [p2ll(0, 0), p2ll(0, 100), p2ll(100, 100), p2ll(100, 0)];
 	});
 
 	afterEach(function () {
-		if (map._conteiner_id) {
-			map.remove();
-		}
-		document.body.removeChild(c);
+		removeMapContainer(map, container);
 	});
 
 	describe("#events", function () {
@@ -226,13 +217,13 @@ describe('Canvas', function () {
 
 	describe('#bringToBack', function () {
 		it('is a no-op for layers not on a map', function () {
-			var path = new L.Polyline([[1, 2], [3, 4], [5, 6]]);
+			var path = L.polyline([[1, 2], [3, 4], [5, 6]]);
 			expect(path.bringToBack()).to.equal(path);
 		});
 
 		it('is a no-op for layers no longer in a LayerGroup', function () {
-			var group = new L.LayerGroup().addTo(map);
-			var path = new L.Polyline([[1, 2], [3, 4], [5, 6]]).addTo(group);
+			var group = L.layerGroup().addTo(map);
+			var path = L.polyline([[1, 2], [3, 4], [5, 6]]).addTo(group);
 
 			group.clearLayers();
 
@@ -242,13 +233,13 @@ describe('Canvas', function () {
 
 	describe('#bringToFront', function () {
 		it('is a no-op for layers not on a map', function () {
-			var path = new L.Polyline([[1, 2], [3, 4], [5, 6]]);
+			var path = L.polyline([[1, 2], [3, 4], [5, 6]]);
 			expect(path.bringToFront()).to.equal(path);
 		});
 
 		it('is a no-op for layers no longer in a LayerGroup', function () {
-			var group = new L.LayerGroup().addTo(map);
-			var path = new L.Polyline([[1, 2], [3, 4], [5, 6]]).addTo(group);
+			var group = L.layerGroup().addTo(map);
+			var path = L.polyline([[1, 2], [3, 4], [5, 6]]).addTo(group);
 
 			group.clearLayers();
 
@@ -260,6 +251,7 @@ describe('Canvas', function () {
 		it("can remove the map without errors", function (done) {
 			L.polygon(latLngs).addTo(map);
 			map.remove();
+			map = null;
 			L.Util.requestAnimFrame(function () { done(); });
 		});
 
@@ -267,12 +259,13 @@ describe('Canvas', function () {
 			map.remove();
 
 			var canvas = L.canvas();
-			map = L.map(c, {renderer: canvas});
+			map = L.map(container, {renderer: canvas});
 			map.setView([0, 0], 6);
 			L.polygon(latLngs).addTo(map);
 
 			canvas.remove();
 			map.remove();
+			map = null;
 			L.Util.requestAnimFrame(function () { done(); });
 		});
 	});
