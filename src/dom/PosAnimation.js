f26488fe753228aaa8f0e47d8334cdc56e13ef26
Florian Bischof <design.falke@gmail.com>
2022-04-19 08:44:25 +0200

    Fix lint

--- src/dom/PosAnimation.js
+++ src/dom/PosAnimation.js
@@ -12,20 +12,20 @@ import * as DomUtil from '../dom/DomUtil';
  * @example
  * ```js
  * var myPositionMarker = L.marker([48.864716, 2.294694]).addTo(map);
- * 
+ *
  * myPositionMarker.on("click", function() {
  * 	var pos = map.latLngToLayerPoint(myPositionMarker.getLatLng());
  * 	pos.y -= 25;
  * 	var fx = new L.PosAnimation();
- * 
+ *
  * 	fx.once('end',function() {
  * 		pos.y += 25;
  * 		fx.run(myPositionMarker._icon, pos, 0.8);
  * 	});
- * 
+ *
  * 	fx.run(myPositionMarker._icon, pos, 0.3);
  * });
- * 
+ *
  * ```
  *
  * @constructor L.PosAnimation()
