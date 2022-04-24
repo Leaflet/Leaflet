baca9abd5e1e7cd5533bb794645bdb8cd05e347c
Florian Bischof <design.falke@gmail.com>
2022-04-24 10:43:22 +0200

    Update docs

--- src/map/handler/Map.ScrollWheelZoom.js
+++ src/map/handler/Map.ScrollWheelZoom.js
@@ -14,6 +14,7 @@ Map.mergeOptions({
 	// @option scrollWheelZoom: Boolean|String = true
 	// Whether the map can be zoomed by using the mouse wheel. If passed `'center'`,
 	// it will zoom to the center of the view regardless of where the mouse was.
+	// With `'shiftkey'` zooming is only enabled while holding shift.
 	scrollWheelZoom: true,
 
 	// @option wheelDebounceTime: Number = 40
