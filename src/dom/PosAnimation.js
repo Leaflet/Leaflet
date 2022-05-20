382f2d3d906545d5de50955c59032ed196fa4fb7
B01 <info@b01.de>
2022-05-20 17:16:37 +0200

    Better PosAnimation example (#7386)
    * Better example
    
    https://github.com/Leaflet/Leaflet/issues/6175
    
    * Update PosAnimation.js
    
    Small changes
    
    * Fix lint
    
    Co-authored-by: Florian Bischof <design.falke@gmail.com>

--- src/dom/PosAnimation.js
+++ src/dom/PosAnimation.js
@@ -11,8 +11,21 @@ import * as DomUtil from '../dom/DomUtil';
  *
  * @example
  * ```js
- * var fx = new L.PosAnimation();
- * fx.run(el, [300, 500], 0.5);
+ * var myPositionMarker = L.marker([48.864716, 2.294694]).addTo(map);
+ *
+ * myPositionMarker.on("click", function() {
+ * 	var pos = map.latLngToLayerPoint(myPositionMarker.getLatLng());
+ * 	pos.y -= 25;
+ * 	var fx = new L.PosAnimation();
+ *
+ * 	fx.once('end',function() {
+ * 		pos.y += 25;
+ * 		fx.run(myPositionMarker._icon, pos, 0.8);
+ * 	});
+ *
+ * 	fx.run(myPositionMarker._icon, pos, 0.3);
+ * });
+ *
  * ```
  *
  * @constructor L.PosAnimation()
