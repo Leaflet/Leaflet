2a8ef47faa10dbfc2a9f9429e6cd0b99363e328c
Lukas Gimberis <lukasgimberis@gmail.com>
2022-09-28 15:19:38 +0100

    Avoid ignoring dragstart events during zoom in/out (#8201)

--- src/dom/Draggable.js
+++ src/dom/Draggable.js
@@ -78,8 +78,6 @@ export var Draggable = Evented.extend({
 
 		this._moved = false;
 
-		if (DomUtil.hasClass(this._element, 'leaflet-zoom-anim')) { return; }
-
 		if (e.touches && e.touches.length !== 1) {
 			// Finish dragging to avoid conflict with touchZoom
 			if (Draggable._dragging === this) {
