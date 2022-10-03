a01985d8e8e98a3d88258cf4f0e152e233335fbf
Rob Jackson <rob@rjackson.me>
2022-10-03 17:02:45 +0100

    Speed up Map.DoubleClickZoomSpec: 812ms to 47ms (17x faster)

--- spec/suites/map/handler/Map.DoubleClickZoomSpec.js
+++ spec/suites/map/handler/Map.DoubleClickZoomSpec.js
@@ -5,7 +5,8 @@ describe("Map.DoubleClickZoom", function () {
 		container = createContainer();
 		map = L.map(container, {
 			center: [0, 0],
-			zoom: 3
+			zoom: 3,
+			zoomAnimation: false
 		});
 	});
 
@@ -15,31 +16,32 @@ describe("Map.DoubleClickZoom", function () {
 
 	it("zooms in while dblclick", function (done) {
 		var zoom = map.getZoom();
-		happen.dblclick(container);
 
 		map.on('zoomend', function () {
 			expect(map.getCenter()).to.be.nearLatLng([17.308687886770034, -17.578125000000004]);
 			expect(map.getZoom()).to.be.greaterThan(zoom);
 			done();
 		});
+
+		happen.dblclick(container);
 	});
 
 	it("zooms out while dblclick and holding shift", function (done) {
 		var zoom = map.getZoom();
-		happen.dblclick(container, {shiftKey: true});
 
 		map.on('zoomend', function () {
 			expect(map.getCenter()).to.be.nearLatLng([-33.137551192346145, 35.15625000000001]);
 			expect(map.getZoom()).to.be.lessThan(zoom);
 			done();
 		});
+
+		happen.dblclick(container, {shiftKey: true});
 	});
 
 	it("doubleClickZoom: 'center'", function (done) {
 		var doubleClickZoomBefore = map.options.doubleClickZoom;
 		map.options.doubleClickZoom = 'center';
 		var zoom = map.getZoom();
-		happen.dblclick(container);
 
 		map.on('zoomend', function () {
 			expect(map.getCenter()).to.be.nearLatLng([0, 0]);
@@ -47,6 +49,8 @@ describe("Map.DoubleClickZoom", function () {
 			map.options.doubleClickZoom = doubleClickZoomBefore;
 			done();
 		});
+
+		happen.dblclick(container);
 	});
 
 });
