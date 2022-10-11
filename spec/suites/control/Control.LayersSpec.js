3d7934fd74121f0f48de6739908821adeaae4258
Lars Willighagen <lars.willighagen@gmail.com>
2022-10-11 21:59:17 +0200

    Add click event to Layers control toggle

--- spec/suites/control/Control.LayersSpec.js
+++ spec/suites/control/Control.LayersSpec.js
@@ -195,6 +195,13 @@ describe("Control.Layers", function () {
 			expect(map._container.querySelector('.leaflet-control-layers-expanded')).to.be.ok();
 		});
 
+		it('expands on click', function () {
+			var layersCtrl = L.control.layers(null, null, {collapsed: true}).addTo(map);
+			var toggle = layersCtrl._container.querySelector('.leaflet-control-layers-toggle');
+			happen.once(toggle, {type:'click'});
+			expect(map._container.querySelector('.leaflet-control-layers-expanded')).to.be.ok();
+		});
+
 		it('does not expand on "Enter" keydown when toggle is not focused', function () {
 			L.control.layers(null, null, {collapsed: true}).addTo(map);
 			happen.once(document, {type:'keydown', keyCode:13});
