b888a52f1dfa8e8b71bf92949578f1d8d7339ffd
Jon Koops <jonkoops@gmail.com>
2022-12-27 00:40:35 +0100

    Remove `noConflict()`

--- spec/suites/core/GeneralSpec.js
+++ spec/suites/core/GeneralSpec.js
@@ -1,11 +1,4 @@
 describe('General', () => {
-	it('noConflict', () => {
-		const leaflet = L;
-		expect(L.noConflict()).to.eql(leaflet);
-		expect(L).to.eql(undefined);
-		L = leaflet;
-	});
-
 	it('namespace extension', () => {
 		L.Util.foo = 'bar';
 		L.Foo = 'Bar';
