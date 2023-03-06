9ca79b3c61028ce1bf3a4ced00ca7da1fafd37bc
Florian Bischof <design.falke@gmail.com>
2023-03-06 17:55:46 +0100

    Adding docu - Thanks @IvanSanchez

--- src/layer/marker/index.js
+++ src/layer/marker/index.js
@@ -3,14 +3,46 @@ export {icon} from './Icon.js';
 import {IconDefault} from './Icon.Default.js';
 Icon.Default = IconDefault;
 
-const colors = ['Pink', 'DarkBlue', 'Lila', 'Magenta', 'Red', 'Orange', 'Brown', 'Green', 'Mint'];
-for (const color of colors) {
+for (const color of ['Pink', 'DarkBlue', 'Lila', 'Magenta', 'Red', 'Orange', 'Brown', 'Green', 'Mint']) {
 	Icon.Default[color] = IconDefault.extend({
 		options:{
 			className: `default-icon-color-${color.toLowerCase()}`
 		}
 	});
 }
+Icon.Default.Blue = IconDefault;
+
+/**
+ * @miniclass Colored default icons (Icon)
+ * @section
+ *
+ * `L.Icon.Default` has several static properties providing different coloured versions of the default icon.
+ *
+ * Usage is as: `L.marker(latlng, { icon: new L.Icon.Default.Green() })`
+ *
+ * Note that `L.Icon.Default.Blue` behaves the same as `L.Icon.Default`.
+ *
+ * @property Pink: L.Icon
+ * The default icon, but tinted pink
+ * @property Blue: L.Icon
+ * The default icon, but tinted blue (same as default)
+ * @property DarkBlue: L.Icon
+ * The default icon, but tinted dark blue
+ * @property Lila: L.Icon
+ * The default icon, but tinted lila
+ * @property Magenta: L.Icon
+ * The default icon, but tinted magenta
+ * @property Red: L.Icon
+ * The default icon, but tinted red
+ * @property Orange: L.Icon
+ * The default icon, but tinted orange
+ * @property Brown: L.Icon
+ * The default icon, but tinted brown
+ * @property Green: L.Icon
+ * The default icon, but tinted green
+ * @property Mint: L.Icon
+ * The default icon, but tinted bluish green
+ */
 
 export {Icon};
 
