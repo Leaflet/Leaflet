4f8e3f5a82e275c452378e2cc659c55645a115a6
Abdullah Sohail <51018513+AbdullahSohail-SE@users.noreply.github.com>
2023-05-18 12:49:11 +0500

    Bugfix: Fix noMoveStart option for fitBounds method (#8911)
    Co-authored-by: Jon Koops <jonkoops@gmail.com>
    Co-authored-by: Florian Bischof <design.falke@gmail.com>

--- src/map/Map.js
+++ src/map/Map.js
@@ -1683,7 +1683,7 @@ export const Map = Evented.extend({
 
 		Util.requestAnimFrame(function () {
 			this
-			    ._moveStart(true, false)
+			    ._moveStart(true, options.noMoveStart ?? false)
 			    ._animateZoom(center, zoom, true);
 		}, this);
 
