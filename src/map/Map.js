e797c29ec0df2c5a0017ec826d05955db33fb97e
Florian Bischof <design.falke@gmail.com>
2023-07-30 22:31:42 +0200

    Update src/map/Map.js

--- src/map/Map.js
+++ src/map/Map.js
@@ -1651,9 +1651,9 @@ export const Map = Evented.extend({
 	},
 
 	_animMoveEnd() {
-		const
-			c = this.getCenter(),
-		    z = this.getZoom();
+		const c = this.getCenter(),
+				z = this.getZoom();
+
 
 		DomUtil.setTransform(
 			this._proxy,
