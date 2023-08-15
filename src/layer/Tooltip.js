92c753bc4d857dad7e7f80aa562f3cc6ca97fb6d
99_D_99 <46034083+dscarb21@users.noreply.github.com>
2023-08-15 00:48:48 -0400

    #9059 Fix - Removed convoluted map-moving / tooltip showing logic. Everything functions as should.

--- src/layer/Tooltip.js
+++ src/layer/Tooltip.js
@@ -417,15 +417,6 @@ Layer.include({
 			return;
 		}
 
-		// If the map is moving, we will show the tooltip after it's done.
-		if (this._map.dragging && this._map.dragging.moving() && !this._openOnceFlag) {
-			this._openOnceFlag = true;
-			this._map.once('moveend', () => {
-				this._openOnceFlag = false;
-				this._openTooltip(e);
-			});
-			return;
-		}
 
 		this._tooltip._source = e.layer || e.target;
 
