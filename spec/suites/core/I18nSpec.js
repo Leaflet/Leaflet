5eed887003182b10d3f9d50e54f4fe3b46d465d7
Yohan Boniface <yohanboniface@free.fr>
2024-02-27 17:42:51 +0100

    Add i18n helpers in Leaflet core
    fix #9092

---
+++ spec/suites/core/I18nSpec.js
@@ -0,0 +1,63 @@
+import {locale, setLocale, registerLocale, translate} from 'leaflet';
+
+describe('I18n', () => {
+	beforeEach(() => {
+		const fr = {
+			'Simple phrase to translate': 'Une simple phrase à traduire',
+			'A phrase with a {variable} to translate': 'Une phrase à traduire avec une {variable}',
+			'A phrase with empty translation': ''
+		};
+		registerLocale('fr', fr);
+		setLocale('fr');
+	});
+
+	it('expects current locale to be fr', () => {
+		expect(locale).to.eql('fr');
+	});
+
+	it('should translate simple sentences', () => {
+		expect(translate('Simple phrase to translate')).to.eql('Une simple phrase à traduire');
+	});
+
+	it('should translate sentences with a variable', () => {
+		expect(translate('A phrase with a {variable} to translate', {variable: 'foo'})).to.eql('Une phrase à traduire avec une foo');
+	});
+
+	it('should translate empty translations', () => {
+		expect(translate('A phrase with empty translation')).to.eql('');
+	});
+
+	it('should allow to override some translations', () => {
+		registerLocale('fr', {'A phrase with empty translation': 'my custom translation'});
+		expect(translate('A phrase with empty translation')).to.eql('my custom translation');
+		// Unchanged
+		expect(translate('Simple phrase to translate')).to.eql('Une simple phrase à traduire');
+	});
+
+	it('should not fail if a variable is missing', () => {
+		expect(translate('A phrase with a {variable} to translate')).to.eql('Une phrase à traduire avec une {variable}');
+	});
+
+	it('should not fail if the translation is missing', () => {
+		expect(translate('A missing translation')).to.eql('A missing translation');
+	});
+
+	it('should not fail if the locale is missing', () => {
+		setLocale('foo');
+		expect(translate('Simple phrase to translate')).to.eql('Simple phrase to translate');
+	});
+
+	it('should allow to add many locales', () => {
+		const it = {
+			'Simple phrase to translate': 'Frase semplice da tradurre',
+			'A phrase with a {variable} to translate': 'Una frase da tradurre con {variable}',
+			'A phrase with empty translation': ''
+		};
+		registerLocale('it', it);
+		setLocale('it');
+		expect(translate('Simple phrase to translate')).to.eql('Frase semplice da tradurre');
+		setLocale('fr');
+		expect(translate('Simple phrase to translate')).to.eql('Une simple phrase à traduire');
+	});
+
+});
--- src/core/index.js
+++ src/core/index.js
@@ -2,6 +2,7 @@ export {default as Browser} from './Browser.js';
 export {Class} from './Class.js';
 export {Evented} from './Events.js';
 export {Handler} from './Handler.js';
+export {locale, translate, setLocale, registerLocale} from './I18n.js';
 
 import * as Util from './Util.js';
 export {Util};
