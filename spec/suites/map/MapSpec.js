8bcc651c04744bb169a3585bdc05074d237965cb
Ashwin Nema <ashwinnema06@gmail.com>
2024-03-27 17:05:51 +0530

    Fixes for #8373[feedback]
    1. typo fix : changing corridinates to coordinates
    
    2.changing to.be.equal to to.be.approximately for round off approximation

--- spec/suites/map/MapSpec.js
+++ spec/suites/map/MapSpec.js
@@ -2227,28 +2227,30 @@ describe('Map', () => {
 			}).to.throw();
 		});
 
+		const tolerance = 1 / 1000000;
+
 		it('returns pixel coordinates relative to the top-left of the CRS extents', () => {
 			map.setView([40, -83], 5);
 			const x = latLng([40, -83]);
 			const a = map.project(x, 5);
-			expect(a.x).to.be.equal(2207.2888888888892);
-			expect(a.y).to.be.equal(3101.3204602149353);
+			expect(a.x).to.be.approximately(2207.288888, tolerance);
+			expect(a.y).to.be.approximately(3101.320460, tolerance);
 		});
 
-		it('test the other corridinates', () => {
+		it('test the other coordinates', () => {
 			map.setView([40, 83], 5);
 			const x = latLng([40, 83]);
 			const b = map.project(x, 5);
-			expect(b.x).to.be.equal(5984.711111111111);
-			expect(b.y).to.be.equal(3101.3204602149353);
+			expect(b.x).to.be.approximately(5984.7111111, tolerance);
+			expect(b.y).to.be.approximately(3101.3204602, tolerance);
 		});
 
-		it('test the prev corridinates with different zoom', () => {
+		it('test the prev coordinates with different zoom', () => {
 			map.setView([40, 83], 5);
 			const x = latLng([40, 83]);
 			const b = map.project(x, 6);
-			expect(b.x).to.be.equal(11969.422222222222);
-			expect(b.y).to.be.equal(6202.640920429871);
+			expect(b.x).to.be.approximately(11969.422222, tolerance);
+			expect(b.y).to.be.approximately(6202.640920, tolerance);
 		});
 	});
 
