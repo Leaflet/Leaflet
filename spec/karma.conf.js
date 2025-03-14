58e5382abadff301c5058e263f027e303abfe4f2
Florian Bischof <design.falke@gmail.com>
2025-03-14 22:57:40 +0100

    add package.json to karma setup

--- spec/karma.conf.js
+++ spec/karma.conf.js
@@ -21,6 +21,7 @@ module.exports = function (/** @type {import('karma').Config} */ config) {
 			{pattern: 'node_modules/sinon/**/*', included: false, served: true},
 			{pattern: 'node_modules/ui-event-simulator/**/*', included: false, served: true},
 			{pattern: 'src/**/*.js', included: false, served: true},
+			{pattern: 'package.json', included: false, served: true},
 			{pattern: 'dist/**/*.png', included: false, served: true},
 			{pattern: 'spec/setup.js', type: 'module'},
 			{pattern: 'spec/suites/**/*.js', type: 'module'},
