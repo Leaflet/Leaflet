722f7f78ad7a4aca51986f26bc2921779eee6f59
Florian Bischof <design.falke@gmail.com>
2025-04-21 10:07:42 +0200

    Copy only SVG images from src to dist
    Co-authored-by: Florian Bischof <design.falke@gmail.com>

--- build/rollup-config.js
+++ build/rollup-config.js
@@ -30,13 +30,8 @@ const config = {
 				const fileNames = [
 					'leaflet.css',
 					'images/logo.svg',
-					'images/layers.png',
 					'images/layers.svg',
-					'images/layers-2x.png',
-					'images/marker-icon.png',
 					'images/marker-icon.svg',
-					'images/marker-icon-2x.png',
-					'images/marker-shadow.png',
 					'images/marker-shadow.svg',
 				];
 				for (const fileName of fileNames) {
