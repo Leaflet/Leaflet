82378018274396ec16004bcf9d86b04004f0d6c2
Simon Legner <Simon.Legner@gmail.com>
2025-05-29 11:23:35 +0200

    Convert Map to ES6 class (mergeOptions)

--- src/map/Map.js
+++ src/map/Map.js
@@ -40,6 +40,101 @@ import * as PointerEvents from '../dom/DomEvent.PointerEvents.js';
 // and optionally an object literal with `Map options`.
 export class Map extends Evented {
 
+	static {
+		this.prototype._initHooks = [];
+		this.mergeOptions({
+			// @section Map State Options
+			// @option crs: CRS = CRS.EPSG3857
+			// The [Coordinate Reference System](#crs) to use. Don't change this if you're not
+			// sure what it means.
+			crs: EPSG3857,
+
+			// @option center: LatLng = undefined
+			// Initial geographic center of the map
+			center: undefined,
+
+			// @option zoom: Number = undefined
+			// Initial map zoom level
+			zoom: undefined,
+
+			// @option minZoom: Number = *
+			// Minimum zoom level of the map.
+			// If not specified and at least one `GridLayer` or `TileLayer` is in the map,
+			// the lowest of their `minZoom` options will be used instead.
+			minZoom: undefined,
+
+			// @option maxZoom: Number = *
+			// Maximum zoom level of the map.
+			// If not specified and at least one `GridLayer` or `TileLayer` is in the map,
+			// the highest of their `maxZoom` options will be used instead.
+			maxZoom: undefined,
+
+			// @option layers: Layer[] = []
+			// Array of layers that will be added to the map initially
+			layers: [],
+
+			// @option maxBounds: LatLngBounds = null
+			// When this option is set, the map restricts the view to the given
+			// geographical bounds, bouncing the user back if the user tries to pan
+			// outside the view. To set the restriction dynamically, use
+			// [`setMaxBounds`](#map-setmaxbounds) method.
+			maxBounds: undefined,
+
+			// @option renderer: Renderer = *
+			// The default method for drawing vector layers on the map. `SVG`
+			// or `Canvas` by default depending on browser support.
+			renderer: undefined,
+
+
+			// @section Animation Options
+			// @option zoomAnimation: Boolean = true
+			// Whether the map zoom animation is enabled. By default it's enabled
+			// in all browsers that support CSS Transitions except Android.
+			zoomAnimation: true,
+
+			// @option zoomAnimationThreshold: Number = 4
+			// Won't animate zoom if the zoom difference exceeds this value.
+			zoomAnimationThreshold: 4,
+
+			// @option fadeAnimation: Boolean = true
+			// Whether the tile fade animation is enabled. By default it's enabled
+			// in all browsers that support CSS Transitions except Android.
+			fadeAnimation: true,
+
+			// @option markerZoomAnimation: Boolean = true
+			// Whether markers animate their zoom with the zoom animation, if disabled
+			// they will disappear for the length of the animation. By default it's
+			// enabled in all browsers that support CSS Transitions except Android.
+			markerZoomAnimation: true,
+
+			// @option transform3DLimit: Number = 2^23
+			// Defines the maximum size of a CSS translation transform. The default
+			// value should not be changed unless a web browser positions layers in
+			// the wrong place after doing a large `panBy`.
+			transform3DLimit: 8388608, // Precision limit of a 32-bit float
+
+			// @section Interaction Options
+			// @option zoomSnap: Number = 1
+			// Forces the map's zoom level to always be a multiple of this, particularly
+			// right after a [`fitBounds()`](#map-fitbounds) or a pinch-zoom.
+			// By default, the zoom level snaps to the nearest integer; lower values
+			// (e.g. `0.5` or `0.1`) allow for greater granularity. A value of `0`
+			// means the zoom level will not be snapped after `fitBounds` or a pinch-zoom.
+			zoomSnap: 1,
+
+			// @option zoomDelta: Number = 1
+			// Controls how much the map's zoom level will change after a
+			// [`zoomIn()`](#map-zoomin), [`zoomOut()`](#map-zoomout), pressing `+`
+			// or `-` on the keyboard, or using the [zoom controls](#control-zoom).
+			// Values smaller than `1` (e.g. `0.5`) allow for greater granularity.
+			zoomDelta: 1,
+
+			// @option trackResize: Boolean = true
+			// Whether the map automatically handles browser window resize to update itself.
+			trackResize: true
+		});
+	}
+
 	constructor(id, options) { // (HTMLElement or String, Object)
 		super();
 		options = Util.setOptions(this, options);
@@ -1674,96 +1769,3 @@ export class Map extends Evented {
 		this._moveEnd(true);
 	}
 }
-
-Map.prototype._initHooks = [];
-Map.prototype.options = {
-	// @section Map State Options
-	// @option crs: CRS = CRS.EPSG3857
-	// The [Coordinate Reference System](#crs) to use. Don't change this if you're not
-	// sure what it means.
-	crs: EPSG3857,
-
-	// @option center: LatLng = undefined
-	// Initial geographic center of the map
-	center: undefined,
-
-	// @option zoom: Number = undefined
-	// Initial map zoom level
-	zoom: undefined,
-
-	// @option minZoom: Number = *
-	// Minimum zoom level of the map.
-	// If not specified and at least one `GridLayer` or `TileLayer` is in the map,
-	// the lowest of their `minZoom` options will be used instead.
-	minZoom: undefined,
-
-	// @option maxZoom: Number = *
-	// Maximum zoom level of the map.
-	// If not specified and at least one `GridLayer` or `TileLayer` is in the map,
-	// the highest of their `maxZoom` options will be used instead.
-	maxZoom: undefined,
-
-	// @option layers: Layer[] = []
-	// Array of layers that will be added to the map initially
-	layers: [],
-
-	// @option maxBounds: LatLngBounds = null
-	// When this option is set, the map restricts the view to the given
-	// geographical bounds, bouncing the user back if the user tries to pan
-	// outside the view. To set the restriction dynamically, use
-	// [`setMaxBounds`](#map-setmaxbounds) method.
-	maxBounds: undefined,
-
-	// @option renderer: Renderer = *
-	// The default method for drawing vector layers on the map. `SVG`
-	// or `Canvas` by default depending on browser support.
-	renderer: undefined,
-
-
-	// @section Animation Options
-	// @option zoomAnimation: Boolean = true
-	// Whether the map zoom animation is enabled. By default it's enabled
-	// in all browsers that support CSS Transitions except Android.
-	zoomAnimation: true,
-
-	// @option zoomAnimationThreshold: Number = 4
-	// Won't animate zoom if the zoom difference exceeds this value.
-	zoomAnimationThreshold: 4,
-
-	// @option fadeAnimation: Boolean = true
-	// Whether the tile fade animation is enabled. By default it's enabled
-	// in all browsers that support CSS Transitions except Android.
-	fadeAnimation: true,
-
-	// @option markerZoomAnimation: Boolean = true
-	// Whether markers animate their zoom with the zoom animation, if disabled
-	// they will disappear for the length of the animation. By default it's
-	// enabled in all browsers that support CSS Transitions except Android.
-	markerZoomAnimation: true,
-
-	// @option transform3DLimit: Number = 2^23
-	// Defines the maximum size of a CSS translation transform. The default
-	// value should not be changed unless a web browser positions layers in
-	// the wrong place after doing a large `panBy`.
-	transform3DLimit: 8388608, // Precision limit of a 32-bit float
-
-	// @section Interaction Options
-	// @option zoomSnap: Number = 1
-	// Forces the map's zoom level to always be a multiple of this, particularly
-	// right after a [`fitBounds()`](#map-fitbounds) or a pinch-zoom.
-	// By default, the zoom level snaps to the nearest integer; lower values
-	// (e.g. `0.5` or `0.1`) allow for greater granularity. A value of `0`
-	// means the zoom level will not be snapped after `fitBounds` or a pinch-zoom.
-	zoomSnap: 1,
-
-	// @option zoomDelta: Number = 1
-	// Controls how much the map's zoom level will change after a
-	// [`zoomIn()`](#map-zoomin), [`zoomOut()`](#map-zoomout), pressing `+`
-	// or `-` on the keyboard, or using the [zoom controls](#control-zoom).
-	// Values smaller than `1` (e.g. `0.5`) allow for greater granularity.
-	zoomDelta: 1,
-
-	// @option trackResize: Boolean = true
-	// Whether the map automatically handles browser window resize to update itself.
-	trackResize: true
-};
