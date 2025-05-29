55896be2c1da7086f8b46f1eea2f7c547c6b242e
Simon Legner <Simon.Legner@gmail.com>
2025-05-29 12:58:53 +0200

    Convert Marker to ES6 class

--- src/layer/marker/Marker.js
+++ src/layer/marker/Marker.js
@@ -21,87 +21,89 @@ import {MarkerDrag} from './Marker.Drag.js';
 
 // @constructor Marker(latlng: LatLng, options? : Marker options)
 // Instantiates a Marker object given a geographical point and optionally an options object.
-export const Marker = Layer.extend({
+export class Marker extends Layer {
 
-	// @section
-	// @aka Marker options
-	options: {
-		// @option icon: Icon = *
-		// Icon instance to use for rendering the marker.
-		// See [Icon documentation](#Icon) for details on how to customize the marker icon.
-		// If not specified, a common instance of `Icon.Default` is used.
-		icon: new IconDefault(),
+	static {
+		// @section
+		// @aka Marker options
+		this.setDefaultOptions({
+			// @option icon: Icon = *
+			// Icon instance to use for rendering the marker.
+			// See [Icon documentation](#Icon) for details on how to customize the marker icon.
+			// If not specified, a common instance of `Icon.Default` is used.
+			icon: new IconDefault(),
 
-		// Option inherited from "Interactive layer" abstract class
-		interactive: true,
+			// Option inherited from "Interactive layer" abstract class
+			interactive: true,
 
-		// @option keyboard: Boolean = true
-		// Whether the marker can be tabbed to with a keyboard and clicked by pressing enter.
-		keyboard: true,
+			// @option keyboard: Boolean = true
+			// Whether the marker can be tabbed to with a keyboard and clicked by pressing enter.
+			keyboard: true,
 
-		// @option title: String = ''
-		// Text for the browser tooltip that appear on marker hover (no tooltip by default).
-		// [Useful for accessibility](https://leafletjs.com/examples/accessibility/#markers-must-be-labelled).
-		title: '',
+			// @option title: String = ''
+			// Text for the browser tooltip that appear on marker hover (no tooltip by default).
+			// [Useful for accessibility](https://leafletjs.com/examples/accessibility/#markers-must-be-labelled).
+			title: '',
 
-		// @option alt: String = 'Marker'
-		// Text for the `alt` attribute of the icon image.
-		// [Useful for accessibility](https://leafletjs.com/examples/accessibility/#markers-must-be-labelled).
-		alt: 'Marker',
+			// @option alt: String = 'Marker'
+			// Text for the `alt` attribute of the icon image.
+			// [Useful for accessibility](https://leafletjs.com/examples/accessibility/#markers-must-be-labelled).
+			alt: 'Marker',
 
-		// @option zIndexOffset: Number = 0
-		// By default, marker images zIndex is set automatically based on its latitude. Use this option if you want to put the marker on top of all others (or below), specifying a high value like `1000` (or high negative value, respectively).
-		zIndexOffset: 0,
+			// @option zIndexOffset: Number = 0
+			// By default, marker images zIndex is set automatically based on its latitude. Use this option if you want to put the marker on top of all others (or below), specifying a high value like `1000` (or high negative value, respectively).
+			zIndexOffset: 0,
 
-		// @option opacity: Number = 1.0
-		// The opacity of the marker.
-		opacity: 1,
+			// @option opacity: Number = 1.0
+			// The opacity of the marker.
+			opacity: 1,
 
-		// @option riseOnHover: Boolean = false
-		// If `true`, the marker will get on top of others when you hover the pointer over it.
-		riseOnHover: false,
+			// @option riseOnHover: Boolean = false
+			// If `true`, the marker will get on top of others when you hover the pointer over it.
+			riseOnHover: false,
 
-		// @option riseOffset: Number = 250
-		// The z-index offset used for the `riseOnHover` feature.
-		riseOffset: 250,
+			// @option riseOffset: Number = 250
+			// The z-index offset used for the `riseOnHover` feature.
+			riseOffset: 250,
 
-		// @option pane: String = 'markerPane'
-		// `Map pane` where the markers icon will be added.
-		pane: 'markerPane',
+			// @option pane: String = 'markerPane'
+			// `Map pane` where the markers icon will be added.
+			pane: 'markerPane',
 
-		// @option shadowPane: String = 'shadowPane'
-		// `Map pane` where the markers shadow will be added.
-		shadowPane: 'shadowPane',
+			// @option shadowPane: String = 'shadowPane'
+			// `Map pane` where the markers shadow will be added.
+			shadowPane: 'shadowPane',
 
-		// @option bubblingPointerEvents: Boolean = false
-		// When `true`, a pointer event on this marker will trigger the same event on the map
-		// (unless [`DomEvent.stopPropagation`](#domevent-stoppropagation) is used).
-		bubblingPointerEvents: false,
+			// @option bubblingPointerEvents: Boolean = false
+			// When `true`, a pointer event on this marker will trigger the same event on the map
+			// (unless [`DomEvent.stopPropagation`](#domevent-stoppropagation) is used).
+			bubblingPointerEvents: false,
 
-		// @option autoPanOnFocus: Boolean = true
-		// When `true`, the map will pan whenever the marker is focused (via
-		// e.g. pressing `tab` on the keyboard) to ensure the marker is
-		// visible within the map's bounds
-		autoPanOnFocus: true,
+			// @option autoPanOnFocus: Boolean = true
+			// When `true`, the map will pan whenever the marker is focused (via
+			// e.g. pressing `tab` on the keyboard) to ensure the marker is
+			// visible within the map's bounds
+			autoPanOnFocus: true,
 
-		// @section Draggable marker options
-		// @option draggable: Boolean = false
-		// Whether the marker is draggable with pointer or not.
-		draggable: false,
+			// @section Draggable marker options
+			// @option draggable: Boolean = false
+			// Whether the marker is draggable with pointer or not.
+			draggable: false,
 
-		// @option autoPan: Boolean = false
-		// Whether to pan the map when dragging this marker near its edge or not.
-		autoPan: false,
+			// @option autoPan: Boolean = false
+			// Whether to pan the map when dragging this marker near its edge or not.
+			autoPan: false,
 
-		// @option autoPanPadding: Point = Point(50, 50)
-		// Distance (in pixels to the left/right and to the top/bottom) of the
-		// map edge to start panning the map.
-		autoPanPadding: [50, 50],
+			// @option autoPanPadding: Point = Point(50, 50)
+			// Distance (in pixels to the left/right and to the top/bottom) of the
+			// map edge to start panning the map.
+			autoPanPadding: [50, 50],
 
-		// @option autoPanSpeed: Number = 10
-		// Number of pixels the map should pan by.
-		autoPanSpeed: 10
-	},
+			// @option autoPanSpeed: Number = 10
+			// Number of pixels the map should pan by.
+			autoPanSpeed: 10
+		});
+	}
 
 	/* @section
 	 *
@@ -111,7 +113,7 @@ export const Marker = Layer.extend({
 	initialize(latlng, options) {
 		Util.setOptions(this, options);
 		this._latlng = new LatLng(latlng);
-	},
+	}
 
 	onAdd(map) {
 		this._zoomAnimated = this._zoomAnimated && map.options.markerZoomAnimation;
@@ -122,7 +124,7 @@ export const Marker = Layer.extend({
 
 		this._initIcon();
 		this.update();
-	},
+	}
 
 	onRemove(map) {
 		if (this.dragging?.enabled()) {
@@ -137,20 +139,20 @@ export const Marker = Layer.extend({
 
 		this._removeIcon();
 		this._removeShadow();
-	},
+	}
 
 	getEvents() {
 		return {
 			zoom: this.update,
 			viewreset: this.update
 		};
-	},
+	}
 
 	// @method getLatLng: LatLng
 	// Returns the current geographical position of the marker.
 	getLatLng() {
 		return this._latlng;
-	},
+	}
 
 	// @method setLatLng(latlng: LatLng): this
 	// Changes the marker position to the given point.
@@ -162,20 +164,20 @@ export const Marker = Layer.extend({
 		// @event move: Event
 		// Fired when the marker is moved via [`setLatLng`](#marker-setlatlng) or by [dragging](#marker-dragging). Old and new coordinates are included in event arguments as `oldLatLng`, `latlng`.
 		return this.fire('move', {oldLatLng, latlng: this._latlng});
-	},
+	}
 
 	// @method setZIndexOffset(offset: Number): this
 	// Changes the [zIndex offset](#marker-zindexoffset) of the marker.
 	setZIndexOffset(offset) {
 		this.options.zIndexOffset = offset;
 		return this.update();
-	},
+	}
 
 	// @method getIcon: Icon
 	// Returns the current icon used by the marker
 	getIcon() {
 		return this.options.icon;
-	},
+	}
 
 	// @method setIcon(icon: Icon): this
 	// Changes the marker icon.
@@ -193,14 +195,14 @@ export const Marker = Layer.extend({
 		}
 
 		return this;
-	},
+	}
 
 	// @method getElement(): HTMLElement
 	// Returns the instance of [`HTMLElement`](https://developer.mozilla.org/docs/Web/API/HTMLElement)
 	// used by Marker layer.
 	getElement() {
 		return this._icon;
-	},
+	}
 
 	update() {
 
@@ -210,7 +212,7 @@ export const Marker = Layer.extend({
 		}
 
 		return this;
-	},
+	}
 
 	_initIcon() {
 		const options = this.options,
@@ -282,7 +284,7 @@ export const Marker = Layer.extend({
 		if (newShadow && addShadow) {
 			this.getPane(options.shadowPane).appendChild(this._shadow);
 		}
-	},
+	}
 
 	_removeIcon() {
 		if (this.options.riseOnHover) {
@@ -300,12 +302,12 @@ export const Marker = Layer.extend({
 		this.removeInteractiveTarget(this._icon);
 
 		this._icon = null;
-	},
+	}
 
 	_removeShadow() {
 		this._shadow?.remove();
 		this._shadow = null;
-	},
+	}
 
 	_setPos(pos) {
 
@@ -320,19 +322,19 @@ export const Marker = Layer.extend({
 		this._zIndex = pos.y + this.options.zIndexOffset;
 
 		this._resetZIndex();
-	},
+	}
 
 	_updateZIndex(offset) {
 		if (this._icon) {
 			this._icon.style.zIndex = this._zIndex + offset;
 		}
-	},
+	}
 
 	_animateZoom(opt) {
 		const pos = this._map._latLngToNewLayerPoint(this._latlng, opt.zoom, opt.center).round();
 
 		this._setPos(pos);
-	},
+	}
 
 	_initInteraction() {
 
@@ -355,7 +357,7 @@ export const Marker = Layer.extend({
 				this.dragging.enable();
 			}
 		}
-	},
+	}
 
 	// @method setOpacity(opacity: Number): this
 	// Changes the opacity of the marker.
@@ -366,7 +368,7 @@ export const Marker = Layer.extend({
 		}
 
 		return this;
-	},
+	}
 
 	_updateOpacity() {
 		const opacity = this.options.opacity;
@@ -378,15 +380,15 @@ export const Marker = Layer.extend({
 		if (this._shadow) {
 			this._shadow.style.opacity = opacity;
 		}
-	},
+	}
 
 	_bringToFront() {
 		this._updateZIndex(this.options.riseOffset);
-	},
+	}
 
 	_resetZIndex() {
 		this._updateZIndex(0);
-	},
+	}
 
 	_panOnFocus() {
 		const map = this._map;
@@ -400,13 +402,13 @@ export const Marker = Layer.extend({
 			paddingTopLeft: anchor,
 			paddingBottomRight: size.subtract(anchor)
 		});
-	},
+	}
 
 	_getPopupAnchor() {
 		return this.options.icon.options.popupAnchor;
-	},
+	}
 
 	_getTooltipAnchor() {
 		return this.options.icon.options.tooltipAnchor;
 	}
-});
+}
