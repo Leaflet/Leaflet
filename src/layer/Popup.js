235b4383f47e87b488e8b33fdd4ebca33422eec6
Iván Sánchez Ortega <ivan@sanchezortega.es>
2025-07-07 11:31:46 +0200

    Set default min width of popups back to 50px
    Signed-off-by: Iván Sánchez Ortega <ivan@sanchezortega.es>

--- src/layer/Popup.js
+++ src/layer/Popup.js
@@ -62,9 +62,9 @@ export const Popup = DivOverlay.extend({
 		// Max width of the popup, in pixels.
 		maxWidth: 300,
 
-		// @option minWidth: Number = 100
+		// @option minWidth: Number = 50
 		// Min width of the popup, in pixels.
-		minWidth: 100,
+		minWidth: 50,
 
 		// @option maxHeight: Number = null
 		// If set, creates a scrollable container of the given height
