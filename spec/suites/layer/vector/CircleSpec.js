03fc48a8481f271e7f6ad16a039ab7fd7a714403
bishalthapa <thapabishal21dev@gmail.com>
2025-08-16 01:51:15 +0545

    Add CircleMarker setStyle tests and update Circle functionality

--- spec/suites/layer/vector/CircleSpec.js
+++ spec/suites/layer/vector/CircleSpec.js
@@ -1,61 +1,89 @@
-import {expect} from 'chai';
-import {Circle, Map, CRS, Transformation} from 'leaflet';
-import {createContainer, removeMapContainer} from '../../SpecHelper.js';
+import { expect } from "chai";
+import { Circle, Map, CRS, Transformation } from "leaflet";
+import { createContainer, removeMapContainer } from "../../SpecHelper.js";
+import { CircleMarker } from "../../../../src/layer/vector/CircleMarker.js";
 
-describe('Circle', () => {
-	let map, container, circle;
+describe("Circle", () => {
+  let map, container, circle;
 
-	beforeEach(() => {
-		container = container = createContainer();
-		map = new Map(container);
-		map.setView([0, 0], 4);
-		circle = new Circle([50, 30], {radius: 200}).addTo(map);
-	});
+  beforeEach(() => {
+    container = container = createContainer();
+    map = new Map(container);
+    map.setView([0, 0], 4);
+    circle = new Circle([50, 30], { radius: 200 }).addTo(map);
+  });
 
-	afterEach(() => {
-		removeMapContainer(map, container);
-	});
+  afterEach(() => {
+    removeMapContainer(map, container);
+  });
 
-	describe('#init', () => {
-		it('uses default radius if not given', () => {
-			const circle = new Circle([0, 0]);
-			expect(circle.getRadius()).to.eql(10);
-		});
-	});
+  describe("#init", () => {
+    it("uses default radius if not given", () => {
+      const circle = new Circle([0, 0]);
+      expect(circle.getRadius()).to.eql(10);
+    });
+  });
 
-	describe('#getBounds', () => {
-		it('returns bounds', () => {
-			const bounds = circle.getBounds();
+  describe("#getBounds", () => {
+    it("returns bounds", () => {
+      const bounds = circle.getBounds();
 
-			expect(bounds.getSouthWest()).nearLatLng([49.99820, 29.99720]);
-			expect(bounds.getNorthEast()).nearLatLng([50.00179, 30.00279]);
-		});
-	});
+      expect(bounds.getSouthWest()).nearLatLng([49.9982, 29.9972]);
+      expect(bounds.getNorthEast()).nearLatLng([50.00179, 30.00279]);
+    });
+  });
 
-	describe('Legacy factory', () => {
-		it('returns same bounds as 1.0 factory', () => {
-			const bounds = circle.getBounds();
+  describe("Legacy factory", () => {
+    it("returns same bounds as 1.0 factory", () => {
+      const bounds = circle.getBounds();
 
-			expect(bounds.getSouthWest()).nearLatLng([49.99820, 29.99720]);
-			expect(bounds.getNorthEast()).nearLatLng([50.00179, 30.00279]);
-		});
-	});
+      expect(bounds.getSouthWest()).nearLatLng([49.9982, 29.9972]);
+      expect(bounds.getNorthEast()).nearLatLng([50.00179, 30.00279]);
+    });
+  });
 
-	describe('CRS Simple', () => {
-		it('returns a positive radius if the x axis of L.CRS.Simple is inverted', () => {
-			map.remove();
+  describe("CRS Simple", () => {
+    it("returns a positive radius if the x axis of L.CRS.Simple is inverted", () => {
+      map.remove();
 
-			class crs extends CRS.Simple {
-				static transformation = new Transformation(-1, 0, -1, 0);
-			}
-			map = new Map(container, {
-				crs
-			});
-			map.setView([0, 0], 4);
+      class crs extends CRS.Simple {
+        static transformation = new Transformation(-1, 0, -1, 0);
+      }
+      map = new Map(container, {
+        crs,
+      });
+      map.setView([0, 0], 4);
 
-			const circle = new Circle([0, 0], {radius: 200}).addTo(map);
+      const circle = new Circle([0, 0], { radius: 200 }).addTo(map);
 
-			expect(circle._radius).to.eql(3200);
-		});
-	});
+      expect(circle._radius).to.eql(3200);
+    });
+  });
+
+  describe.only("CircleMarker", () => {
+    // <- only this suite will run
+    let map, container, circleMarker;
+
+    beforeEach(() => {
+      container = createContainer();
+      map = new Map(container).setView([0, 0], 4);
+      circleMarker = new CircleMarker([0, 0], { radius: 5 }).addTo(map);
+    });
+
+    afterEach(() => {
+      removeMapContainer(map, container);
+    });
+
+    it("updates radius when setStyle is called", () => {
+      circleMarker.setStyle({ radius: 20 });
+      expect(circleMarker.getRadius()).to.eql(20);
+    });
+
+    it("updates other styles without changing radius", () => {
+      const oldRadius = circleMarker.getRadius();
+      circleMarker.setStyle({ color: "red" });
+      expect(circleMarker.getRadius()).to.eql(oldRadius);
+      expect(circleMarker.options.color).to.eql("red");
+    });
+  });
 });
--- src/layer/vector/Circle.js
+++ src/layer/vector/Circle.js
@@ -1,10 +1,9 @@
-import {CircleMarker} from './CircleMarker.js';
-import {Path} from './Path.js';
-import * as Util from '../../core/Util.js';
-import {LatLng} from '../../geo/LatLng.js';
-import {LatLngBounds} from '../../geo/LatLngBounds.js';
-import {Earth} from '../../geo/crs/CRS.Earth.js';
-
+import { CircleMarker } from "./CircleMarker.js";
+import { Path } from "./Path.js";
+import * as Util from "../../core/Util.js";
+import { LatLng } from "../../geo/LatLng.js";
+import { LatLngBounds } from "../../geo/LatLngBounds.js";
+import { Earth } from "../../geo/crs/CRS.Earth.js";
 
 /*
  * @class Circle
@@ -25,76 +24,91 @@ import {Earth} from '../../geo/crs/CRS.Earth.js';
 // Instantiates a circle object given a geographical point, and an options object
 // which contains the circle radius.
 export class Circle extends CircleMarker {
+  initialize(latlng, options) {
+    Util.setOptions(this, options);
+    this._latlng = new LatLng(latlng);
 
-	initialize(latlng, options) {
-		Util.setOptions(this, options);
-		this._latlng = new LatLng(latlng);
+    if (isNaN(this.options.radius)) {
+      throw new Error("Circle radius cannot be NaN");
+    }
 
-		if (isNaN(this.options.radius)) { throw new Error('Circle radius cannot be NaN'); }
+    // @section
+    // @aka Circle options
+    // @option radius: Number; Radius of the circle, in meters.
+    this._mRadius = this.options.radius;
+  }
 
-		// @section
-		// @aka Circle options
-		// @option radius: Number; Radius of the circle, in meters.
-		this._mRadius = this.options.radius;
-	}
+  // @method setRadius(radius: Number): this
+  // Sets the radius of a circle. Units are in meters.
+  setRadius(radius) {
+    this._mRadius = radius;
+    return this.redraw();
+  }
 
-	// @method setRadius(radius: Number): this
-	// Sets the radius of a circle. Units are in meters.
-	setRadius(radius) {
-		this._mRadius = radius;
-		return this.redraw();
-	}
+  // @method getRadius(): Number
+  // Returns the current radius of a circle. Units are in meters.
+  getRadius() {
+    return this._mRadius;
+  }
 
-	// @method getRadius(): Number
-	// Returns the current radius of a circle. Units are in meters.
-	getRadius() {
-		return this._mRadius;
-	}
+  // @method getBounds(): LatLngBounds
+  // Returns the `LatLngBounds` of the path.
+  getBounds() {
+    const half = [this._radius, this._radiusY ?? this._radius];
 
-	// @method getBounds(): LatLngBounds
-	// Returns the `LatLngBounds` of the path.
-	getBounds() {
-		const half = [this._radius, this._radiusY ?? this._radius];
+    return new LatLngBounds(
+      this._map.layerPointToLatLng(this._point.subtract(half)),
+      this._map.layerPointToLatLng(this._point.add(half))
+    );
+  }
 
-		return new LatLngBounds(
-			this._map.layerPointToLatLng(this._point.subtract(half)),
-			this._map.layerPointToLatLng(this._point.add(half)));
-	}
+  setStyle(style) {
+    // Update radius if provided
+    if (style && style.radius !== undefined) {
+      this.setRadius(style.radius);
+    }
 
-	setStyle = Path.prototype.setStyle;
+    // Call the original Path.setStyle for other style options
+    return Path.prototype.setStyle.call(this, style);
+  }
 
-	_project() {
+  _project() {
+    const lng = this._latlng.lng,
+      lat = this._latlng.lat,
+      map = this._map,
+      crs = map.options.crs;
 
-		const lng = this._latlng.lng,
-		    lat = this._latlng.lat,
-		    map = this._map,
-		    crs = map.options.crs;
+    if (crs.distance === Earth.distance) {
+      const d = Math.PI / 180,
+        latR = this._mRadius / Earth.R / d,
+        top = map.project([lat + latR, lng]),
+        bottom = map.project([lat - latR, lng]),
+        p = top.add(bottom).divideBy(2),
+        lat2 = map.unproject(p).lat;
+      let lngR =
+        Math.acos(
+          (Math.cos(latR * d) - Math.sin(lat * d) * Math.sin(lat2 * d)) /
+            (Math.cos(lat * d) * Math.cos(lat2 * d))
+        ) / d;
 
-		if (crs.distance === Earth.distance) {
-			const d = Math.PI / 180,
-			      latR = (this._mRadius / Earth.R) / d,
-			      top = map.project([lat + latR, lng]),
-			      bottom = map.project([lat - latR, lng]),
-			      p = top.add(bottom).divideBy(2),
-			      lat2 = map.unproject(p).lat;
-			let lngR = Math.acos((Math.cos(latR * d) - Math.sin(lat * d) * Math.sin(lat2 * d)) /
-			            (Math.cos(lat * d) * Math.cos(lat2 * d))) / d;
+      if (isNaN(lngR) || lngR === 0) {
+        lngR = latR / Math.cos((Math.PI / 180) * lat); // Fallback for edge case, #2425
+      }
 
-			if (isNaN(lngR) || lngR === 0) {
-				lngR = latR / Math.cos(Math.PI / 180 * lat); // Fallback for edge case, #2425
-			}
+      this._point = p.subtract(map.getPixelOrigin());
+      this._radius = isNaN(lngR) ? 0 : p.x - map.project([lat2, lng - lngR]).x;
+      this._radiusY = p.y - top.y;
+    } else {
+      const latlng2 = crs.unproject(
+        crs.project(this._latlng).subtract([this._mRadius, 0])
+      );
 
-			this._point = p.subtract(map.getPixelOrigin());
-			this._radius = isNaN(lngR) ? 0 : p.x - map.project([lat2, lng - lngR]).x;
-			this._radiusY = p.y - top.y;
+      this._point = map.latLngToLayerPoint(this._latlng);
+      this._radius = Math.abs(
+        this._point.x - map.latLngToLayerPoint(latlng2).x
+      );
+    }
 
-		} else {
-			const latlng2 = crs.unproject(crs.project(this._latlng).subtract([this._mRadius, 0]));
-
-			this._point = map.latLngToLayerPoint(this._latlng);
-			this._radius = Math.abs(this._point.x - map.latLngToLayerPoint(latlng2).x);
-		}
-
-		this._updateBounds();
-	}
+    this._updateBounds();
+  }
 }
