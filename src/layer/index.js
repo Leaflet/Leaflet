cbe2d0a1eeed70acc06bc7e9f59085aa267d883d
Simon Legner <Simon.Legner@gmail.com>
2025-08-17 06:38:53 +0200

    Move GeoJSON function as static functions into class (#9840)

--- src/layer/index.js
+++ src/layer/index.js
@@ -1,17 +1,7 @@
 export {Layer} from './Layer.js';
 export {LayerGroup} from './LayerGroup.js';
 export {FeatureGroup} from './FeatureGroup.js';
-import {GeoJSON, geometryToLayer, coordsToLatLng, coordsToLatLngs, latLngToCoords, latLngsToCoords, getFeature, asFeature} from './GeoJSON.js';
-
-// TODO: can they be static functions in the GeoJSON Class?
-GeoJSON.geometryToLayer = geometryToLayer;
-GeoJSON.coordsToLatLng = coordsToLatLng;
-GeoJSON.coordsToLatLngs = coordsToLatLngs;
-GeoJSON.latLngToCoords = latLngToCoords;
-GeoJSON.latLngsToCoords = latLngsToCoords;
-GeoJSON.getFeature = getFeature;
-GeoJSON.asFeature = asFeature;
-export {GeoJSON};
+export {GeoJSON} from './GeoJSON.js';
 
 export {BlanketOverlay} from './BlanketOverlay.js';
 
