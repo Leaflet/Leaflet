91a33d27060b80b57f135edcc9cfdbe2eac33262
Jon Koops <jonkoops@gmail.com>
2025-10-23 12:50:47 +0200

    Remove user-agent detection for `Browser.mobile` (#9952)
    Signed-off-by: Jon Koops <jonkoops@gmail.com>

--- src/core/Browser.js
+++ src/core/Browser.js
@@ -19,7 +19,7 @@ const chrome = userAgentContains('chrome');
 const safari = !chrome && userAgentContains('safari');
 
 // @property mobile: Boolean; `true` for all browsers running in a mobile device.
-const mobile = typeof orientation !== 'undefined' || userAgentContains('mobile');
+const mobile = typeof orientation !== 'undefined';
 
 // @property pointer: Boolean
 // `true` for all browsers supporting [pointer events](https://msdn.microsoft.com/en-us/library/dn433244%28v=vs.85%29.aspx).
