c5feaf4a09cf9d0f5244be9da66bd614f5148fa6
Jon Koops <jonkoops@gmail.com>
2025-12-30 12:06:55 +0100

    Replace `uglify-js` scripts with `@rollup/plugin-terser` (#10041)
    Signed-off-by: Jon Koops <jonkoops@gmail.com>

--- build/rollup-config.js
+++ build/rollup-config.js
@@ -1,60 +1,81 @@
+import terser from '@rollup/plugin-terser';
 import {readFileSync} from 'node:fs';
+import {defineConfig} from 'rollup';
 import {simpleGit} from 'simple-git';
 import pkg from '../package.json' with {type: 'json'};
 
-const release = process.env.NODE_ENV === 'release';
 const version = await getVersion();
-const banner = createBanner(version);
+const banner = `/* @preserve
+ * Leaflet ${version}, a JS library for interactive maps. https://leafletjs.com
+ * (c) 2010-${new Date().getFullYear()} Volodymyr Agafonkin, (c) 2010-2011 CloudMade
+ */
+`;
 
-/** @type {import('rollup').RollupOptions} */
-const config = {
-	input: 'src/Leaflet.js',
-	output: [
-		{
-			file: pkg.exports['.'],
-			format: 'es',
-			banner,
-			sourcemap: true
-		},
-		{
-			file: './dist/leaflet-global-src.js',
-			name: 'L',
-			format: 'umd',
-			banner,
-			sourcemap: true,
-			freeze: false,
-			esModule: false,
-			noConflict: true,
-			amd: {
-				id: pkg.name
-			}
+const STATIC_ASSETS = [
+	'leaflet.css',
+	'images/logo.svg',
+	'images/layers.svg',
+	'images/marker-icon.svg',
+	'images/marker-shadow.svg'
+];
+
+/** @type {import('rollup').OutputPlugin} */
+const staticAssetsPlugin = {
+	name: 'static-assets',
+	generateBundle() {
+		for (const fileName of STATIC_ASSETS) {
+			const source = readFileSync(new URL(`../src/${fileName}`, import.meta.url));
+			this.emitFile({type: 'asset', fileName, source});
 		}
-	],
-	plugins: [
-		{
-			name: 'copy-leaflet-assets',
-			generateBundle() {
-				const fileNames = [
-					'leaflet.css',
-					'images/logo.svg',
-					'images/layers.svg',
-					'images/marker-icon.svg',
-					'images/marker-shadow.svg',
-				];
-				for (const fileName of fileNames) {
-					const source = readFileSync(new URL(`../src/${fileName}`, import.meta.url));
-					this.emitFile({type: 'asset',	fileName, source});
-				}
-			},
-		},
-	]
+	},
 };
 
-export default config;
+/** @type {import('rollup').OutputOptions} */
+const commonOptions = {
+	banner,
+	sourcemap: true
+};
+
+/** @type {import('rollup').OutputOptions} */
+const umdOptions = {
+	...commonOptions,
+	name: 'L',
+	format: 'umd',
+	freeze: false,
+	noConflict: true,
+	amd: {
+		id: pkg.name
+	}
+};
+
+export default defineConfig({
+	input: './src/Leaflet.js',
+	output: [
+		{
+			...commonOptions,
+			file: pkg.exports['.'],
+			plugins: [staticAssetsPlugin]
+		},
+		{
+			...commonOptions,
+			file: './dist/leaflet.js',
+			plugins: [terser()],
+		},
+		{
+			...umdOptions,
+			file: './dist/leaflet-global-src.js',
+		},
+		{
+			...umdOptions,
+			file: './dist/leaflet-global.js',
+			plugins: [terser()],
+		}
+	]
+});
 
 async function getVersion() {
 	// Skip the git branch+rev in the banner when doing a release build
-	if (release) {
+	if (process.env.NODE_ENV === 'release') {
 		return pkg.version;
 	}
 
@@ -63,12 +84,4 @@ async function getVersion() {
 	const commit = await git.revparse(['--short', 'HEAD']);
 
 	return `${pkg.version}+${branch}.${commit}`;
-}
-
-export function createBanner(version) {
-	return `/* @preserve
- * Leaflet ${version}, a JS library for interactive maps. https://leafletjs.com
- * (c) 2010-${new Date().getFullYear()} Volodymyr Agafonkin, (c) 2010-2011 CloudMade
- */
-`;
 }
