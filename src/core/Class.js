d5eba907c74649ed6958ffd972b78c7144d15e57
Simon Legner <Simon.Legner@gmail.com>
2026-01-01 14:15:40 +0100

    Class: _initHooksReady = Promise.withResolvers

--- src/core/Class.js
+++ src/core/Class.js
@@ -63,7 +63,7 @@ export class Class {
 	}
 
 	constructor(...args) {
-		this._initHooksCalled = false;
+		this._initHooksReady = Promise.withResolvers(); //  Baseline 2024 Newly available
 
 		Util.setOptions(this);
 
@@ -74,12 +74,16 @@ export class Class {
 
 		// call all constructor hooks
 		this.callInitHooks();
+
+		this._initHooksReady.resolve();
 	}
 
-	callInitHooks() {
-		if (this._initHooksCalled) {
+	async callInitHooks() {
+		if (!this._initHooksReady) { // _initHooksCalled
 			return;
 		}
+		await this._initHooksReady.promise;
+		this._initHooksReady = undefined;
 
 		// collect all prototypes in chain
 		const prototypes = [];
@@ -98,7 +102,9 @@ export class Class {
 				hook.call(this);
 			}
 		}
-
-		this._initHooksCalled = true;
 	}
+}
+
+export function withInitHooks(TargetClass) {
+	return TargetClass;
 }
