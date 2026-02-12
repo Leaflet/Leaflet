c0f194f13f8b6b3d565637372f88e65ddac201c5
pierreeurope <pierre.europe@pm.me>
2026-02-12 09:52:50 +0100

    Docs: Add documentation for move and zoom event properties
    Added documentation for the properties included in move and zoom events:
    - move event: Documents flyTo and pinch properties
    - zoom event: Documents pinch property
    
    This improves API documentation by making it clear what data is available
    in these commonly-used events.

--- src/map/Map.js
+++ src/map/Map.js
@@ -1243,6 +1243,8 @@ export class LeafletMap extends Evented {
 			// @event zoom: Event
 			// Fired repeatedly during any change in zoom level,
 			// including zoom and fly animations.
+			// The event object may contain a `pinch` property (boolean) indicating
+			// whether the zoom originated from a pinch gesture.
 			if (zoomChanged || (data?.pinch)) {	// Always fire 'zoom' if pinching because #3530
 				this.fire('zoom', data);
 			}
@@ -1250,6 +1252,9 @@ export class LeafletMap extends Evented {
 			// @event move: Event
 			// Fired repeatedly during any movement of the map,
 			// including pan and fly animations.
+			// The event object contains a `flyTo` property (boolean) indicating
+			// whether the move is part of a flyTo animation, and may contain
+			// a `pinch` property (boolean) for pinch-related moves.
 			this.fire('move', data);
 		} else if (data?.pinch) {	// Always fire 'zoom' if pinching because #3530
 			this.fire('zoom', data);
