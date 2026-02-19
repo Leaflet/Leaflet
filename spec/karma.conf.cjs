34236567ee7c01a4a16186ed3f409e8032bcd226
Jon Koops <jonkoops@gmail.com>
2026-02-19 17:58:02 +0100

    Upgrade to ESLint v10 and fix reported issues
    Signed-off-by: Jon Koops <jonkoops@gmail.com>

--- spec/karma.conf.cjs
+++ spec/karma.conf.cjs
@@ -1,4 +1,3 @@
-/* eslint-env node */
 // See: https://karma-runner.github.io/latest/config/configuration-file.html
 module.exports = function (/** @type {import('karma').Config} */ config) {
 	const isCoverageEnabled = process.argv.includes('--coverage');
@@ -11,16 +10,24 @@ module.exports = function (/** @type {import('karma').Config} */ config) {
 			'karma-chrome-launcher',
 			'karma-safarinative-launcher',
 			'karma-firefox-launcher',
-			'karma-time-stats-reporter'
+			'karma-time-stats-reporter',
 		],
 		frameworks: ['mocha'],
 		customContextFile: 'spec/context.html',
 		customDebugFile: 'spec/debug.html',
 		files: [
 			{pattern: 'node_modules/chai/**/*', included: false, served: true},
-			{pattern: 'node_modules/prosthetic-hand/**/*', included: false, served: true},
+			{
+				pattern: 'node_modules/prosthetic-hand/**/*',
+				included: false,
+				served: true,
+			},
 			{pattern: 'node_modules/sinon/**/*', included: false, served: true},
-			{pattern: 'node_modules/ui-event-simulator/**/*', included: false, served: true},
+			{
+				pattern: 'node_modules/ui-event-simulator/**/*',
+				included: false,
+				served: true,
+			},
 			{pattern: 'dist/leaflet-src.js', included: false, served: true},
 			{pattern: 'src/**/*.js', included: false, served: true},
 			{pattern: 'package.json', included: false, served: true},
@@ -32,53 +39,50 @@ module.exports = function (/** @type {import('karma').Config} */ config) {
 		reporters: ['progress', 'time-stats'],
 		coverageReporter: {
 			dir: 'coverage/',
-			reporters: [
-				{type: 'html', subdir: 'html'},
-				{type: 'text-summary'}
-			]
+			reporters: [{type: 'html', subdir: 'html'}, {type: 'text-summary'}],
 		},
 		timeStatsReporter: {
 			reportTimeStats: false,
-			longestTestsCount: 10
+			longestTestsCount: 10,
 		},
 		logLevel: config.LOG_WARN,
 		browsers: ['Chrome'],
 		customLaunchers: {
-			'Chrome': {
+			Chrome: {
 				base: 'ChromeHeadless',
 				flags: [
 					'--window-size=1920,1080', // Set a fixed window size
-				]
+				],
 			},
-			'Firefox': {
+			Firefox: {
 				base: 'FirefoxHeadless',
 				prefs: {
-					'dom.w3c_touch_events.enabled': 1
-				}
+					'dom.w3c_touch_events.enabled': 1,
+				},
 			},
-			'FirefoxRetina': {
+			FirefoxRetina: {
 				base: 'FirefoxHeadless',
 				prefs: {
-					'layout.css.devPixelsPerPx': 2
-				}
-			}
+					'layout.css.devPixelsPerPx': 2,
+				},
+			},
 			// 'SafariNative'
 		},
 		concurrency: 1,
 		browserConsoleLogOptions: {level: 'error'},
 		client: {
 			mocha: {
-				forbidOnly: process.env.CI || false
+				forbidOnly: process.env.CI || false,
 			},
-			runAsTouchBrowser
-		}
+			runAsTouchBrowser,
+		},
 	};
 
 	if (isCoverageEnabled) {
 		karmaConfig.plugins.push('karma-coverage');
 		karmaConfig.reporters.push('coverage');
 		karmaConfig.preprocessors = {
-			'dist/leaflet-src.js': 'coverage'
+			'dist/leaflet-src.js': 'coverage',
 		};
 	}
 
